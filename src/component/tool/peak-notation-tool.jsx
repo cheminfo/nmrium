119a509d718484992f78f78cc19a6bf980d3e75d
hamed-musallam <hamed.musallam@gmail.com>
2019-07-05 12:56:25 +0200

    fix zoom in and out

--- src/component/tool/peak-notation-tool.jsx
+++ src/component/tool/peak-notation-tool.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import PropTypes from 'prop-types';
-import '../css/peak-notification-tool.scss';
+import '../css/peak-notification-tool.css';
 
 export const NotationTemplate = ({ id, x, y, value }) => {
   // console.log(x);
