d83b10100f4112c9126363e1cd8602da3a7e86bb
Luc Patiny <luc@patiny.com>
2019-07-26 10:10:03 +0200

    fix eslint of filter

--- src/data/filter1d/filter.js
+++ src/data/filter1d/filter.js
@@ -1,12 +1,11 @@
-import {SHIFT_X} from './filter1d-type';
+import { SHIFT_X } from './filter1d-type';
 import shiftX from './shiftX';
 
-export default function applyFilter(filterOption,data){
-
-    switch(filterOption.kind){
-         case SHIFT_X:
-             return {x:shiftX(data.x,filterOption.value),y:data.y};
-        default: 
-        return null;
-    }
+export default function applyFilter(filterOption, data) {
+  switch (filterOption.kind) {
+    case SHIFT_X:
+      return { x: shiftX(data.x, filterOption.value), y: data.y };
+    default:
+      return null;
+  }
 }
