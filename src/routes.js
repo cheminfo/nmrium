02cb6280e410bd9b04a34980bf03697fa5d7964a
Luc Patiny <luc@patiny.com>
2019-08-06 21:22:35 +0200

    rename Spectrum13C to Fid13C

--- src/routes.js
+++ src/routes.js
@@ -17,35 +17,31 @@
 */
 // import Notifications from "./dashboard/views/Notifications.jsx";
 // import Icons from "./dashboard/views/Icons.jsx";
-import Spectrum13C from "./dashboard/views/Spectrum13C";
-import Spectrum1H from "./dashboard/views/Spectrum1H";
-import XTC814d from "./dashboard/views/XTC814d";
+import Spectrum13C from './dashboard/views/Fid13C';
+import Spectrum1H from './dashboard/views/Spectrum1H';
+import XTC814d from './dashboard/views/XTC814d';
 
 var dashRoutes = [
-
   {
-    path: "/Spectrum1H",
-    name: "1H Spectrum",
-    icon: "design-2_ruler-pencil",
+    path: '/Spectrum1H',
+    name: '1H Spectrum',
+    icon: 'design-2_ruler-pencil',
     component: Spectrum1H,
-    layout: "/admin",
-
+    layout: '/admin',
   },
   {
-    path: "/Spectrum13C",
-    name: "13C Spectrum",
-    icon: "design-2_ruler-pencil",
-    component:Spectrum13C,
-    layout: "/admin",
-  }
-  ,
+    path: '/Fid13C',
+    name: '13C FID',
+    icon: 'design-2_ruler-pencil',
+    component: Spectrum13C,
+    layout: '/admin',
+  },
   {
-    path: "/XTC814d",
-    name: "XTC 814d",
-    icon: "design-2_ruler-pencil",
-    component:XTC814d,
-    layout: "/admin",
-  }
-  
+    path: '/XTC814d',
+    name: 'XTC 814d',
+    icon: 'design-2_ruler-pencil',
+    component: XTC814d,
+    layout: '/admin',
+  },
 ];
 export default dashRoutes;
