0eeeae4e285c2f8f9e3e7562d9030334386514f8
Luc Patiny <luc@patiny.com>
2019-08-13 09:36:48 +0200

    rename to Analysis

---
+++ src/data/Analysis.js
@@ -0,0 +1,10 @@
+export class Analysis {
+  constructor() {
+    this.data1d = [];
+    this.data2d = [];
+    this.molfile = []; // chemical structures
+    this.preferences = {
+      display: {},
+    };
+  }
+}
--- src/data/Sample.js
+++
@@ -1,7 +0,0 @@
-export class Sample {
-  constructor() {
-    this.data1d = [];
-    this.data2d = [];
-    this.molfile = ''; // chemical structure
-  }
-}
