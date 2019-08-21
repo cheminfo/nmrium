27a382c180aa5a961ef0e026edad65a425ef7130
hamed-musallam <hamed.musallam@gmail.com>
2019-08-21 09:12:27 +0200

    fix bugs when get data from jcamp file

--- src/data/__tests__/Datum1D.test.js
+++ src/data/__tests__/Datum1D.test.js
@@ -4,9 +4,8 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 describe('test Datum1D', () => {
-  let Data1DManagerObject = new Data1DManager(); 
   let jcamp = readFileSync(join(__dirname, './data/ethylbenzene-1h.jdx'), 'utf8');
-  let data = Data1DManagerObject.fromJcamp('1', jcamp, 'test', 'red', true,true);
+  let data = Data1DManager.fromJcamp( jcamp, 'test', 'red', true,true);
 
   test('check x, re, im', () => {
     expect(data.x).toHaveLength(16384);
