d345d4b96fc858650e995dcc88f403b0f6824a9b
hamed-musallam <hamed.musallam@gmail.com>
2019-09-02 16:03:35 +0200

    fetal: integrate with  molecule editor structure package

--- src/component/toolbar/MoleculePanel.jsx
+++ src/component/toolbar/MoleculePanel.jsx
@@ -1,12 +1,19 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react';
-import { SmilesSvgRenderer  } from 'react-ocl';
+import { SmilesSvgRenderer } from 'react-ocl';
 
-import StructureEditor from 'openchemlib';
-
+// import StructureEditor from 'openchemlib';
+import { StructureEditor } from 'react-ocl/full';
 import { FaPlus, FaPaste } from 'react-icons/fa';
 
 import '../css/molecule.css';
-import { Button, Tooltip } from '@material-ui/core';
+import {
+  Button,
+  Tooltip,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@material-ui/core';
 // import { SVGRenderer } from 'openchemlib/types';
 
 const initialMolfile = `
@@ -25,9 +32,9 @@ Actelion Java MolfileCreator 1.0
   6  5  1  0  0  0  0
 M  END
 `;
-const MoleculePanel = () => {
-  const refMoleculeEditor = useRef();
 
+export const StructureEditorModal = (props) => {
+  const { onClose, selectedValue, open } = props;
   const [molfile, setMolfile] = useState(initialMolfile);
 
   const cb = useCallback(
@@ -36,6 +43,53 @@ const MoleculePanel = () => {
     },
     [setMolfile],
   );
+
+  const handleClose = useCallback(() => {
+    onClose(selectedValue);
+  }, [selectedValue, onClose]);
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      aria-labelledby="simple-dialog-title"
+      open={open}
+      fullWidth={true}
+      maxWidth="md"
+    >
+      <DialogContent dividers>
+        {console.log(open)}
+        {/* <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle> */}
+        <StructureEditor
+          initialMolfile={molfile}
+          svgMenu={true}
+          fragment={false}
+          onChange={cb}
+        />
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Close
+        </Button>
+        <Button onClick={handleClose} color="primary">
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+const MoleculePanel = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const handleOpen = useCallback(() => {
+    console.log('ssss');
+    setOpen(true);
+  }, []);
 
   return (
     <div className="molecule-container">
@@ -46,24 +100,17 @@ const MoleculePanel = () => {
           </Button>
         </Tooltip>
         <Tooltip title="Add Molecule" placement="left-start">
-          <Button>
+          <Button onClick={handleOpen}>
             <FaPlus />
           </Button>
         </Tooltip>
       </div>
       <div className="molecule-body">
-       <SmilesSvgRenderer smiles="COCCOOOCO" />
-          {/* <StructureEditor
-          initialMolfile={molfile}
-           svgMenu={true}
-           fragment={false}
-           onChange={cb}
-         /> */}
+        <SmilesSvgRenderer smiles="COCCOOOCO" />
+        <StructureEditorModal open={open} onClose={handleClose} />
       </div>
     </div>
   );
 };
 
 export default MoleculePanel;
-
-    
