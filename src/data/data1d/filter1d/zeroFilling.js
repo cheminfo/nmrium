afd31b18310c1877ecf23afb14a4d625cc55466e
hamed-musallam <hamed.musallam@gmail.com>
2019-10-28 16:15:18 +0100

    fix zero filling bug

--- src/data/data1d/filter1d/zeroFilling.js
+++ src/data/data1d/filter1d/zeroFilling.js
@@ -10,14 +10,16 @@ export default function zeroFilling(datum1D, size) {
   }
   const re = datum1D.data.re;
   const im = datum1D.data.im;
-  const newRE = new Float64Array(size);
-  const newIM = new Float64Array(size);
+  let newRE = new Float64Array(size);
+  let newIM = new Float64Array(size);
   const length = Math.min(size, re.length);
   for (let i = 0; i < length; i++) {
     newRE[i] = re[i];
     newIM[i] = im[i];
   }
-  datum1D.data = { ...datum1D.data, ...{ re: newRE, newIM } };
+  newRE = [...newRE, ...Array(datum1D.data.x.length - newRE.length).fill(0)];
+  newIM = [...newRE, ...Array(datum1D.data.x.length - newRE.length).fill(0)];
+  datum1D.data = { ...datum1D.data, ...{ re: newRE, im: newIM } };
 }
 
 export function isApplicable(datum1D) {
