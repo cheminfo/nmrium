2f9c7b91c1a5500f43987b88ab3612cdf75386f2
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-11-04 15:17:57 +0100

    ready for tests : set value to the added with in Hz

---
+++ src/data/data1d/filter1d/__tests__/lineBroadening.test.js
@@ -0,0 +1,23 @@
+import lineBroadening from '../lineBroadening';
+
+describe('lineBroadening', () => {
+  it('simple x, re, im to 1', () => {
+    let spectrum = {
+      data: {
+        re: [1, 1, 1, 1],
+        im: [0, 0, 0, 0],
+        x: [0, 1, 2, 3],
+      },
+      info: {
+        isComplex: true,
+        isFid: true,
+      },
+    };
+    lineBroadening(spectrum, 1);
+    expect(spectrum.data).toStrictEqual({
+    //  re: [1, 1, 1, 1],
+    //  im: [0, 0, 0, 0],
+      del: [-2.718281828459045],
+    });
+  });
+});
