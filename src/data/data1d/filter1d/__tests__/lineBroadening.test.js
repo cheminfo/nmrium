84898924fd4f1a393b2e1febf52ef154d2a4a446
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-11-04 15:38:21 +0100

    ready for tests : set value to the added with in Hz7

--- src/data/data1d/filter1d/__tests__/lineBroadening.test.js
+++ src/data/data1d/filter1d/__tests__/lineBroadening.test.js
@@ -15,8 +15,10 @@ describe('lineBroadening', () => {
     };
     lineBroadening(spectrum, 1);
     expect(spectrum.data).toStrictEqual({
-      re: [1, 0.9, 0.8, 0.7],// not correct... but just testing...
-      im: [0, 0, 0, 0],
+      re: new Float64Array([1, 0.9, 0.8, 0.7]),// not correct... but just testing...
+      im: new Float64Array([0, 0, 0, 0],
+      x: new Float64Array([0, 1, 2, 3]),
+      });
    // del: [-2.718281828459045],
    //del: -2.718,
   });
