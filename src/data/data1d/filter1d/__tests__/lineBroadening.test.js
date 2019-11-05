4a8260087fee3b9176046354289b7278099c0b37
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-11-05 09:07:58 +0100

    fixed problem with Array/Float64Array in test

--- src/data/data1d/filter1d/__tests__/lineBroadening.test.js
+++ src/data/data1d/filter1d/__tests__/lineBroadening.test.js
@@ -1,12 +1,12 @@
 import lineBroadening from '../lineBroadening';
 
 describe('lineBroadening', () => {
-  it('simple x, re, im to 1', () => {
+  it('simple x, re, im to 1 Hz exp.', () => {
     let spectrum = {
       data: {
-        re: [1, 1, 1, 1],
-        im: [0, 0, 0, 0],
-        x: [0, 1, 2, 3],
+        re: new Float64Array([1, 1, 1, 1]),//re: [1, 1, 1, 1], // was not OK...
+        im: new Float64Array([0, 0, 0, 0]),
+        x: new Float64Array([0, 1, 2, 3]),
       },
       info: {
         isComplex: true,
@@ -14,10 +14,16 @@ describe('lineBroadening', () => {
       },
     };
     lineBroadening(spectrum, 1);
-    expect(spectrum.data).toStrictEqual({
-      re: new Float64Array([ 1, 0.06598803584531254, 0.004354420874722253, 0.00028733968076674924]),
+    //expect(spectrum.data).toBeCloseTo({
+      expect(spectrum.data).toStrictEqual({
+        re: new Float64Array([
+        1,
+        0.06598803584531254,
+        0.004354420874722253,
+        0.00028733968076674924
+      ]),
       im: new Float64Array([0, 0, 0, 0]),
       x: new Float64Array([0, 1, 2, 3]),
-    });
+    },5);
   });
 });
