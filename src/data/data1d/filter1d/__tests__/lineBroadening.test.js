2cc542d53f4b77dc7f315aecd9a1cc69d3256080
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-11-05 11:31:30 +0100

    fixed format

--- src/data/data1d/filter1d/__tests__/lineBroadening.test.js
+++ src/data/data1d/filter1d/__tests__/lineBroadening.test.js
@@ -4,7 +4,7 @@ describe('lineBroadening', () => {
   it('simple x, re, im to 1 Hz exp.', () => {
     let spectrum = {
       data: {
-        re: new Float64Array([1, 1, 1, 1]),//re: [1, 1, 1, 1], // was not OK...
+        re: new Float64Array([1, 1, 1, 1]), //re: [1, 1, 1, 1], // was not OK...
         im: new Float64Array([0, 0, 0, 0]),
         x: new Float64Array([0, 1, 2, 3]),
       },
@@ -15,15 +15,18 @@ describe('lineBroadening', () => {
     };
     lineBroadening(spectrum, 1);
     //expect(spectrum.data).toBeCloseTo({
-      expect(spectrum.data).toStrictEqual({
+    expect(spectrum.data).toStrictEqual(
+      {
         re: new Float64Array([
-        1,
-        0.06598803584531254,// this is ugly... may not work. toBeCloseTo not for objects
-        0.004354420874722253,
-        0.00028733968076674924
-      ]),
-      im: new Float64Array([0, 0, 0, 0]),
-      x: new Float64Array([0, 1, 2, 3]),
-    },5);
+          1,
+          0.06598803584531254, // this is ugly... may not work. toBeCloseTo not for objects
+          0.004354420874722253,
+          0.00028733968076674924,
+        ]),
+        im: new Float64Array([0, 0, 0, 0]),
+        x: new Float64Array([0, 1, 2, 3]),
+      },
+      5,
+    );
   });
 });
