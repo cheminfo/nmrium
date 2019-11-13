14d180fbb18a0a5e29fc771d9e3049bc806e37fc
hamed-musallam <hamed.musallam@gmail.com>
2019-11-13 13:03:10 +0100

    push new components

---
+++ src/component/Notations/ModifiedPeakNotation.jsx
@@ -0,0 +1,62 @@
+import { jsx, css } from '@emotion/core';
+
+/** @jsx jsx */
+
+const styles2 = css`
+  pointer-events: bounding-box;
+  user-select: none;
+  :hover .target {
+    visibility: visible !important;
+    cursor: pointer;
+  }
+
+  .target {
+    visibility: hidden;
+  }
+`;
+
+function DeleteButton() {
+  return (
+    <svg className="target">
+      <rect rx="5" width="14" height="14" fill="#c81121" />
+      <line x1="5" x2="10" y1="8" y2="8" stroke="white" strokeWidth="2" />
+    </svg>
+  );
+}
+
+function EditButton() {
+  return (
+    <svg className="target" transform="translate(0,16)">
+      <rect rx="5" width="14" height="14" fill="#c81121" />
+      <line x1="5" x2="10" y1="8" y2="8" stroke="white" strokeWidth="2" />
+    </svg>
+  );
+}
+
+export const ModifiedPeakNotation = ({
+  id,
+  x,
+  y,
+  value,
+  color,
+  decimalFraction,
+}) => {
+  return (
+    <g css={styles2} id={id} transform={`translate(${x},${y})`}>
+      <line x1="0" x2="0" y1="-5" y2={-30} stroke={color} strokeWidth="1" />
+      <text
+        x="40"
+        y="5"
+        dy="0em"
+        dx="0em"
+        style={{ transform: 'rotate(-90deg)' }}
+      >
+        {parseFloat(value).toFixed(decimalFraction)}
+      </text>
+      <DeleteButton />
+      <EditButton />
+    </g>
+  );
+};
+
+export default ModifiedPeakNotation;
