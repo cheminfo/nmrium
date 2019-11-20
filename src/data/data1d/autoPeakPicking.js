fc1e03c89a14f47867475de4788956b711350bc7
jobo322 <jose.bolanos@correounivalle.edu.co>
2019-11-20 10:01:10 -0500

    use spectra-data to make autoPeakPicking

--- src/data/data1d/autoPeakPicking.js
+++ src/data/data1d/autoPeakPicking.js
@@ -1,12 +1,13 @@
-import { gsd } from 'ml-gsd';
+import * as SD from 'spectra-data';
 
 export default function autoPeakPicking(datum1D, options) {
-  const { maxNumberOfPeaks, minMaxRatio } = options;
+  const { minMaxRatio } = options;
   // we calculate the noise but this could be improved
   let noise = datum1D.data.re.map((y) => Math.abs(y)).sort()[
     Math.floor(datum1D.data.re.length / 2)
   ];
-  const peaks = gsd(datum1D.data.x, datum1D.data.re, {
+  const spectrum = SD.NMR.fromXY(datum1D.data.x, datum1D.data.re);
+  const peaks = spectrum.createPeaks({
     noiseLevel: noise * 3,
     minMaxRatio: minMaxRatio, // Threshold to determine if a given peak should be considered as a noise
     realTopDetection: true,
@@ -15,7 +16,7 @@ export default function autoPeakPicking(datum1D, options) {
     sgOptions: { windowSize: 7, polynomial: 3 },
   });
 
-  return peaks.slice(0, maxNumberOfPeaks).map((peak) => {
+  return peaks.map((peak) => {
     return {
       xIndex: peak.index,
       xShift: datum1D.data.x[peak.index] - peak.xShift,
