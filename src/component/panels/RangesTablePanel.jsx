8a9be36bde52f30466e2f7632345c053d71ec448
michaelwenk <michael.wenk@uni-jena.de>
2019-11-25 19:58:42 +0100

    fetching ranges data "from", "to" and "integral" and showing in ranges table

--- src/component/panels/RangesTablePanel.jsx
+++ src/component/panels/RangesTablePanel.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   Table,
   TableHead,
-  //   TableBody,
+  TableBody,
   TableRow,
   TableCell,
 } from '../elements/Table';
@@ -12,21 +12,39 @@ import { useChartData } from '../context/ChartContext';
 import NoTableData from './placeholder/NoTableData';
 
 const RangesTablePanel = () => {
-  //   const [ranges, setRanges] = useState([]);
-  const [ranges] = useState([]); // to replace by line above
+  const [ranges, setRanges] = useState([]);
   const { activeSpectrum, data } = useChartData();
 
-  return activeSpectrum && data && ranges.length > 0 ? (
+  useEffect(() => {
+    if (data && activeSpectrum) {
+      const activeSpectrumData = data.find((d) => d.id === activeSpectrum.id);
+      if (activeSpectrumData) {
+        setRanges(activeSpectrumData.ranges);
+      } else {
+        setRanges({});
+      }
+    }
+  }, [activeSpectrum, data]);
+
+  return ranges && ranges.length > 0 ? (
     <>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell align="center">From</TableCell>
             <TableCell align="center">To</TableCell>
-            <TableCell align="center">Range</TableCell>
+            <TableCell align="center">Integral</TableCell>
           </TableRow>
         </TableHead>
-        {/* <TableBody /> */}
+        <TableBody>
+          {ranges.map((range) => (
+            <TableRow key={range.from + range.to + range.integral}>
+              <TableCell align="center">{range.from.toFixed(3)}</TableCell>
+              <TableCell align="center">{range.to.toFixed(3)}</TableCell>
+              <TableCell align="center">{range.integral.toFixed(1)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
       </Table>
     </>
   ) : (
