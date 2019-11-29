c83c6bd825d046571ec87e917b81b42504747f6a
michaelwenk <michael.wenk@uni-jena.de>
2019-11-29 12:14:28 +0100

    backwards

--- src/component/panels/RangesTablePanel.jsx
+++ src/component/panels/RangesTablePanel.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Table,
   TableHead,
-  TableBody,
+  //   TableBody,
   TableRow,
   TableCell,
 } from '../elements/Table';
@@ -12,35 +12,21 @@ import { useChartData } from '../context/ChartContext';
 import NoTableData from './placeholder/NoTableData';
 
 const RangesTablePanel = () => {
+  //   const [ranges, setRanges] = useState([]);
+  const [ranges] = useState([]); // to replace by line above
   const { activeSpectrum, data } = useChartData();
 
-  return activeSpectrum &&
-    data &&
-    data.find((d) => d.id === activeSpectrum.id && d.ranges) ? (
+  return activeSpectrum && data && ranges.length > 0 ? (
     <>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell align="center">From</TableCell>
             <TableCell align="center">To</TableCell>
-            <TableCell align="center">Integral</TableCell>
+            <TableCell align="center">Range</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {data
-            .filter((d) => d.id === activeSpectrum.id) // && d.ranges)
-            .map((d) =>
-              d.ranges.map((range) => (
-                <TableRow key={range.from + range.to + range.integral}>
-                  <TableCell align="center">{range.from.toFixed(3)}</TableCell>
-                  <TableCell align="center">{range.to.toFixed(3)}</TableCell>
-                  <TableCell align="center">
-                    {range.integral.toFixed(1)}
-                  </TableCell>
-                </TableRow>
-              )),
-            )}
-        </TableBody>
+        {/* <TableBody /> */}
       </Table>
     </>
   ) : (
