080957913e24cdc3ca76d7f02457d4ed09928a22
Luc Patiny <luc@patiny.com>
2019-12-06 16:35:58 +0100

    fix: auto peak picking takes into account the number of peaks

--- src/data/data1d/autoPeakPicking.js
+++ src/data/data1d/autoPeakPicking.js
@@ -1,13 +1,13 @@
 import * as SD from 'spectra-data';
 
 export default function autoPeakPicking(datum1D, options) {
-  const { minMaxRatio } = options;
+  const { minMaxRatio, maxNumberOfPeaks } = options;
   // we calculate the noise but this could be improved
   let noise = datum1D.data.re.map((y) => Math.abs(y)).sort()[
     Math.floor(datum1D.data.re.length / 2)
   ];
   const spectrum = SD.NMR.fromXY(datum1D.data.x, datum1D.data.re);
-  const peaks = spectrum.createPeaks({
+  let peaks = spectrum.createPeaks({
     noiseLevel: noise * 3,
     minMaxRatio: minMaxRatio, // Threshold to determine if a given peak should be considered as a noise
     realTopDetection: true,
@@ -15,6 +15,9 @@ export default function autoPeakPicking(datum1D, options) {
     smoothY: false,
     sgOptions: { windowSize: 7, polynomial: 3 },
   });
+
+  peaks.sort((a, b) => b.y - a.y);
+  if (maxNumberOfPeaks) peaks = peaks.slice(0, maxNumberOfPeaks);
 
   return peaks.map((peak) => {
     return {
