feb5e4f8b46458699677e2bb2f5718c616e6b053
hamed-musallam <hamed.musallam@gmail.com>
2020-01-29 10:48:17 +0100

    fix import default values

---
+++ src/component/panels/preferences-panels/defaultValues.js
@@ -0,0 +1,18 @@
+const integralDefaultValues = {
+  showValue: true,
+  valueFormat: '0.00',
+  showNB: true,
+  NBFormat: '0.00',
+};
+
+const peaksDefaultValues = {
+  showPanel: true,
+  PPMShow: true,
+  PPMFormat: '0.00',
+  HZShow: true,
+  HZFormat: '0.00',
+  peakWidthShow: true,
+  peakFormat: '0.00',
+};
+
+export { peaksDefaultValues, integralDefaultValues };
