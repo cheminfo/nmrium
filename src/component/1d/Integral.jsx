2145ec62c6574a3cff0997232b79e7bf9183a3d4
hamed-musallam <hamed.musallam@gmail.com>
2020-02-11 15:40:42 +0100

    close #332,close #333,close #334

---
+++ src/component/1d/Integral.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState, useCallback, Fragment } from 'react';
+import { XY } from 'ml-spectra-processing';
+
+import IntegralResizable from './IntegralResizable';
+
+const Integral = ({
+  x,
+  y,
+  from,
+  to,
+  xDomain,
+  scaleY,
+  scaleX,
+  isActive,
+  integralID,
+  spectrumID,
+}) => {
+  const [integral, setIntegral] = useState();
+
+  useEffect(() => {
+    const integralResult = XY.integral(
+      { x: x, y: y },
+      {
+        from: from,
+        to: to,
+        reverse: true,
+      },
+    );
+    setIntegral(integralResult);
+  }, [from, to, x, y]);
+
+  const makePath = useCallback(() => {
+    if (integral && scaleY) {
+      const pathPoints = XY.reduce(integral, {
+        from: xDomain[0],
+        to: xDomain[1],
+        nbPoints: 200,
+        optimize: true,
+      });
+
+      let path = `M ${scaleX(pathPoints.x[0])} ${scaleY(pathPoints.y[0])}`;
+
+      path += pathPoints.x
+        .slice(1)
+        .map((point, i) => {
+          return ` L ${scaleX(point)} ${scaleY(pathPoints.y[i])}`;
+        })
+        .join('');
+      //   console.log(path);
+      return path;
+    } else {
+      return '';
+    }
+  }, [integral, scaleX, scaleY, xDomain]);
+
+  return (
+    <Fragment>
+      <path
+        className="line"
+        stroke="black"
+        fill="none"
+        style={{
+          transformOrigin: 'center top',
+          opacity: isActive ? 1 : 0.2,
+        }}
+        d={makePath()}
+        // vectorEffect="non-scaling-stroke"
+      />
+
+      <IntegralResizable
+        spectrumID={spectrumID}
+        integralID={integralID}
+        integralData={integral}
+        from={from}
+        to={to}
+        scaleY={scaleY}
+      />
+    </Fragment>
+  );
+};
+
+export default Integral;
