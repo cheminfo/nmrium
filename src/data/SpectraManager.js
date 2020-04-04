8e4f15b120f6a8736cc27d51867cae9839f214b1
Luc Patiny <luc@patiny.com>
2020-04-04 18:24:52 +0200

    chore: allow console log profiling

--- src/data/SpectraManager.js
+++ src/data/SpectraManager.js
@@ -22,6 +22,7 @@ export function addJcamp(spectra, jcamp, options = {}) {
     keepRecordsRegExp: /.*/,
     profiling: true,
   });
+  // eslint-disable-next-line no-console
   console.log(converted.profiling);
   let entries = converted.flatten;
   if (entries.length === 0) return;
