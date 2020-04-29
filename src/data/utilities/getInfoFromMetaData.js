0849c23696ad537a72bc875023a926d2baecc3f8
jul <julien.wist@correounivalle.edu.co>
2020-04-29 11:13:21 -0500

    fix: change variable name

--- src/data/utilities/getInfoFromMetaData.js
+++ src/data/utilities/getInfoFromMetaData.js
@@ -25,7 +25,7 @@ export function getInfoFromMetaData(metaData) {
   maybeAdd(info, 'probe', metaData.$PROBHD);
   maybeAdd(info, 'bf1', metaData.$BF1);
   maybeAdd(info, 'sfo1', metaData.$SFO1);
-  maybeAdd(info, 'spectral width', metaData.$SW);
+  maybeAdd(info, 'spectralWidth', metaData.$SW);
   maybeAdd(info, 'number of points', metaData.$TD);
 
   let digitalFilterParameters = getDigitalFilterParameters(
