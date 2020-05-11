4295546b6eb799c1418e843c448f4a98e38dd4c9
Luc Patiny <luc@patiny.com>
2020-05-11 16:45:51 +0200

    chore: use median package to calculate noise

--- src/data/data1d/autoPeakPicking.js
+++ src/data/data1d/autoPeakPicking.js
@@ -1,16 +1,16 @@
 import * as SD from 'spectra-data';
 
+import median from 'ml-array-median';
 import generateID from '../utilities/generateID';
 
 export default function autoPeakPicking(datum1D, options) {
   const { minMaxRatio, maxNumberOfPeaks } = options;
   // we calculate the noise but this could be improved
-  let noise = datum1D.data.re.map((y) => Math.abs(y)).sort()[
-    Math.floor(datum1D.data.re.length / 2)
-  ];
+  let noise = median(datum1D.data.re.map((y) => Math.abs(y)));
+
   const spectrum = SD.NMR.fromXY(datum1D.data.x, datum1D.data.re);
   let peaks = spectrum.createPeaks({
-    noiseLevel: noise * 3,
+    noiseLevel: noise * 5,
     minMaxRatio: minMaxRatio, // Threshold to determine if a given peak should be considered as a noise
     realTopDetection: true,
     maxCriteria: true,
