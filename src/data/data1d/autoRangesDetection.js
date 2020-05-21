509178852e481c98e5b088848c671a3d42dac5c9
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-05-21 07:19:18 -0500

    chore(ranges): start using nmr-processing for autoRangesPicking

--- src/data/data1d/autoRangesDetection.js
+++ src/data/data1d/autoRangesDetection.js
@@ -1,4 +1,4 @@
-import * as SD from 'spectra-data';
+import { autoRangesPicking } from 'nmr-processing';
 
 export default function autoRangesDetection(datum1D, options) {
   const {
@@ -13,20 +13,26 @@ export default function autoRangesDetection(datum1D, options) {
   let noise = datum1D.data.re.map((y) => Math.abs(y)).sort()[
     Math.floor(datum1D.data.re.length / 2)
   ];
-  const spectrum = SD.NMR.fromXY(datum1D.data.x, datum1D.data.re);
-  const ranges = spectrum.createRanges({
-    noiseLevel: noise * 3,
-    minMaxRatio: minMaxRatio, // Threshold to determine if a given peak should be considered as a noise
-    realTopDetection: true,
-    maxCriteria: true,
-    smoothY: false,
-    nH,
-    compile,
-    frequencyCluster,
-    clean,
-    keepPeaks,
-    sgOptions: { windowSize: 7, polynomial: 3 },
-  });
 
+  let { frequency, nucleus } = datum1D.info;
+  let { re, x } = datum1D.data;
+  const ranges = autoRangesPicking(
+    { x, y: re },
+    {
+      noiseLevel: noise * 3,
+      minMaxRatio: minMaxRatio, // Threshold to determine if a given peak should be considered as a noise
+      realTopDetection: true,
+      maxCriteria: true,
+      smoothY: false,
+      nH,
+      compile,
+      frequency,
+      nucleus,
+      frequencyCluster,
+      clean,
+      keepPeaks,
+      sgOptions: { windowSize: 7, polynomial: 3 },
+    },
+  );
   return ranges;
 }
