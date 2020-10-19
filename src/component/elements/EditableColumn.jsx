d8f02841d4dc58465233b7ce75d95790eae95ae8
hamed <hamed.musallam@gmail.com>
2020-10-19 11:14:49 +0200

    chore: enable/disable editable column component

--- src/component/elements/EditableColumn.jsx
+++ src/component/elements/EditableColumn.jsx
@@ -1,45 +1,76 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 
 import Input from './Input';
 
-const EditableColumn = ({ onSave, value, type = 'text', style }) => {
-  const [enabled, enableEdit] = useState(false);
-  const refInput = useRef();
-  const editModeHandler = useCallback(
-    (flag, event = null) => {
-      if (event && flag === false) {
-        if (event.keyCode === 13) {
-          // when press Enter
-          onSave(event);
-          enableEdit(flag);
-        } else if (event.keyCode === 27) {
-          // when press Escape
+const EditableColumn = forwardRef(
+  ({ onSave, value, type, style, onEditStart, editStatus }, ref) => {
+    const [enabled, enableEdit] = useState();
+    const refInput = useRef();
+
+    useEffect(() => {
+      enableEdit(editStatus);
+    }, [editStatus]);
+
+    useImperativeHandle(ref, () => ({
+      startEdit: () => {
+        enableEdit(true);
+      },
+      closeEdit: () => {
+        enableEdit(false);
+      },
+    }));
+
+    const editModeHandler = useCallback(
+      (flag, event = null) => {
+        if (!flag) {
+          if (event.keyCode === 13) {
+            // when press Enter
+            onSave(event);
+            enableEdit(flag);
+          } else if (event.keyCode === 27) {
+            // when press Escape
+            enableEdit(flag);
+          }
+        } else {
+          onEditStart(event);
           enableEdit(flag);
         }
-      } else {
-        enableEdit(flag);
-      }
-    },
-    [onSave],
-  );
+      },
+      [onEditStart, onSave],
+    );
 
-  return (
-    <div
-      style={{ width: '100%', height: '100%', ...style }}
-      onDoubleClick={() => editModeHandler(true)}
-    >
-      {!enabled && value}
-      {enabled && (
-        <Input
-          enableAutoSelect={true}
-          ref={refInput}
-          value={value}
-          type={type}
-          onKeyDown={(e) => editModeHandler(false, e)}
-        />
-      )}
-    </div>
-  );
+    return (
+      <div
+        style={{ width: '100%', height: '100%', ...style }}
+        onDoubleClick={(event) => editModeHandler(true, event)}
+      >
+        {!enabled && value}
+        {enabled && (
+          <Input
+            enableAutoSelect={true}
+            ref={refInput}
+            value={value}
+            type={type}
+            onKeyDown={(e) => editModeHandler(false, e)}
+          />
+        )}
+      </div>
+    );
+  },
+);
+
+EditableColumn.defaultProps = {
+  onSave: () => null,
+  onEditStart: () => null,
+  type: 'text',
+  editStatus: false,
 };
 
 export default EditableColumn;
