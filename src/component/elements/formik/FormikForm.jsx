a63e10cac01dba6f99a8ff33cc7da2af4704e739
hamed <hamed.musallam@gmail.com>
2020-11-06 09:57:54 +0100

    refactor custom Formik components

--- src/component/elements/formik/FormikForm.jsx
+++ src/component/elements/formik/FormikForm.jsx
@@ -2,7 +2,7 @@ import { Formik } from 'formik';
 import PropTypes from 'prop-types';
 import React, { forwardRef } from 'react';
 
-const AppForm = forwardRef(
+const FormikForm = forwardRef(
   ({ initialValues, onSubmit, validationSchema, children }, ref) => {
     return (
       <Formik
@@ -17,15 +17,15 @@ const AppForm = forwardRef(
   },
 );
 
-AppForm.defaultProps = {
+FormikForm.defaultProps = {
   initialValues: {},
   onSubmit: () => null,
   validationSchema: null,
 };
-AppForm.propTypes = {
+FormikForm.propTypes = {
   initialValues: PropTypes.object,
   onSubmit: PropTypes.func,
   validationSchema: PropTypes.object,
 };
 
-export default AppForm;
+export default FormikForm;
