c6cf56159fb248d27c3977b836670508b1e649b3
hamed <hamed.musallam@gmail.com>
2020-12-17 12:15:13 +0100

    chore: order tabs and preferences by nucleus close  #734

--- src/component/utility/GroupByInfoKey.js
+++ src/component/utility/GroupByInfoKey.js
@@ -1,8 +1,21 @@
-const GroupByInfoKey = (key) => (array) =>
-  array.reduce((objectsByKeyValue, obj) => {
+function getNucleusSum(input) {
+  return input
+    .split(',')
+    .reduce((acc, i) => (acc += Number(i.replace(/\D/g, ''))), 0);
+}
+const GroupByInfoKey = (key) => (array) => {
+  const unorderedGroup = array.reduce((objectsByKeyValue, obj) => {
     const value = obj.info[key];
     objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
     return objectsByKeyValue;
   }, {});
+
+  return Object.keys(unorderedGroup)
+    .sort((a, b) => getNucleusSum(a) - getNucleusSum(b))
+    .reduce((acc, key) => {
+      acc[key] = unorderedGroup[key];
+      return acc;
+    }, {});
+};
 
 export default GroupByInfoKey;
