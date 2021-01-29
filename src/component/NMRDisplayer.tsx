b923b2f178204485630773252fca1a33be88384e
Michaël Zasso <targos@protonmail.com>
2021-01-29 21:42:27 +0100

    refactor: move compilation fully to TypeScript

--- src/component/NMRDisplayer.tsx
+++ src/component/NMRDisplayer.tsx
@@ -131,12 +131,76 @@ const containerStyles = css`
   }
 `;
 
+export interface NMRDisplayerProps {
+  data?: NMRDisplayerData;
+  docsBaseUrl?: string;
+  onDataChange?: (data: any) => void;
+  preferences?: NMRDisplayerPreferences;
+}
+
+export type NMRDisplayerPreferences = Partial<{
+  general: Partial<{
+    disableMultipletAnalysis: boolean;
+    hideSetSumFromMolecule: boolean;
+  }>;
+  panels: Partial<{
+    hideSpectraPanel: boolean;
+    hideInformationPanel: boolean;
+    hidePeaksPanel: boolean;
+    hideIntegralsPanel: boolean;
+    hideRangesPanel: boolean;
+    hideStructuresPanel: boolean;
+    hideFiltersPanel: boolean;
+    hideZonesPanel: boolean;
+    hideSummaryPanel: boolean;
+    hideMultipleSpectraAnalysisPanel: boolean;
+  }>;
+  toolBarButtons: Partial<{
+    hideZoomTool: boolean;
+    hideZoomOutTool: boolean;
+    hideImport: boolean;
+    hideExportAs: boolean;
+    hideSpectraStackAlignments: boolean;
+    hideSpectraCenterAlignments: boolean;
+    hideRealImaginary: boolean;
+    hidePeakTool: boolean;
+    hideIntegralTool: boolean;
+    hideAutoRangesTool: boolean;
+    hideZeroFillingTool: boolean;
+    hidePhaseCorrectionTool: boolean;
+    hideBaseLineCorrectionTool: boolean;
+    hideFFTTool: boolean;
+    hideMultipleSpectraAnalysisTool: boolean;
+  }>;
+}>;
+
+export interface NMRDisplayerData {
+  molecules?: Array<{ molfile: string }>;
+  spectra: Array<{
+    data?: any;
+    id?: string;
+    display: {
+      name?: string;
+    };
+    source:
+      | {
+          jcamp: string;
+        }
+      | {
+          jcampURL: string;
+        }
+      | {
+          original: any;
+        };
+  }>;
+}
+
 function NMRDisplayer({
   data: dataProp,
   onDataChange,
   docsBaseUrl,
   preferences,
-}) {
+}: NMRDisplayerProps) {
   const rootRef = useRef<HTMLDivElement>(null);
   const elementsWraperRef = useRef<HTMLDivElement>(null);
   const [show, toggle] = useToggle(false);
@@ -199,7 +263,7 @@ function NMRDisplayer({
 
   const dispatchMiddleWare = useMemo(() => {
     function dataChangeHandler(data) {
-      onDataChange(data);
+      onDataChange?.(data);
     }
     return dispatchMiddleware(dispatch, dataChangeHandler);
   }, [onDataChange]);
