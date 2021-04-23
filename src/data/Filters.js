a4a697c1615617b5bac3c74984279c067362d2a1
hamed <hamed.musallam@gmail.com>
2021-04-23 11:45:20 +0200

    chore: fix filters import

--- src/data/Filters.js
+++ src/data/Filters.js
@@ -1,13 +1,13 @@
 import * as absolute from './data1d/filter1d/absolute';
 import * as autoPhaseCorrection from './data1d/filter1d/autoPhaseCorrection';
 import * as baselineCorrection from './data1d/filter1d/baselineCorrection';
-import * as pareto from './data1d/filter1d/centerMean';
+import * as centerMean from './data1d/filter1d/centerMean';
 import * as digitalFilter from './data1d/filter1d/digitalFilter';
 import * as equallySpaced from './data1d/filter1d/equallySpaced';
 import * as fft from './data1d/filter1d/fft';
 import * as fromTo from './data1d/filter1d/fromTo';
 import * as lineBroadening from './data1d/filter1d/lineBroadening';
-import * as centerMean from './data1d/filter1d/pareto';
+import * as pareto from './data1d/filter1d/pareto';
 import * as phaseCorrection from './data1d/filter1d/phaseCorrection';
 import * as shiftX from './data1d/filter1d/shiftX';
 import * as standardDeviation from './data1d/filter1d/standardDeviation';
