63514e561f47911af1f541ff1f354611891fda6b
hamed <hamed.musallam@gmail.com>
2021-05-27 12:22:35 +0200

    chore: fix Datum1D test

--- src/data/data1d/__tests__/Datum1D.test.js
+++ src/data/data1d/__tests__/Datum1D.test.js
@@ -1,38 +1,37 @@
-// import { readFileSync } from 'fs';
-// import { join } from 'path';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-// import { Data1DManager } from '../Data1DManager';
-
-// eslint-disable-next-line jest/no-commented-out-tests
-// describe('test Datum1D', () => {
-//   let jcamp = readFileSync(
-//     join(__dirname, './data/ethylbenzene-1h.jdx'),
-//     'utf8',
-//   );
-//   let data = Data1DManager.fromJcamp(jcamp, {
-//     display: {
-//       name: 'test',
-//       color: 'red',
-//       isVisible: true,
-//       isPeaksMarkersVisible: true,
-//     },
-//     source: {
-//       jcamp: null,
-//       jcampURL: null,
-//       original: null,
-//     },
-//   });
-
-// eslint-disable-next-line jest/no-commented-out-tests
-//   it('check x, re, im', () => {
-//     expect(data.data.x).toHaveLength(16384);
-//     expect(data.data.re).toHaveLength(16384);
-//     expect(data.data.im).toHaveLength(16384);
-//   });
-// });
+import { addJcamp } from '../../SpectraManager';
 
 describe('test Datum1D', () => {
+  const jcamp = readFileSync(
+    join(__dirname, './data/ethylbenzene-1h.jdx'),
+    'utf8',
+  );
+  const spectra = [];
+
+  addJcamp(
+    spectra,
+    jcamp,
+    {
+      display: {
+        name: 'test',
+        color: 'red',
+        isVisible: true,
+        isPeaksMarkersVisible: true,
+      },
+      source: {
+        jcamp: null,
+        jcampURL: null,
+        original: null,
+      },
+    },
+    [],
+  );
+
   it('check x, re, im', () => {
-    expect(true).toStrictEqual(true);
+    expect(spectra[0].data.x).toHaveLength(16384);
+    expect(spectra[0].data.re).toHaveLength(16384);
+    expect(spectra[0].data.im).toHaveLength(16384);
   });
 });
