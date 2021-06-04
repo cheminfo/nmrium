cfa7c9a76baabe7a11eddd97711e33e620549a7d
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2021-06-04 17:30:48 +0200

    refactor: change Checkbox from jsx to tsx

---
+++ src/component/elements/CheckBox.tsx
@@ -0,0 +1,45 @@
+import { useState, useCallback, Ref, ChangeEvent } from 'react';
+
+import { forwardRefWithAs } from '../../utils';
+
+interface CheckBoxProps {
+  onChange?: (value: ChangeEvent<HTMLInputElement>) => void;
+  name?: string;
+  disabled?: boolean;
+  checked: boolean;
+}
+
+const CheckBox = forwardRefWithAs(
+  (props: CheckBoxProps, ref: Ref<HTMLInputElement>) => {
+    const {
+      checked: checkProps,
+      disabled = false,
+      name = '',
+      onChange = () => null,
+    } = props;
+
+    const [checked, setCheck] = useState<boolean>(checkProps);
+
+    const handleCheck = useCallback(
+      (e) => {
+        setCheck(!checked);
+        onChange(e);
+      },
+      [checked, onChange],
+    );
+
+    return (
+      <input
+        ref={ref}
+        type="checkbox"
+        name={name}
+        id={name}
+        onChange={handleCheck}
+        defaultChecked={checkProps}
+        disabled={disabled}
+      />
+    );
+  },
+);
+
+export default CheckBox;
