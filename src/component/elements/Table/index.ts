3d10ea23ad0816b3a0a386ff1bace6dff9b4d45f
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2021-06-04 17:32:15 +0200

    refactor: change export from jsx to tsx

---
+++ src/component/elements/Table/index.ts
@@ -0,0 +1,7 @@
+import Table from './Table';
+import TableBody from './TableBody';
+import TableCell from './TableCell';
+import TableHead from './TableHead';
+import TableRow from './TableRow';
+
+export { Table, TableHead, TableBody, TableRow, TableCell };
