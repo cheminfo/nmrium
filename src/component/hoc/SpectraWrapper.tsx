b3359f572ae304d1ceb5468cf0feb781b53199c6
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2021-06-23 17:00:08 +0200

    refactor: change component/toc from jsx to tsx

--- src/component/hoc/SpectraWrapper.tsx
+++ src/component/hoc/SpectraWrapper.tsx
@@ -2,11 +2,20 @@ import { forwardRef } from 'react';
 
 import { useChartData } from '../context/ChartContext';
 
-export default function SpectraWrapper(WrappedComponent) {
-  function Wrapper(props) {
-    const { data, activeSpectrum, activeTab, displayerMode } = useChartData();
+interface WrapperProps {
+  onTabChange?: (element: any) => void;
+  data?: any;
+  activeSpectrum?: string;
+  activeTab?: string;
+  forwardedRef?: any;
+  spectrums?: any;
+}
 
+export default function SpectraWrapper(WrappedComponent) {
+  function Wrapper(props: WrapperProps) {
+    const { data, activeSpectrum, activeTab, displayerMode } = useChartData();
     const { forwardedRef, ...rest } = props;
+
     return (
       <WrappedComponent
         {...rest}
@@ -19,7 +28,7 @@ export default function SpectraWrapper(WrappedComponent) {
     );
   }
 
-  return forwardRef((props, ref) => {
+  return forwardRef((props: WrapperProps, ref) => {
     return <Wrapper {...props} forwardedRef={ref} />;
   });
 }
