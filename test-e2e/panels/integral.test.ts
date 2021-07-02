c5484e78958fb3ca1931f611e60f19498d9988f0
hamed <hamed.musallam@gmail.com>
2021-07-02 15:33:32 +0200

    test: fix test cases - remove check for boundingBox if null - use not.toContain('NaN') instead of not.toBeNaN()

--- test-e2e/panels/integral.test.ts
+++ test-e2e/panels/integral.test.ts
@@ -9,34 +9,31 @@ test('Should Add Integral', async () => {
   await nmrium.page.click('[data-test-id="tool-integral"]');
 
   // get the bounding box {x,y,width,hegith} for the drawing area
-  const boundingBox = await containerElemment?.boundingBox();
+  const boundingBox = (await containerElemment?.boundingBox()) as BoundingBox;
 
-  // check if the returned bounding box is not null
-  if (boundingBox) {
-    const cursorStartX = boundingBox.x + boundingBox.width / 2;
-    const cursorStartY = boundingBox.y + boundingBox.height / 2;
-    //move the custose to the center of the draw area
-    await nmrium.page.mouse.move(cursorStartX, cursorStartY, { steps: 15 });
-    // press shift + left mouse down
-    await nmrium.page.keyboard.down('Shift');
-    await nmrium.page.mouse.down({ button: 'left' });
+  const cursorStartX = boundingBox.x + boundingBox.width / 2;
+  const cursorStartY = boundingBox.y + boundingBox.height / 2;
+  //move the custose to the center of the draw area
+  await nmrium.page.mouse.move(cursorStartX, cursorStartY, { steps: 15 });
+  // press shift + left mouse down
+  await nmrium.page.keyboard.down('Shift');
+  await nmrium.page.mouse.down({ button: 'left' });
 
-    //move the cusrose to new postion and still hold shift and left mouse button
-    await nmrium.page.mouse.move(cursorStartX + 100, cursorStartY, {
-      steps: 15,
-    });
+  //move the cusrose to new postion and still hold shift and left mouse button
+  await nmrium.page.mouse.move(cursorStartX + 100, cursorStartY, {
+    steps: 15,
+  });
 
-    //release shift and left mouse button
-    await nmrium.page.keyboard.up('Shift');
-    await nmrium.page.mouse.up({ button: 'left' });
+  //release shift and left mouse button
+  await nmrium.page.keyboard.up('Shift');
+  await nmrium.page.mouse.up({ button: 'left' });
 
-    //should have integral with at least 1000 point
-    await nmrium.page.waitForSelector('g.integrals');
-    const path = (await nmrium.page.getAttribute(
-      'g.integrals path',
-      'd',
-    )) as string;
-    expect(path.length).toBeGreaterThan(1000);
-    expect(path).not.toContain('NaN');
-  }
+  //should have integral with at least 1000 point
+  await nmrium.page.waitForSelector('g.integrals');
+  const path = (await nmrium.page.getAttribute(
+    'g.integrals path',
+    'd',
+  )) as string;
+  expect(path.length).toBeGreaterThan(1000);
+  expect(path).not.toContain('NaN');
 });
--- test-e2e/panels/spectra.test.ts
+++ test-e2e/panels/spectra.test.ts
@@ -42,34 +42,32 @@ test('Should Zoom', async () => {
   await nmrium.open1D();
   const containerElemment = await nmrium.page.waitForSelector('.Pane1');
 
-  const boundingBox = await containerElemment?.boundingBox();
+  const boundingBox = (await containerElemment?.boundingBox()) as BoundingBox;
 
-  if (boundingBox) {
-    const cursorStartX = boundingBox.x + boundingBox.width / 2;
-    const cursorStartY = boundingBox.y + boundingBox.height / 2;
-    const previousPath = (await nmrium.page.getAttribute(
-      ':nth-match([data-test-id="spectrum-line"],1)',
-      'd',
-    )) as string;
+  const cursorStartX = boundingBox.x + boundingBox.width / 2;
+  const cursorStartY = boundingBox.y + boundingBox.height / 2;
+  const previousPath = (await nmrium.page.getAttribute(
+    ':nth-match([data-test-id="spectrum-line"],1)',
+    'd',
+  )) as string;
 
-    await nmrium.page.mouse.move(cursorStartX, cursorStartY, { steps: 15 });
-    await nmrium.page.mouse.down();
-    await nmrium.page.mouse.move(cursorStartX + 100, cursorStartY, {
-      steps: 15,
-    });
-    await nmrium.page.mouse.up();
+  await nmrium.page.mouse.move(cursorStartX, cursorStartY, { steps: 15 });
+  await nmrium.page.mouse.down();
+  await nmrium.page.mouse.move(cursorStartX + 100, cursorStartY, {
+    steps: 15,
+  });
+  await nmrium.page.mouse.up();
 
-    await nmrium.page.waitForTimeout(1000);
+  await nmrium.page.waitForTimeout(1000);
 
-    const path = (await nmrium.page.getAttribute(
-      ':nth-match([data-test-id="spectrum-line"],1)',
-      'd',
-    )) as string;
+  const path = (await nmrium.page.getAttribute(
+    ':nth-match([data-test-id="spectrum-line"],1)',
+    'd',
+  )) as string;
 
-    expect(path.length).toBeGreaterThan(1000);
-    expect(path).not.toBeNaN();
-    expect(path).not.toMatch(previousPath);
-  }
+  expect(path.length).toBeGreaterThan(1000);
+  expect(path).not.toContain('NaN');
+  expect(path).not.toMatch(previousPath);
 });
 
 test('Check change spectrum color, Should be white', async () => {
@@ -82,18 +80,16 @@ test('Check change spectrum color, Should be white', async () => {
   await nmrium.page.click(':nth-match([data-test-id="color-indicator"], 1)');
 
   const colorSelector = await nmrium.page.$('.sketch-picker > div');
-  const boundingBox = await colorSelector?.boundingBox();
+  const boundingBox = (await colorSelector?.boundingBox()) as BoundingBox;
 
-  if (boundingBox) {
-    await nmrium.page.mouse.click(boundingBox.x, boundingBox.y, {
-      button: 'left',
-    });
-    await nmrium.page.waitForTimeout(1000);
+  await nmrium.page.mouse.click(boundingBox.x, boundingBox.y, {
+    button: 'left',
+  });
+  await nmrium.page.waitForTimeout(1000);
 
-    const color = (await nmrium.page.getAttribute(
-      ':nth-match([data-test-id="spectrum-line"],1)',
-      'stroke',
-    )) as string;
-    expect(color).toMatch('#ffffffff');
-  }
+  const color = (await nmrium.page.getAttribute(
+    ':nth-match([data-test-id="spectrum-line"],1)',
+    'stroke',
+  )) as string;
+  expect(color).toMatch('#ffffffff');
 });
