b5569f73a15b40c17387a05004dcf713ec052134
hamed <hamed.musallam@gmail.com>
2021-07-13 15:27:31 +0200

    test: use selectRange utility in test add integral

--- test-e2e/panels/integral.test.ts
+++ test-e2e/panels/integral.test.ts
@@ -1,4 +1,5 @@
 import NmriumPage from '../NmriumPage';
+import { selectRange } from '../utilities/selectRange';
 
 test('Should Add Integral', async () => {
   const nmrium = await NmriumPage.create();
@@ -9,27 +10,11 @@ test('Should Add Integral', async () => {
   //select integral tool
   await nmrium.page.click('[data-test-id="tool-integral"]');
 
-  // get the bounding box {x,y,width,hegith} for the drawing area
-  const boundingBox = (await containerElemment?.boundingBox()) as BoundingBox;
-
-  const cursorStartX = boundingBox.x + boundingBox.width / 2;
-  const cursorStartY = boundingBox.y + boundingBox.height / 2;
-  //move the custose to the center of the draw area
-  await nmrium.page.mouse.move(cursorStartX, cursorStartY, { steps: 15 });
-  // press shift + left mouse down
-  await nmrium.page.keyboard.down('Shift');
-  await nmrium.page.mouse.down({ button: 'left' });
-
-  //move the cusrose to new postion and still hold shift and left mouse button
-  await nmrium.page.mouse.move(cursorStartX + 100, cursorStartY, {
-    steps: 15,
+  await selectRange(nmrium, containerElemment, {
+    axis: 'X',
+    startX: 50,
+    endX: 70,
   });
-
-  //release shift and left mouse button
-  await nmrium.page.keyboard.up('Shift');
-  await nmrium.page.mouse.up({ button: 'left' });
-
-  await nmrium.page.waitForTimeout(1000);
 
   //should have integral with at least 1000 point
   const path = (await nmrium.page.getAttribute(
