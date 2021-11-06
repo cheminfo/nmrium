e1537ef002ade0a36e368c1d57694977e3ab439b
michaelwenk <michael.wenk@uni-jena.de>
2021-11-06 12:08:09 +0100

    chore: removed unused css content

--- src/component/panels/header/DefaultPanelHeader.tsx
+++ src/component/panels/header/DefaultPanelHeader.tsx
@@ -7,16 +7,15 @@ import ToggleButton from '../../elements/ToggleButton';
 import ToolTip from '../../elements/ToolTip/ToolTip';
 
 const styles = css`
-
   display: flex;
   flex-direction: row;
   border-bottom: 0.55px solid rgb(240, 240, 240);
   padding: 0px 5px;
 
-  .left-container{
+  .left-container {
     display: flex;
     flex-direction: row;
-    flex:1;  
+    flex: 1;
   }
   button {
     background-color: transparent;
@@ -24,10 +23,10 @@ const styles = css`
     padding: 5px;
   }
 
-  button:disabled{
-    opacity:0.6;
+  button:disabled {
+    opacity: 0.6;
   }
-  
+
   .counter-label {
     margin: 0;
     text-align: right;
@@ -35,19 +34,6 @@ const styles = css`
     padding: 0px 10px;
     white-space: nowrap;
   }
-
-  filter-button{
-    borderRadius: 5px;
-    marginTop: 3px;
-    marginLeft: 5px;
-    color: black;
-    backgroundColor: transparent;
-    border: none;
-    height: 16px;
-    width: 18px;
-    fontSize: 12px;
-    padding: 0;
-  },
 `;
 
 interface DefaultPanelHeaderProps {
