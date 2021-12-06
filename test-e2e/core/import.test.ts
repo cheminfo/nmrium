73d9ee7c5dd123b5bc91165b42aa76b255dbb606
hamed <hamed.musallam@gmail.com>
2021-12-06 16:23:49 +0100

    test: add test case for migration from version 1 to latest

--- test-e2e/core/import.test.ts
+++ test-e2e/core/import.test.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 
 import NmriumPage from '../NmriumPage';
 
-test('should load and migrate .nmrium data from version 0 to version 1', async ({
+test('should load and migrate .nmrium data from version 0 to latest version', async ({
   page,
 }) => {
   const nmrium = await NmriumPage.create(page);
@@ -14,6 +14,22 @@ test('should load and migrate .nmrium data from version 0 to version 1', async (
     nmrium.page.click('_react=MenuItem[id = "importFile"]'),
   ]);
   await fileChooser.setFiles('test-e2e/data/1h-version-0.nmrium');
+
+  await nmrium.page.click('_react=InternalTab[tabid = "1H,1H"]');
+});
+
+test('should load and migrate .nmrium data from version 1 to latest version', async ({
+  page,
+}) => {
+  const nmrium = await NmriumPage.create(page);
+
+  await nmrium.clickTool('menu-Import');
+
+  const [fileChooser] = await Promise.all([
+    nmrium.page.waitForEvent('filechooser'),
+    nmrium.page.click('_react=MenuItem[id = "importFile"]'),
+  ]);
+  await fileChooser.setFiles('test-e2e/data/1h-version-1-datasource.nmrium');
 
   await nmrium.page.click('_react=InternalTab[tabid = "1H,1H"]');
 });
