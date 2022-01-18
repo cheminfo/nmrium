85fb8a10aeb715db71e25ce9e3a687a2e2702e16
michaelwenk <michael.wenk@uni-jena.de>
2022-01-18 18:01:54 +0100

    fix: eslint error

--- src/data/types/data2d/Signal2D.ts
+++ src/data/types/data2d/Signal2D.ts
@@ -1,6 +1,7 @@
 import PathLength from './PathLength';
 import { NMRSignal2D } from 'nmr-processing';
 import { Signal2DAxisData } from './Signal2DAxisData';
+
 export interface Signal2D extends Omit<NMRSignal2D, 'id' | 'x' | 'y'> {
   id: string;
   // peaks: any;
