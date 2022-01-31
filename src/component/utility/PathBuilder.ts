382ddc95a32d42c3ff9c5144135ba6ae3c2d9eea
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2022-01-31 15:24:12 +0100

    refactor: remove unused internal function to format string

--- src/component/utility/PathBuilder.ts
+++ src/component/utility/PathBuilder.ts
@@ -2,21 +2,14 @@ export class PathBuilder {
   private array: Array<string> = [];
 
   public moveTo(x: number, y: number) {
-    this.array.push(this.format(`M ${x} ${y}`));
+    this.array.push(`M ${x} ${y}`);
   }
 
   public lineTo(x: number, y: number) {
-    this.array.push(this.format(`L ${x} ${y}`));
+    this.array.push(`L ${x} ${y}`);
   }
 
   public toString() {
     return this.array.join(' ');
-  }
-
-  /**
-   * Add space before and after the string
-   */
-  private format(element: string) {
-    return ` ${element} `;
   }
 }
