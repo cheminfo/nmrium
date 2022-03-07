1ad068ca4e6429306b67a6cfca35bc00cc1e96eb
hamed-musallam <hamed.musallam@gmail.com>
2022-03-07 14:32:53 +0100

    chore: fix code format

--- src/component/hooks/useXScale.ts
+++ src/component/hooks/useXScale.ts
@@ -1,8 +1,8 @@
 import { ScaleLinear } from 'd3';
-import { useMemo } from "react";
+import { useMemo } from 'react';
 
-import { getXScale } from "../1d/utilities/scale";
-import { useChartData } from "../context/ChartContext";
+import { getXScale } from '../1d/utilities/scale';
+import { useChartData } from '../context/ChartContext';
 
 export function useXScale(): ScaleLinear<any, any, never> {
   const { width, margin, xDomains, xDomain, mode, activeSpectrum } =
