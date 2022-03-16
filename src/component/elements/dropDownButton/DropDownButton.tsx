2016c99198e4397140823c94ea5945c9c74f9430
hamed-musallam <hamed.musallam@gmail.com>
2022-03-16 12:26:26 +0100

    refactor: DropDownButton component

--- src/component/elements/dropDownButton/DropDownButton.tsx
+++ src/component/elements/dropDownButton/DropDownButton.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 import { FaEllipsisH } from 'react-icons/fa';
 
 import { useGlobal } from '../../context/GlobalContext';
@@ -15,28 +15,49 @@ const styles = css`
   border: 0.55px solid lightgray;
   font-size: 10px;
 
-  button {
-    text-transform: Capitalize;
-  }
+  // button {
+  //   text-transform: Capitalize;
+  // }
 `;
 
-function defaultFormatSelectedValue(val) {
-  return val;
+export interface DropDownListItem {
+  key: string;
+  label: string;
+  index?: number;
 }
 
-function DropDownButton(props) {
+export interface DropDownListProps {
+  data: Array<DropDownListItem>;
+  onSelect: (index: number) => void;
+  renderItem?: ((item: DropDownListItem) => ReactNode) | null;
+}
+
+interface DropDownButtonProps extends Omit<DropDownListProps, 'onSelect'> {
+  selectedKey?: string;
+  onSelect?: (item: DropDownListItem) => void;
+  formatSelectedValue?: (Item: DropDownListItem) => string;
+}
+
+function DropDownButton(props: DropDownButtonProps) {
   const {
-    data = null,
+    data,
     selectedKey,
     onSelect,
-    formatSelectedValue = defaultFormatSelectedValue,
+    formatSelectedValue = (item) => item.label,
     renderItem = null,
   } = props;
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(selectedKey);
+  const [item, setItem] = useState<DropDownListItem | null>();
   const { rootRef } = useGlobal();
 
   const drop = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (selectedKey) {
+      const item = data.find((i) => i.key === selectedKey) || null;
+      setItem(item);
+    }
+  }, [selectedKey, data]);
 
   const handleClick = useCallback(
     (e) => {
@@ -61,8 +82,8 @@ function DropDownButton(props) {
 
   const selectHandler = useCallback(
     (index) => {
-      setValue(data[index].label);
-      onSelect(data[index].key);
+      setItem(data[index]);
+      onSelect?.(data[index]);
     },
     [data, onSelect],
   );
@@ -76,7 +97,7 @@ function DropDownButton(props) {
           event.stopPropagation();
         }}
       >
-        {!value ? <FaEllipsisH /> : formatSelectedValue(value)}
+        {!item ? <FaEllipsisH /> : formatSelectedValue(item)}
       </button>
       {open && (
         <DropDownList
