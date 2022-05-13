60761406ddf4cab58b3eddc66be9537e15908b3e
hamed-musallam <hamed.musallam@gmail.com>
2022-05-13 11:35:20 +0200

    feat: create hook for panel preferences

---
+++ src/component/hooks/usePanelPerferences.ts
@@ -0,0 +1,104 @@
+import { useMemo } from 'react';
+
+import { usePreferences } from '../context/PreferencesContext';
+import {
+  integralDefaultValues,
+  peaksDefaultValues,
+  zoneDefaultValues,
+  getRangeDefaultValues,
+  databaseDefaultValues,
+} from '../panels/extra/preferences/defaultValues';
+import { getValue } from '../utility/LocalStorage';
+import {
+  DatabasePanelPreferences,
+  IntegralsPanelPreferences,
+  PeaksPanelPreferences,
+  RangesPanelPreferences,
+  Workspace,
+  ZonesPanelPreferences,
+} from '../workspaces/Workspace';
+
+type Panel = 'peaks' | 'integrals' | 'zones' | 'ranges' | 'database';
+
+function getDefaultPreferences(panelKey: Panel, nucleus: string) {
+  switch (panelKey) {
+    case 'peaks':
+      return peaksDefaultValues;
+    case 'integrals':
+      return integralDefaultValues;
+    case 'ranges':
+      return getRangeDefaultValues(nucleus);
+    case 'zones':
+      return zoneDefaultValues;
+    case 'database':
+      return databaseDefaultValues;
+
+    default:
+      return {};
+  }
+}
+
+function getKeyPath(panelKey: Panel, nucleus: string) {
+  if (panelKey === 'database') {
+    return `formatting.panels.${panelKey}`;
+  }
+
+  return `formatting.panels.${panelKey}.[${nucleus}]`;
+}
+
+interface PreferencesReturnType {
+  peaks: PeaksPanelPreferences;
+  integrals: IntegralsPanelPreferences;
+  zones: ZonesPanelPreferences;
+  ranges: RangesPanelPreferences;
+  database: DatabasePanelPreferences;
+}
+
+export function usePanelPreferences<T extends Panel>(
+  panelKey: T,
+  nucleus: string,
+): PreferencesReturnType[T] {
+  const { current } = usePreferences();
+
+  return useMemo(
+    () =>
+      getValue(
+        current,
+        getKeyPath(panelKey, nucleus),
+        getDefaultPreferences(panelKey, nucleus),
+      ),
+    [current, nucleus, panelKey],
+  );
+}
+export function getPanelPreferences(
+  preferences: Workspace,
+  panelKey: Panel,
+  nucleus: string,
+) {
+  return getValue(
+    preferences,
+    getKeyPath(panelKey, nucleus),
+    getDefaultPreferences(panelKey, nucleus),
+  );
+}
+
+export function usePanelPreferencesByNuclei<T extends Panel>(
+  panelKey: T,
+  nuclei: string[],
+): Record<string, PreferencesReturnType[T]> {
+  const { current } = usePreferences();
+
+  return useMemo(
+    () =>
+      nuclei.reduce((acc, nucleusLabel) => {
+        acc[nucleusLabel] = getPanelPreferences(
+          current,
+          panelKey,
+          nucleusLabel,
+        );
+        return acc;
+      }, {}),
+
+    [current, nuclei, panelKey],
+  );
+}
