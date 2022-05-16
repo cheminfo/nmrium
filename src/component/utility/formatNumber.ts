0963d8913c68e0f7c2675715a69ffe35d83a76b2
hamed-musallam <hamed.musallam@gmail.com>
2022-05-16 12:57:49 +0200

    refactor: rename formatNumber

--- src/component/utility/formatNumber.ts
+++ src/component/utility/formatNumber.ts
@@ -1,11 +1,13 @@
 import Numeral from 'numeral';
 
-export function formatNumber(value, format, prefix = '', suffix = '') {
+function formatNumber(value, format, prefix = '', suffix = '') {
   return prefix + Numeral(value).format(format) + suffix;
 }
 
-export function getNumberOfDecimals(value: number | string) {
+function getNumberOfDecimals(value: number | string) {
   value = String(value).trim();
   const lastIndex = value.lastIndexOf('.');
   return lastIndex > 0 ? value.substr(lastIndex).split('').length - 1 : 0;
 }
+
+export { formatNumber, getNumberOfDecimals };
