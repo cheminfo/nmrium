6cd7e00b4888cfd556a1c5a6cd8fc8fe7aee730d
hamed-musallam <hamed.musallam@gmail.com>
2022-05-25 13:44:19 +0200

    chore: create find spectrum helper

---
+++ src/component/reducer/helper/getSpectrum.ts
@@ -0,0 +1,26 @@
+import { Draft } from 'immer';
+
+import { State } from '../Reducer';
+
+export function getSpectrum(state: Draft<State>);
+export function getSpectrum(state: Draft<State>, index: number);
+export function getSpectrum(state: Draft<State>, id: string);
+
+export function getSpectrum(state: Draft<State>, value?: number | string) {
+  if (value === undefined) {
+    if (state.activeSpectrum?.id) {
+      const index = state.activeSpectrum.index;
+      return state.data[index];
+    }
+  }
+
+  if (typeof value === 'number') {
+    return state.data[value] || undefined;
+  }
+
+  if (typeof value === 'string') {
+    return state.data.find((spectrum) => spectrum.id === value);
+  }
+
+  return;
+}
