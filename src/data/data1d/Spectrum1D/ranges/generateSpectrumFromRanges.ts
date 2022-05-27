0f218c34a795cece0116620a74e7f06558cb1482
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-05-26 21:37:25 -0500

    feat: update nmr-processing to 8.1.0

--- src/data/data1d/Spectrum1D/ranges/generateSpectrumFromRanges.ts
+++ src/data/data1d/Spectrum1D/ranges/generateSpectrumFromRanges.ts
@@ -1,19 +1,10 @@
-import { NMRRange, rangesToXY } from 'nmr-processing';
+import { rangesToXY } from 'nmr-processing';
 
 import { UsedColors } from '../../../../types/UsedColors';
 import { Datum1D, Range } from '../../../types/data1d';
 import { initiateDatum1D } from '../initiateDatum1D';
 
 import { mapRanges } from './mapRanges';
-
-const defaultFromTo = (nucleus: string) => {
-  switch (nucleus.toUpperCase()) {
-    case '13C':
-      return { from: -5, to: 206 };
-    default:
-      return { from: -0.5, to: 10.5 };
-  }
-};
 
 export function generateSpectrumFromRanges(
   ranges: Range[],
@@ -29,7 +20,7 @@ export function generateSpectrumFromRanges(
   const { nucleus, solvent, name = null, frequency = 400 } = info;
 
   const { x, y } = rangesToXY(ranges, {
-    ...getFromTo(ranges, defaultFromTo(nucleus)),
+    nucleus,
     frequency,
     nbPoints: 2 ** 17,
   });
@@ -52,20 +43,4 @@ export function generateSpectrumFromRanges(
   datum.ranges.values = mapRanges(ranges, datum);
 
   return datum;
-}
-
-function getFromTo(ranges: NMRRange[], fromTo: { from: number; to: number }) {
-  let from = Number.MAX_SAFE_INTEGER;
-  let to = Number.MIN_SAFE_INTEGER;
-  for (const range of ranges) {
-    for (const signal of range.signals || []) {
-      if (from > signal.delta) from = signal.delta;
-      if (to < signal.delta) to = signal.delta;
-    }
-  }
-
-  return {
-    from: Math.min(from - 0.5, fromTo.from),
-    to: Math.max(to + 0.5, fromTo.to),
-  };
 }
