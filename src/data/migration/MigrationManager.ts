829eba8ea83bdd364baa87fdc241f0df06f4efbc
hamed-musallam <hamed.musallam@gmail.com>
2022-07-27 12:21:53 +0200

    feat: migrate .nmrium  from version 2 to version 3

--- src/data/migration/MigrationManager.ts
+++ src/data/migration/MigrationManager.ts
@@ -1,7 +1,8 @@
 import migrateToVersion1 from './migrateToVersion1';
 import migrateToVersion2 from './migrateToVersion2';
+import migrateToVersion3 from './migrateToVersion3';
 
-export const CURRENT_EXPORT_VERSION = 2;
+export const CURRENT_EXPORT_VERSION = 3;
 
 function migrationPipe(functions: ((data: any) => any)[]) {
   return (input: any) => functions.reduce((input, func) => func(input), input);
@@ -11,6 +12,7 @@ export function migrate(data: any): any {
   let migrationsFuncs: ((data: any) => any)[] = [
     migrateToVersion1,
     migrateToVersion2,
+    migrateToVersion3,
   ];
   let index = data?.version || 0;
 
