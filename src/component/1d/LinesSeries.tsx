784a793e30f429392eeb502b70b16efe415ddbbd
hamed-musallam <hamed.musallam@gmail.com>
2022-07-28 15:21:56 +0200

    feat: move selected spectrum to front

--- src/component/1d/LinesSeries.tsx
+++ src/component/1d/LinesSeries.tsx
@@ -4,16 +4,21 @@ import { useChartData } from '../context/ChartContext';
 import Line from './Line';
 
 function LinesSeries() {
-  const { data, tempData, displayerKey, xDomains } = useChartData();
-  const _data = tempData ? tempData : data;
+  const { data, tempData, displayerKey, xDomains, activeSpectrum } =
+    useChartData();
+  let _data = tempData ? tempData : data;
 
   return (
     <g clipPath={`url(#${displayerKey}clip-chart-1d)`} className="spectrums">
       {_data
         ?.filter((d) => d.display.isVisible === true && xDomains[d.id])
         .map((d, i) => (
-          <Line key={d.id} {...d} data={get1DDataXY(d)} index={i} />
+          <g key={d.id} id={d.id}>
+            <Line {...d} data={get1DDataXY(d)} index={i} />
+          </g>
         ))}
+
+      {activeSpectrum?.id && <use href={`#${activeSpectrum?.id}`} />}
     </g>
   );
 }
