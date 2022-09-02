f655b53bbd3d0e2255023d4364f3ee2b1310658e
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-09-02 09:54:56 -0500

    chore: add first points to the end of the spectra

--- src/data/data1d/filter1d/digitalFilter.ts
+++ src/data/data1d/filter1d/digitalFilter.ts
@@ -1,3 +1,4 @@
+import { xAdd } from 'ml-spectra-processing';
 import { Datum1D } from '../../types/data1d/Datum1D';
 
 export const id = 'digitalFilter';
@@ -19,15 +20,21 @@ export function apply(datum1D: Datum1D, options: any = {}) {
 
   let pointsToShift = Math.floor(digitalFilterValue);
 
-  const skip = 0;
-  pointsToShift += 0;
+  const skip = 0; //Math.floor(digitalFilterValue + 2);
+  // pointsToShift = ; //Math.max(skip - 6, 0);
 
   const newRe = new Float64Array(re.length);
   const newIm = new Float64Array(im.length);
   newRe.set(re.slice(pointsToShift));
-  newRe.set(re.slice(skip, pointsToShift), re.length - pointsToShift);
+  newRe.set(
+    xAdd(re.slice(skip, pointsToShift), re.slice(re.length - pointsToShift)),
+    re.length - pointsToShift,
+  );
   newIm.set(im.slice(pointsToShift));
-  newIm.set(im.slice(skip, pointsToShift), im.length - pointsToShift);
+  newIm.set(
+    xAdd(im.slice(skip, pointsToShift), im.slice(re.length - pointsToShift)),
+    im.length - pointsToShift,
+  );
 
   datum1D.data.re = newRe;
   datum1D.data.im = newIm;
