ab16297c4366f0689f3cc14d7671a44f1d23fdf5
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-10-02 16:19:50 -0500

    chore: use pre-release nmr-load-save

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ const viewportOverride: ViewportSize = {
 const config: PlaywrightTestConfig = {
   testDir: 'test-e2e',
   retries: 0,
-  workers: 1,
+  workers: 2,
   forbidOnly: !!process.env.CI,
   use: {
     headless: true,
