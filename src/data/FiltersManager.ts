6e4d71db46fae9b2b383a2133816aa30fba6ebf6
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-10-04 20:55:53 +0100

    fix: eslint errors

--- src/data/FiltersManager.ts
+++ src/data/FiltersManager.ts
@@ -25,7 +25,6 @@ function applyFilter(datum, filters: any[] = []) {
     };
     const previousFilter = lookupForFilter(datum, filter.name);
     if (previousFilter) {
-      // todo: remove reduce
       const reduceResult = Filters[filter.name].reduce(
         previousFilter.value,
         filterOption.value,
