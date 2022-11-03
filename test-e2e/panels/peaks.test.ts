5ca1ec1546aa268bde04cb304695de66e44018dc
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-11-03 13:31:49 +0100

    test: processed spectra

--- test-e2e/panels/peaks.test.ts
+++ test-e2e/panels/peaks.test.ts
@@ -107,3 +107,60 @@ test('Automatic peak picking should work', async ({ page }) => {
 
   await expect(nmrium.page.locator('_react=PeakAnnotation')).toHaveCount(50);
 });
+test('Processed spectra peaks', async ({ page }) => {
+  const nmrium = await NmriumPage.create(page);
+  await test.step('Open Processed 13C Spectrum', async () => {
+    await nmrium.page.click('li >> text=General');
+    await nmrium.page.click('li >> text=Processed 13C Spectrum');
+
+    // wait specturm to load
+    await expect(nmrium.page.locator('#nmrSVG')).toBeVisible();
+  });
+  await test.step('Check peaks', async () => {
+    //Close spectra panel
+    await nmrium.clickPanel('Spectra');
+    //Open peaks panel
+    await nmrium.clickPanel('Peaks');
+
+    const peaks = nmrium.page.locator('_react=PeaksTable >> tbody >> tr');
+    await expect(peaks).toHaveCount(16);
+
+    const peaksData = [
+      { p: '26.93', intensity: '2304124678.90' },
+      { p: '28.58', intensity: '2220075572.10' },
+      { p: '36.30', intensity: '2169790333.08' },
+      { p: '50.39', intensity: '2029514468.85' },
+      { p: '53.66', intensity: '2053846103.58' },
+    ];
+    for (const [i, { p, intensity }] of peaksData.entries()) {
+      const peak = peaks.nth(i);
+      await expect(peak).toBeVisible();
+      await expect(peak).toContainText(p);
+      await expect(peak).toContainText(intensity);
+    }
+  });
+});
+test('Check no negative peaks in processed spectra', async ({ page }) => {
+  const nmrium = await NmriumPage.create(page);
+  await test.step('Open 13c spectrum', async () => {
+    await nmrium.page.click('li >> text=General');
+    await nmrium.page.click('li >> text=Processed 13C Spectrum');
+
+    // wait specturm to load
+    await expect(nmrium.page.locator('#nmrSVG')).toBeVisible();
+  });
+  await test.step('Check negative peaks', async () => {
+    //Close spectra panel
+    await nmrium.clickPanel('Spectra');
+    //Open peaks panel
+    await nmrium.clickPanel('Peaks');
+    const peaks = nmrium.page.locator('_react=PeaksTable >> tbody >> tr');
+
+    for (let i = 0; i < 16; i++) {
+      const peak = peaks.nth(i);
+      expect(await peak.locator('td >> nth=2').textContent()).not.toContain(
+        '-',
+      );
+    }
+  });
+});
