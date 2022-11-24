a32b262dda556fe288508dc573e6df25d2e63e02
Michaël Zasso <targos@protonmail.com>
2022-11-24 17:18:43 +0100

    test: fix e2e test with local dev server
    Event count is different in React dev mode.

--- test-e2e/core/basic.test.ts
+++ test-e2e/core/basic.test.ts
@@ -41,8 +41,8 @@ test('check callbacks count', async ({ page }) => {
   const dataCount = nmrium.page.locator('[data-test-id="data-count"]');
   const viewCount = nmrium.page.locator('[data-test-id="view-count"]');
 
-  await expect(dataCount).toContainText('3');
-  await expect(viewCount).toContainText('3');
+  await expect(dataCount).toContainText(/3|4/);
+  await expect(viewCount).toContainText(/3|4/);
 
   //switch to 1d
   await nmrium.page.click('_react=Tab[tabid="1H"]');
@@ -55,8 +55,8 @@ test('check callbacks count', async ({ page }) => {
   expect(path.length).toBeGreaterThan(1000);
   expect(path).not.toContain('NaN');
 
-  await expect(dataCount).toContainText('3');
-  await expect(viewCount).toContainText('5');
+  await expect(dataCount).toContainText(/3|4/);
+  await expect(viewCount).toContainText(/5|6/);
 
   const spectrumLineLocator = nmrium.page.locator(
     'data-test-id=spectrum-line >> nth=0',
