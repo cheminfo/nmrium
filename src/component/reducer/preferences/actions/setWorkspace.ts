5bfb7eb6f4f729918f08f58f3f66ead59928ab41
hamed-musallam <hamed.musallam@gmail.com>
2022-12-07 12:37:52 +0100

    feat: read view and settings from nmrium file

--- src/component/reducer/preferences/actions/setWorkspace.ts
+++ src/component/reducer/preferences/actions/setWorkspace.ts
@@ -1,18 +1,31 @@
 import { Draft } from 'immer';
+import lodashMerge from 'lodash/merge';
 
-import { PreferencesState, WorkspaceAction } from '../preferencesReducer';
+import { workspaceDefaultProperties } from '../../../workspaces/workspaceDefaultProperties';
+import { PreferencesState, SetWorkspaceAction } from '../preferencesReducer';
 import { getPreferencesByWorkspace } from '../utilities/getPreferencesByWorkspace';
 
 export function setWorkspace(
   draft: Draft<PreferencesState>,
-  action: WorkspaceAction,
+  action: SetWorkspaceAction,
 ) {
-  const workspaceKey = action.payload.workspace;
-  if (!draft.workspaces[workspaceKey]) {
-    draft.workspaces[workspaceKey] = getPreferencesByWorkspace(
-      workspaceKey,
-      draft.customWorkspaces,
+  if (action.payload.workspaceSource === 'any') {
+    const workspaceKey = action.payload.workspace;
+    if (!draft.workspaces[workspaceKey]) {
+      draft.workspaces[workspaceKey] = getPreferencesByWorkspace(
+        workspaceKey,
+        draft.customWorkspaces,
+      );
+    }
+    draft.workspace.current = workspaceKey;
+  } else if (action.payload.workspaceSource === 'nmriumFile') {
+    const data: any = lodashMerge(
+      {},
+      workspaceDefaultProperties,
+      action.payload.data,
+      { label: 'NMRium File' },
     );
+    delete data.version;
+    draft.workspaces[draft.workspacesTempKeys.nmriumWorkspaceKey] = data;
   }
-  draft.workspace.current = workspaceKey;
 }
