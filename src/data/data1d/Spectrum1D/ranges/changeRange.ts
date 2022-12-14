822e46729403a4871d0c49906a1bd32ba3fa84ae
hamed-musallam <hamed.musallam@gmail.com>
2022-12-14 18:01:13 +0100

    feat: redetect signal when resizing the range
    close #1880

--- src/data/data1d/Spectrum1D/ranges/changeRange.ts
+++ src/data/data1d/Spectrum1D/ranges/changeRange.ts
@@ -1,7 +1,9 @@
+import { v4 } from '@lukeed/uuid';
 import { xyIntegration } from 'ml-spectra-processing';
 
 import { Datum1D, Range } from '../../../types/data1d';
 
+import detectSignal from './detectSignal';
 import { updateRangesRelativeValues } from './updateRangesRelativeValues';
 
 export function changeRange(datum: Datum1D, range: Range) {
@@ -11,6 +13,8 @@ export function changeRange(datum: Datum1D, range: Range) {
   const index = datum.ranges.values.findIndex((i) => i.id === id);
   const absolute = xyIntegration({ x, y: re }, { from, to, reverse: true });
 
+  const signal = detectSignal(x, re, from, to, datum.info.originFrequency);
+
   if (index !== -1) {
     datum.ranges.values[index] = {
       ...datum.ranges.values[index],
@@ -18,6 +22,17 @@ export function changeRange(datum: Datum1D, range: Range) {
       originTo: to,
       ...range,
       absolute,
+      signals: [
+        {
+          id: v4(),
+          ...(signal || {
+            multiplicity: 's',
+            kind: 'signal',
+            delta: 0,
+            js: [],
+          }),
+        },
+      ],
     };
     updateRangesRelativeValues(datum);
   }
