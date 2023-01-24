b2aee3b3257d81417101f0f40b1c1521bdc1df07
hamed-musallam <hamed.musallam@gmail.com>
2023-01-24 16:37:31 +0100

    refactor: enable/disable automatic assignments

--- test-e2e/panels/autoassignment.test.ts
+++ test-e2e/panels/autoassignment.test.ts
@@ -33,12 +33,6 @@ test('automatic assignment panel', async ({ page }) => {
       '_react=AutomaticAssignment >> _react=SpectraAutomaticPickingButton',
     );
 
-    await nmrium.page
-      .locator(
-        'text=Automatic Ranges/Zones detection for all spectra in progress',
-      )
-      .waitFor({ state: 'hidden' });
-
     // Wait for auto range to be applied.
     await nmrium.page
       .locator('_react=Range >> text=3.21')
@@ -49,9 +43,10 @@ test('automatic assignment panel', async ({ page }) => {
     );
 
     // Wait for auto assignments process completed.
-    await nmrium.page
-      .locator('text=Auto Assignments')
-      .waitFor({ state: 'hidden' });
+    const autoAssignmentsLoader = nmrium.page.locator('text=Auto Assignments');
+
+    await autoAssignmentsLoader.waitFor({ state: 'visible' });
+    await autoAssignmentsLoader.waitFor({ state: 'hidden' });
 
     await expect(
       nmrium.page.locator(
