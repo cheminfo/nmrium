05d6fe0c1c8ca2dc19c33f73784a0ec0b9cb0362
jobo322 <jose.bolanos@correounivalle.edu.co>
2023-02-06 03:49:45 -0500

    chore: refactor options of shift2D[X|Y] filter

--- src/data/data2d/filter2d/shiftX.ts
+++ src/data/data2d/filter2d/shiftX.ts
@@ -9,10 +9,11 @@ export const name = 'Shift 2D X';
  * @param {number} [shiftValue=0]
  */
 
-export function apply(datum2D: Datum2D, shiftValue = 0) {
+export function apply(datum2D: Datum2D, options: { shift?: number } = {}) {
+  const { shift = 0 } = options;
   for (const key in datum2D.data) {
-    datum2D.data[key].minX += shiftValue;
-    datum2D.data[key].maxX += shiftValue;
+    datum2D.data[key].minX += shift;
+    datum2D.data[key].maxX += shift;
   }
 }
 
