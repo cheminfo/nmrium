ed3701c7099df1e5b708eb494b4deef6387ef64f
hamed-musallam <hamed.musallam@gmail.com>
2023-04-19 17:28:51 +0200

    test: fix buttons selector

--- test-e2e/panels/settings.test.ts
+++ test-e2e/panels/settings.test.ts
@@ -6,7 +6,7 @@ test('process 1d FID 13c spectrum', async ({ page }) => {
   const nmrium = await NmriumPage.create(page);
   await test.step('Spectra panel settings', async () => {
     await nmrium.page.click(
-      '_react=AccordionItem[title="Spectra"] >> _react=ToolTip[title="preferences"]',
+      '_react=AccordionItem[title="Spectra"] >> _react=ToolTip[title="preferences" i]',
     );
     await nmrium.page.click(
       '_react=AccordionItem[title="Spectra"] >> _react=ToolTip[title="Save preferences" i]',
@@ -16,7 +16,7 @@ test('process 1d FID 13c spectrum', async ({ page }) => {
   await test.step('Peaks panel settings', async () => {
     await nmrium.clickPanel('Peaks');
     await nmrium.page.click(
-      '_react=AccordionItem[title="Peaks"] >> _react=ToolTip[title="preferences"]',
+      '_react=AccordionItem[title="Peaks"] >> _react=ToolTip[title="preferences" i]',
     );
     await nmrium.page.click(
       '_react=AccordionItem[title="Peaks"] >> _react=ToolTip[title="Save preferences" i]',
@@ -26,7 +26,7 @@ test('process 1d FID 13c spectrum', async ({ page }) => {
   await test.step('Integrals panel settings', async () => {
     await nmrium.clickPanel('Integrals');
     await nmrium.page.click(
-      '_react=AccordionItem[title="Integrals"] >> _react=ToolTip[title="preferences"]',
+      '_react=AccordionItem[title="Integrals"] >> _react=ToolTip[title="preferences" i]',
     );
     await nmrium.page.click(
       '_react=AccordionItem[title="Integrals"] >> _react=ToolTip[title="Save preferences" i]',
@@ -36,7 +36,7 @@ test('process 1d FID 13c spectrum', async ({ page }) => {
   await test.step('Ranges panel settings', async () => {
     await nmrium.clickPanel('Ranges');
     await nmrium.page.click(
-      '_react=AccordionItem[title="Ranges"] >> _react=ToolTip[title="preferences"]',
+      '_react=AccordionItem[title="Ranges"] >> _react=ToolTip[title="preferences" i]',
     );
     await nmrium.page.click(
       '_react=AccordionItem[title="Ranges"] >> _react=ToolTip[title="Save preferences" i]',
