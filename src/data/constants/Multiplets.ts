fa4dc72d725b9de76a040aca67426034e71e81e6
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2023-05-12 10:25:00 -0500

    fix: use a central source of possible patterns for multiplicities (#2352)

--- src/data/constants/Multiplets.ts
+++ src/data/constants/Multiplets.ts
@@ -1,19 +1,15 @@
+import { MultiplicityPatterns } from 'nmr-processing';
+
 export interface MultipletDefinition {
   label: string;
   value: string;
   multiplicity: number | null;
 }
 
-export const Multiplets: MultipletDefinition[] = [
-  { label: 'massive (m)', value: 'm', multiplicity: null },
-  { label: 'singlet (s)', value: 's', multiplicity: 1 },
-  { label: 'broad singlet (br s)', value: 'br s', multiplicity: 1 },
-  { label: 'doublet (d)', value: 'd', multiplicity: 2 },
-  { label: 'triplet (t)', value: 't', multiplicity: 3 },
-  { label: 'quartet (q)', value: 'q', multiplicity: 4 },
-  { label: 'quintet (i)', value: 'i', multiplicity: 5 },
-  { label: 'sextet (x)', value: 'x', multiplicity: 6 },
-  { label: 'septet (p)', value: 'p', multiplicity: 7 },
-  { label: 'octet (o)', value: 'o', multiplicity: 8 },
-  { label: 'nonet (n)', value: 'n', multiplicity: 9 },
-];
+export const Multiplets: MultipletDefinition[] = MultiplicityPatterns.map(
+  ({ label, value, multiplicity }) => ({
+    label: `${label} (${value})`,
+    value,
+    multiplicity,
+  }),
+);
