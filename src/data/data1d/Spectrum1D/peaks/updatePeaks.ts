89cc524ec606fdfacf3f5142539dcb3ba5d4250d
jobo322 <jose.bolanos@correounivalle.edu.co>
2023-06-05 22:09:29 -0500

    chore: remove updatePeaks

--- src/data/data1d/Spectrum1D/peaks/updatePeaks.ts
+++
@@ -1,9 +0,0 @@
-import { Spectrum1D } from 'nmr-load-save';
-import { mapPeaks } from 'nmr-processing';
-
-export function updatePeaks(spectrum: Spectrum1D) {
-  spectrum.peaks.values = mapPeaks(spectrum.peaks.values, spectrum, {
-    checkIsExisting: false,
-    shiftTarget: 'current',
-  });
-}
