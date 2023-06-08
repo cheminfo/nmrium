c2d34a2bd1664ff745f60a32cffc8ca56cf0b4cf
hamed-musallam <hamed.musallam@gmail.com>
2023-06-08 17:30:39 +0200

    fix: draw a molecule with one atom
    check if the atoms or the bounds exist in the molfile
    
    close #2400

--- src/data/utilities/isMolFileEmpty.ts
+++ src/data/utilities/isMolFileEmpty.ts
@@ -1,10 +1,14 @@
 export function isMolFileEmpty(molFile: string) {
   if (!molFile) return true;
 
-  const molText =
-    /(?<s>M {2}V30 BEGIN BOND)(?<mol>.*?)(?<e>M {2}V30 END BOND)/gs.exec(
+  const bounds =
+    /(?<s>M {2}V30 BEGIN BOND)(?<bounds>.*?)(?<e>M {2}V30 END BOND)/gs.exec(
       molFile,
-    )?.groups?.mol;
+    )?.groups?.bounds;
+  const atoms =
+    /(?<s>M {2}V30 BEGIN ATOM)(?<atoms>.*?)(?<e>M {2}V30 END ATOM)/gs.exec(
+      molFile,
+    )?.groups?.atoms;
 
-  return !molText?.trim();
+  return !(atoms?.trim() || bounds?.trim());
 }
