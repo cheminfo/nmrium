1b37ced8cad3dde6b81eef4adb88d5e90525bd02
hamed-musallam <hamed.musallam@gmail.com>
2023-06-21 16:25:02 +0200

    style: DeltaInput component

--- src/component/modal/editRange/forms/components/DeltaInput.tsx
+++ src/component/modal/editRange/forms/components/DeltaInput.tsx
@@ -1,12 +1,14 @@
 import { translateMultiplet } from 'nmr-processing';
-import { CSSProperties } from 'react';
 
+import { InputStyle } from '../../../../elements/Input';
 import FormikInput from '../../../../elements/formik/FormikInput';
 
-const style: Record<'input', CSSProperties> = {
+const style: InputStyle = {
   input: {
     width: '50px',
     height: '26px',
+  },
+  inputWrapper: {
     borderWidth: 0,
     margin: '0 5px',
   },
