b8fe1a42d7e0df9050a6d932def0fb1a89422b91
Michaël Zasso <targos@protonmail.com>
2023-07-27 16:36:28 +0200

    fix: clamp SVG paths to avoid rendering issues (#2551)
    Closes: https://github.com/cheminfo/nmrium/issues/2517

--- src/component/utility/PathBuilder.ts
+++ src/component/utility/PathBuilder.ts
@@ -2,10 +2,14 @@ export class PathBuilder {
   private array: string[] = [];
 
   public moveTo(x: number, y: number) {
+    x = clamp(x);
+    y = clamp(y);
     this.array.push(`M ${x} ${y}`);
   }
 
   public lineTo(x: number, y: number) {
+    x = clamp(x);
+    y = clamp(y);
     this.array.push(`L ${x} ${y}`);
   }
 
@@ -16,4 +20,12 @@ export class PathBuilder {
   public toString() {
     return this.array.join(' ');
   }
+}
+
+/**
+ * Clamp values to avoid rendering issues of the SVG.
+ * This assumes that the viewport has pixel coordinates (so the max values are outside of the visible area)
+ */
+function clamp(value: number) {
+  return Math.max(-1e5, Math.min(1e5, value));
 }
