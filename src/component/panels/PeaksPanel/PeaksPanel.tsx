23f61062b44d30ebca826048ebe6a3f864d42b97
hamed musallam <hamed.musallam@gmail.com>
2023-08-07 13:16:38 +0200

    refactor: change peaks displaying mode icon

--- src/component/panels/PeaksPanel/PeaksPanel.tsx
+++ src/component/panels/PeaksPanel/PeaksPanel.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { SvgNmrFt, SvgNmrPeaks } from 'cheminfo-font';
+import { SvgNmrFt, SvgNmrPeaks, SvgNmrPeaksTopLabels } from 'cheminfo-font';
 import SvgPeaks from 'cheminfo-font/lib-react-cjs/lib-react-tsx/nmr/Peaks';
 import { PeaksViewState, Spectrum1D } from 'nmr-load-save';
 import { Info1D, Peak1D, Peaks } from 'nmr-processing';
@@ -221,7 +221,9 @@ function PeaksPanelInner({
             disabled={!peaks?.values || peaks.values.length === 0}
             value={peaksViewState.displayingMode === 'spread'}
           >
-            <SvgNmrPeaks style={{ pointerEvents: 'none', fontSize: '12px' }} />
+            <SvgNmrPeaksTopLabels
+              style={{ pointerEvents: 'none', fontSize: '12px' }}
+            />
           </ActiveButton>
         </DefaultPanelHeader>
       )}
