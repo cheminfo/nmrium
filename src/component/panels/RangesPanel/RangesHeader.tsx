7f992700dc8727f36e4130e5572b78c2705a588e
Luc Patiny <lpatiny@users.noreply.github.com>
2023-08-11 07:42:02 +0200

    chore: improve integration tooltip (#2607)

--- src/component/panels/RangesPanel/RangesHeader.tsx
+++ src/component/panels/RangesPanel/RangesHeader.tsx
@@ -243,7 +243,7 @@ function RangesHeader({
 
         <ActiveButton
           className="icon"
-          popupTitle="Fix integral values"
+          popupTitle="Fixed integration sum"
           popupPlacement="right"
           onClick={changeSumConstantFlagHandler}
           value={ranges?.options?.isSumConstant}
