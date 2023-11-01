29f0aa6a2adbaeea87ea5ef510208c96b4a843e5
hamed musallam <hamed.musallam@gmail.com>
2023-11-01 21:42:38 +0100

    refactor: useDraggable hook

--- src/component/elements/draggable/useDraggable.ts
+++ src/component/elements/draggable/useDraggable.ts
@@ -1,8 +1,14 @@
-import { useCallback, useMemo, useRef, useState } from 'react';
+import { useRef } from 'react';
 
 export interface Position {
   x: number;
   y: number;
+}
+
+interface DragChangeCb {
+  position: Position;
+  action: Action;
+  isActive: boolean;
 }
 
 interface UseDraggable {
@@ -10,13 +16,13 @@ interface UseDraggable {
   parentElement?: HTMLElement | null;
   fromEdge?: boolean;
   dragHandleClassName?: string;
+  onChange: (dragEvent: DragChangeCb) => void;
 }
+
 export interface Draggable {
   onPointerDown: (
     event: React.PointerEvent<HTMLDivElement | SVGElement>,
   ) => void;
-  position: { value: Position; action: Action };
-  isActive: boolean;
 }
 type Action = 'start' | 'move' | 'end' | null;
 
@@ -26,91 +32,75 @@ export default function useDraggable(props: UseDraggable): Draggable {
     parentElement,
     fromEdge = false,
     dragHandleClassName,
+    onChange,
   } = props;
 
   const isActive = useRef<boolean>(false);
   const positionRef = useRef<Position>({ x, y });
-  const [position, setPosition] = useState<{ value: Position; action: Action }>(
-    {
-      value: { x, y },
-      action: null,
-    },
-  );
 
-  const onPointerDown = useCallback(
-    (e: React.PointerEvent<HTMLDivElement | SVGElement>) => {
+  function onPointerDown(e: React.PointerEvent<HTMLDivElement | SVGElement>) {
+    e.stopPropagation();
+    isActive.current = true;
+    const eventTarget = e.currentTarget as HTMLElement;
+    positionRef.current = { x, y };
+
+    const classes =
+      (e.target as HTMLElement).getAttribute('class')?.split(' ') || [];
+    if (
+      (dragHandleClassName && classes.includes(dragHandleClassName)) ||
+      !dragHandleClassName
+    ) {
+      const _parentElement = parentElement || eventTarget?.parentElement;
+      if (_parentElement) {
+        const parentBounding = _parentElement.getBoundingClientRect();
+        const currentBounding = eventTarget?.getBoundingClientRect();
+        const startPosition: Position = {
+          x: parentBounding.x + (!fromEdge ? e.clientX - currentBounding.x : 0),
+          y: parentBounding.y + (!fromEdge ? e.clientY - currentBounding.y : 0),
+        };
+
+        if (parentBounding) {
+          positionRef.current = startPosition;
+        }
+        onChange({ position: { x, y }, action: 'start', isActive: true });
+      }
+
+      window.addEventListener('pointermove', moveCallback);
+      window.addEventListener('pointerup', upCallback);
+    }
+
+    function upCallback(e: PointerEvent) {
       e.stopPropagation();
-      isActive.current = true;
-      const eventTarget = e.currentTarget as HTMLElement;
-
-      const classes =
-        (e.target as HTMLElement).getAttribute('class')?.split(' ') || [];
-      if (
-        (dragHandleClassName && classes.includes(dragHandleClassName)) ||
-        !dragHandleClassName
-      ) {
-        const _parentElement = parentElement || eventTarget?.parentElement;
-        if (_parentElement) {
-          const parentBounding = _parentElement.getBoundingClientRect();
-          const currentBounding = eventTarget?.getBoundingClientRect();
-          const startPosition: Position = {
-            x:
-              parentBounding.x +
-              (!fromEdge ? e.clientX - currentBounding.x : 0),
-            y:
-              parentBounding.y +
-              (!fromEdge ? e.clientY - currentBounding.y : 0),
-          };
-
-          if (parentBounding) {
-            positionRef.current = startPosition;
-          }
-          setPosition((prevPortion) => ({ ...prevPortion, action: 'start' }));
-        }
-
-        window.addEventListener('pointermove', moveCallback);
-        window.addEventListener('pointerup', upCallback);
+      if (isActive.current) {
+        onChange({
+          position: {
+            x: e.clientX - positionRef.current.x,
+            y: e.clientY - positionRef.current.y,
+          },
+          action: 'end',
+          isActive: false,
+        });
+        isActive.current = false;
       }
 
-      function upCallback(e: PointerEvent) {
-        e.stopPropagation();
-        if (isActive.current) {
-          setPosition({
-            value: {
-              x: e.clientX - positionRef.current.x,
-              y: e.clientY - positionRef.current.y,
-            },
-            action: 'end',
-          });
-          isActive.current = false;
-        }
+      window.removeEventListener('pointermove', moveCallback);
+      window.removeEventListener('pointerup', upCallback);
+    }
+    function moveCallback(e: PointerEvent) {
+      e.stopPropagation();
 
-        window.removeEventListener('pointermove', moveCallback);
-        window.removeEventListener('pointerup', upCallback);
+      if (isActive.current) {
+        onChange({
+          position: {
+            x: e.clientX - positionRef.current.x,
+            y: e.clientY - positionRef.current.y,
+          },
+          action: 'move',
+          isActive: true,
+        });
       }
-      function moveCallback(e: PointerEvent) {
-        e.stopPropagation();
+    }
+  }
 
-        if (isActive.current) {
-          setPosition({
-            value: {
-              x: e.clientX - positionRef.current.x,
-              y: e.clientY - positionRef.current.y,
-            },
-            action: 'move',
-          });
-        }
-      }
-    },
-    [dragHandleClassName, fromEdge, parentElement],
-  );
-
-  return useMemo(
-    () => ({
-      onPointerDown,
-      position,
-      isActive: isActive.current,
-    }),
-    [onPointerDown, position],
-  );
+  return { onPointerDown };
 }
