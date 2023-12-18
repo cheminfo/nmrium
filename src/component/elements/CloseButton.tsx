103e920954be92072ad7e9493de28afbee3614a7
hamed musallam <hamed.musallam@gmail.com>
2023-12-18 22:55:17 +0100

    refactor: toolbar button

--- src/component/elements/CloseButton.tsx
+++ src/component/elements/CloseButton.tsx
@@ -1,24 +1,7 @@
 import { FaTimes } from 'react-icons/fa';
-import { Button, ButtonProps } from 'react-science/ui';
 
-export function CloseButton(props: ButtonProps) {
-  const { onClick, title = 'Close', ...otherProps } = props;
-  return (
-    <Button
-      {...otherProps}
-      minimal
-      intent="danger"
-      onClick={onClick}
-      icon={<FaTimes />}
-      tooltipProps={{
-        content: title,
-        placement: 'bottom-start',
-        intent: 'danger',
-        compact: true,
-      }}
-      style={{
-        fontSize: '1.25em',
-      }}
-    />
-  );
+import { ToolBarButton, ToolBarButtonProps } from './ToolBarButton';
+
+export function CloseButton(props: Pick<ToolBarButtonProps, "onClick" | "title" | "className">) {
+  return <ToolBarButton {...props} intent="danger" icon={<FaTimes />} placement='bottom-start' />;
 }
