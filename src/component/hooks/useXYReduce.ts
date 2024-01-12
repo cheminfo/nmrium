9005e297b4da1be570aa9868e704c9b70063c6b5
andcastillo@gmail.com <andcastillo@gmail.com>
2024-01-12 17:34:34 +0800

    fix: xy data with discontinuous  x
    Related to #2820

--- src/component/hooks/useXYReduce.ts
+++ src/component/hooks/useXYReduce.ts
@@ -1,4 +1,4 @@
-import { DataXY } from 'cheminfo-types';
+import { DataXY, FromTo } from 'cheminfo-types';
 import { xyReduce } from 'ml-spectra-processing';
 import { useCallback } from 'react';
 
@@ -23,9 +23,32 @@ export default function useXYReduce(
         domainAxis === XYReducerDomainAxis.XAxis ? xDomain : yDomain;
       return xyReduce(
         { x, y },
-        { from, to, nbPoints: width * 4, optimize: true },
+        { from, to, nbPoints: width * 4, optimize: true, zones: getZones(x)},
       );
     },
     [domainAxis, width, xDomain, yDomain],
   );
+}
+
+function getZones(x: Float64Array): FromTo[] {
+  const zones: FromTo[] = [];
+  let from = x[0];
+  const deltaX = x[1] - x[0];
+  const deltaTol = deltaX * 0.005; // Accept deltas having this discrepancy as equal
+  let i = 1;
+  for(; i < x.length; i++) {
+    if(Math.abs(x[i + 1] - x[i] - deltaX) > deltaTol) {
+      zones.push({from, to: x[i]})
+      from = x[i + 1];
+      i++;
+    } 
+  }
+  zones.push({from, to: x[i - 1]});
+
+  if(zones.length >= x.length/10) {
+    // There are too many zones compared with the original x
+    return [];
+  }
+
+  return zones;
 }
