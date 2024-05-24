656dabc685383893005936f63d11758c3c545326
hamed musallam <hamed.musallam@gmail.com>
2024-05-24 09:32:43 +0200

    refactor: improve stocsy paths with grouping by color

--- src/component/1d/matrix/Stocsy.tsx
+++ src/component/1d/matrix/Stocsy.tsx
@@ -1,17 +1,13 @@
 import { xFindClosestIndex } from 'ml-spectra-processing';
 import { matrixToStocsy } from 'nmr-processing';
-import { useEffect, useRef } from 'react';
 
 import { useChartData } from '../../context/ChartContext';
-import {
-  withExport,
-  withExportRegister,
-} from '../../context/PrepareExportContext';
 import { useScaleChecked } from '../../context/ScaleContext';
 import { usePanelPreferences } from '../../hooks/usePanelPreferences';
+import { PathBuilder } from '../../utility/PathBuilder';
 import { getYScaleWithRation } from '../utilities/scale';
 
-import { useMatrix } from './useMatrix';
+import { interpolatedColorsPoints, useMatrix } from './useMatrix';
 
 interface StocsyProps {
   x: Float64Array | never[];
@@ -19,9 +15,13 @@ interface StocsyProps {
   color: string[];
   scaleRatio: number;
 }
-const circleRadius = 5;
 
-const componentId = 'stocsy';
+interface StocsyIndexPointProps {
+  x: Float64Array | never[];
+  color: string[];
+  chemicalShift: number | null;
+  xIndex: number;
+}
 
 export function Stocsy() {
   const {
@@ -51,14 +51,11 @@ export function InnerStocsy({ scaleRatio, chemicalShift }) {
     <g>
       <StocsyIndexPoint
         x={x}
-        y={y}
         color={color}
-        scaleRatio={scaleRatio}
         chemicalShift={chemicalShift}
         xIndex={xIndex}
       />
 
-      <RenderStocsyAsCanvas x={x} y={y} color={color} scaleRatio={scaleRatio} />
       <RenderStocsyAsSVG x={x} y={y} color={color} scaleRatio={scaleRatio} />
     </g>
   );
@@ -75,110 +72,55 @@ function useYScale(scaleRatio: number) {
   });
 }
 
-const StocsyIndexPoint = (
-  props: StocsyProps & { chemicalShift: number | null; xIndex: number },
-) => {
-  const { x, y, color, scaleRatio, chemicalShift, xIndex } = props;
-  const scaleY = useYScale(scaleRatio);
+function StocsyIndexPoint(props: StocsyIndexPointProps) {
+  const { x, color, chemicalShift, xIndex } = props;
   const { scaleX } = useScaleChecked();
 
   const xPixel = scaleX()(chemicalShift ?? x[0]);
-  const yPixel = scaleY(y[xIndex]);
   const colorValue = color[xIndex];
 
   return (
-    <g transform={`translate(${xPixel} ${yPixel})`}>
-      <circle r={circleRadius} fill={colorValue} />
+    <g transform={`translate(${xPixel} 0)`}>
+      <line
+        x1={-0.5}
+        y1={0}
+        x2={-0.5}
+        y2="100%"
+        stroke={colorValue}
+        strokeDasharray="5 2"
+      />
     </g>
   );
-};
+}
 
-const RenderStocsyAsCanvas = withExportRegister((props: StocsyProps) => {
-  const { x, y, color, scaleRatio } = props;
-
-  const { width, margin, height } = useChartData();
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const scaleY = useYScale(scaleRatio);
-  const { scaleX } = useScaleChecked();
-
-  const { right, bottom, left } = margin;
-  const canvasWidth = width - right - left;
-  const canvasHeight = height - bottom;
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext('2d');
-
-    function drawLinesInRect(x1, x2, y1, y2, color) {
-      if (!ctx) return;
-      ctx.strokeStyle = color;
-      ctx.lineWidth = 1;
-      ctx.beginPath();
-      ctx.moveTo(x1, x2);
-      ctx.lineTo(y1, y2);
-      ctx.stroke();
-    }
-
-    let index = 0;
-    for (const val of x) {
-      if (index === x.length - 1) {
-        break;
-      }
-      const nextIndex = index + 1;
-
-      const x1 = scaleX()(val);
-      const y1 = scaleY(y[index]);
-      const x2 = scaleX()(x[nextIndex]);
-      const y2 = scaleY(y[nextIndex]);
-
-      drawLinesInRect(x1, y1, x2, y2, color[index]);
-      index++;
-    }
-
-    return () => {
-      if (ctx && canvas) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-      }
-    };
-  }, [color, height, scaleX, scaleY, width, x, y]);
-
-  return (
-    <foreignObject
-      {...{
-        width: canvasWidth,
-        height: canvasHeight,
-      }}
-      data-no-export="true"
-    >
-      <canvas
-        ref={canvasRef}
-        {...{ width: canvasWidth, height: canvasHeight }}
-      />
-    </foreignObject>
-  );
-}, componentId);
-
-const RenderStocsyAsSVG = withExport((props: StocsyProps) => {
+function RenderStocsyAsSVG(props: StocsyProps) {
   const { x, y, color, scaleRatio } = props;
   const scaleY = useYScale(scaleRatio);
   const { scaleX } = useScaleChecked();
+  const data = interpolatedColorsPoints({ x, y, color });
 
-  return Array.from(x).map((val, index) => {
-    if (index === x.length - 1 || typeof val !== 'number') {
-      return null;
+  return Object.keys(data).map((color) => {
+    const points = data[color];
+    const pointDiff = x[1] - x[0];
+    const pathBuilder = new PathBuilder();
+    pathBuilder.moveTo(scaleX()(points[0].x), scaleY(points[0].y));
+
+    for (let i = 1; i < points.length; i++) {
+      const segmentDiff = points[i].x - points[i - 1].x;
+
+      if (segmentDiff >= pointDiff) {
+        pathBuilder.moveTo(scaleX()(points[i].x), scaleY(points[i].y));
+      } else {
+        pathBuilder.lineTo(scaleX()(points[i].x), scaleY(points[i].y));
+      }
     }
-    const nextIndex = index + 1;
-    const x1 = scaleX()(val);
-    const y1 = scaleY(y[index]);
-    const x2 = scaleX()(x[nextIndex]);
-    const y2 = scaleY(y[nextIndex]);
-
     return (
       <path
-        key={`${val}`}
-        d={`M${x1},${y1} L${x2},${y2} Z`}
+        key={color}
+        d={pathBuilder.toString()}
+        stroke={color}
         fill="transparent"
-        stroke={color[index]}
       />
     );
   });
-}, componentId);
+}
--- src/component/1d/matrix/useMatrix.ts
+++ src/component/1d/matrix/useMatrix.ts
@@ -56,3 +56,50 @@ export function useMatrix() {
     ),
   };
 }
+
+interface InterpolateColorsPointsOptions {
+  x: Float64Array | never[];
+  y: number[];
+  color: string[];
+}
+
+export function interpolatedColorsPoints(data: InterpolateColorsPointsOptions) {
+  const groups: Record<string, Array<{ x: number; y: number }>> = {};
+
+  for (let i = 0; i < data.color.length - 1; i++) {
+    const color = data.color[i];
+    const nextIndex = i + 1;
+    const nextColor = data.color[nextIndex];
+    const x1 = data.x[i];
+    const y1 = data.y[i];
+    const x2 = data.x[nextIndex];
+    const y2 = data.y[nextIndex];
+
+    // add the original point
+    if (!groups[color]) groups[color] = [];
+    groups[color].push({ x: x1, y: y1 });
+
+    const interpolatedX = (x1 + x2) / 2;
+    const interpolatedY = (y1 + y2) / 2;
+
+    // add the interpolated point
+    if (!groups[color]) groups[color] = [];
+    groups[color].push({ x: interpolatedX, y: interpolatedY });
+
+    // If the next point has a different color, add a segement with that color
+    if (color !== nextColor) {
+      if (!groups[nextColor]) groups[nextColor] = [];
+      groups[nextColor].push({ x: interpolatedX, y: interpolatedY });
+    }
+  }
+
+  // Add the last original point
+  const lastIndex = data.x.length - 1;
+  const lastColor = data.color[lastIndex];
+  const lastX = data.x[lastIndex];
+  const lastY = data.y[lastIndex];
+  if (!groups[lastColor]) groups[lastColor] = [];
+  groups[lastColor].push({ x: lastX, y: lastY });
+
+  return groups;
+}
