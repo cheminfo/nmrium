60e645572c3b9b6e264e86272cfedb9153a539a1
hamed musallam <hamed.musallam@gmail.com>
2024-05-30 14:03:18 +0200

    refactor: numeric input component

---
+++ src/component/elements/NumberInput2.tsx
@@ -0,0 +1,138 @@
+/** @jsxImportSource @emotion/react */
+import { Classes, NumericInput, NumericInputProps } from '@blueprintjs/core';
+import debounce from 'lodash/debounce';
+import {
+  useEffect,
+  useMemo,
+  useState,
+  isValidElement,
+  forwardRef,
+} from 'react';
+
+interface ValueProps
+  extends Pick<React.InputHTMLAttributes<HTMLInputElement>, 'name' | 'style'>,
+    Pick<NumericInputProps, 'onValueChange' | 'value'> {
+  checkValue?: (element?: number) => boolean;
+  debounceTime?: number;
+}
+type UseInputProps = Omit<ValueProps, 'name'>;
+export interface NumberInput2Props
+  extends Omit<NumericInputProps, 'value' | 'onValueChange'>,
+    ValueProps {
+  format?: () => (element: string) => number | string;
+  autoSelect?: boolean;
+}
+
+function useNumberInput(props: UseInputProps) {
+  const {
+    value: externalValue,
+    debounceTime,
+    onValueChange,
+    checkValue,
+  } = props;
+  const [internalValue, setValue] = useState<number | string>();
+  const value = debounceTime ? internalValue : externalValue;
+  const [isDebounced, setDebouncedStatus] = useState<boolean>(false);
+
+  const debounceOnValueChange = useMemo(
+    () =>
+      debounce(
+        (
+          valueAsNumber: number,
+          valueAsString: string,
+          inputElement: HTMLInputElement | null,
+        ) => {
+          onValueChange?.(valueAsNumber, valueAsString, inputElement);
+          setDebouncedStatus(true);
+        },
+        debounceTime,
+      ),
+    [debounceTime, onValueChange],
+  );
+
+  useEffect(() => {
+    if (debounceTime) {
+      setValue(externalValue);
+    }
+  }, [debounceTime, externalValue]);
+
+  function handleValueChange(
+    valueAsNumber: number,
+    valueAsString: string,
+    inputElement: HTMLInputElement | null,
+  ) {
+    setDebouncedStatus(false);
+
+    if (!checkValue || checkValue?.(valueAsNumber)) {
+      if (debounceTime) {
+        setValue(valueAsString);
+        debounceOnValueChange(valueAsNumber, valueAsString, inputElement);
+      } else {
+        onValueChange?.(valueAsNumber, valueAsString, inputElement);
+      }
+    }
+  }
+
+  return {
+    handleValueChange,
+    debounceOnValueChange,
+    isDebounced,
+    value,
+  };
+}
+
+function getIcon(leftIcon) {
+  if (isValidElement(leftIcon)) {
+    return <span className={Classes.ICON}>{leftIcon}</span>;
+  }
+
+  return leftIcon;
+}
+
+function getClasses(isDebounced: boolean, className = '') {
+  const classes = ['text-input', className];
+  if (isDebounced) {
+    classes.push('debounce-end');
+  }
+
+  return classes.join(' ');
+}
+
+function InnerNumberInput(props: NumberInput2Props, ref) {
+  const {
+    debounceTime = 0,
+    onValueChange,
+    checkValue,
+    value: externalValue,
+    className = '',
+    leftIcon,
+    autoSelect = false,
+    ...otherInputProps
+  } = props;
+
+  const { handleValueChange, isDebounced, value } = useNumberInput({
+    value: externalValue,
+    debounceTime,
+    onValueChange,
+    checkValue,
+  });
+
+  const icon = getIcon(leftIcon);
+  const classes = getClasses(isDebounced, className);
+
+  return (
+    <NumericInput
+      leftIcon={icon}
+      className={classes}
+      inputRef={ref}
+      onValueChange={handleValueChange}
+      value={value}
+      selectAllOnFocus={autoSelect}
+      {...otherInputProps}
+    />
+  );
+}
+
+const NumberInput2 = forwardRef(InnerNumberInput);
+
+export { NumberInput2 };
