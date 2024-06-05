93b294dcc28a03f6c5227a2863721416d373db8c
hamed musallam <hamed.musallam@gmail.com>
2024-06-05 15:09:17 +0200

    refactor: databases table

--- src/component/modal/setting/settings-tabs/DatabasesTabContent.tsx
+++ src/component/modal/setting/settings-tabs/DatabasesTabContent.tsx
@@ -1,12 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import { Classes } from '@blueprintjs/core';
+import { css } from '@emotion/react';
 import { v4 } from '@lukeed/uuid';
 import { Field, useFormikContext } from 'formik';
 import { CustomWorkspaces } from 'nmr-load-save';
-import { CSSProperties } from 'react';
-import { FaLink, FaPlus, FaTimes } from 'react-icons/fa';
+import { CSSProperties, useCallback, useMemo } from 'react';
+import { FaPlus, FaRegTrashAlt, FaLink } from 'react-icons/fa';
+import { Button } from 'react-science/ui';
 
-import Button from '../../../elements/Button';
+import { CheckBoxCell } from '../../../elements/CheckBoxCell';
 import { GroupPane } from '../../../elements/GroupPane';
-import FormikCheckBox from '../../../elements/formik/FormikCheckBox';
+import ReactTable, { Column } from '../../../elements/ReactTable/ReactTable';
+import { tableInputStyle } from '../../../elements/ReactTable/Style';
 import FormikInput from '../../../elements/formik/FormikInput';
 import type { NMRiumWorkspace } from '../../../main';
 import { getPreferencesByWorkspace } from '../../../reducer/preferences/utilities/getPreferencesByWorkspace';
@@ -48,24 +53,143 @@ function DatabasesTabContent({
   originalWorkspaces,
 }: DatabasesTabContentProps) {
   const { values, setFieldValue } = useFormikContext();
+  const databases = (values as any).databases?.data || [];
 
-  const databases = (values as any).databases;
+  const addHandler = useCallback(
+    (data: readonly any[], index = 0) => {
+      let databases: any[] = [];
+      const emptyField = {
+        key: v4(),
+        label: '',
+        url: '',
+        enabled: true,
+      };
+      if (data && Array.isArray(data)) {
+        databases = [...data.slice(0, index), emptyField, ...data.slice(index)];
+      } else {
+        databases.push(emptyField);
+      }
+      void setFieldValue('databases.data', databases);
+    },
+    [setFieldValue],
+  );
 
-  function deleteHandler(index: number) {
-    const _database = databases.data.slice();
-    _database.splice(index, 1);
-    void setFieldValue('databases.data', _database);
-  }
+  const deleteHandler = useCallback(
+    (data, index: number) => {
+      const databases = data.filter((_, columnIndex) => columnIndex !== index);
+      void setFieldValue('databases.data', databases);
+    },
+    [setFieldValue],
+  );
 
-  function addHandler() {
-    const newDatabase = {
-      key: v4(),
-      label: '',
-      url: '',
-      enabled: true,
-    };
-    void setFieldValue('databases.data', [...databases.data, newDatabase]);
-  }
+  const COLUMNS: Array<Column<any>> = useMemo(
+    () => [
+      {
+        Header: '#',
+        style: { width: '25px', textAlign: 'center' },
+        accessor: (_, index) => index + 1,
+      },
+      {
+        Header: 'Label',
+        style: { minWidth: '150px' },
+        Cell: ({ row }) => {
+          return (
+            <FormikInput
+              name={`databases.data.${row.index}.label`}
+              style={tableInputStyle}
+            />
+          );
+        },
+      },
+      {
+        Header: 'URL',
+        style: { width: '100%' },
+        Cell: ({ row }) => {
+          return (
+            <FormikInput
+              name={`databases.data.${row.index}.url`}
+              style={tableInputStyle}
+            />
+          );
+        },
+      },
+      {
+        Header: 'Enabled',
+        style: { width: '30px' },
+        Cell: ({ row }) => (
+          <CheckBoxCell
+            name={`databases.data.${row.index}.enabled`}
+            defaultValue
+          />
+        ),
+      },
+      {
+        Header: 'Auto Load',
+        style: { width: '30px' },
+        Cell: ({ row }) => (
+          <Field
+            style={style.checkbox}
+            type="radio"
+            name="databases.defaultDatabase"
+            value={row.original.key}
+          />
+        ),
+      },
+      {
+        Header: '',
+        style: { maxWidth: '100px', width: '85px' },
+        id: 'add-button',
+        Cell: ({ data, row }) => {
+          const { index, original: record } = row;
+          return (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                padding: '0 3px',
+              }}
+            >
+              <Button
+                small
+                outlined
+                intent="danger"
+                tooltipProps={{ content: '', disabled: true }}
+                onClick={() => deleteHandler(data, index)}
+              >
+                <FaRegTrashAlt className={Classes.ICON} />
+              </Button>
+
+              <Button
+                small
+                intent="success"
+                outlined
+                tooltipProps={{ content: '', disabled: true }}
+                onClick={() => addHandler(data, index + 1)}
+                css={css`
+                  margin: 0 3px;
+                `}
+              >
+                <FaPlus className={Classes.ICON} />
+              </Button>
+
+              {isGoogleDocument(record.url) && (
+                <Button
+                  small
+                  outlined
+                  intent="primary"
+                  onClick={() => window.open(record.url, '_blank')}
+                  tooltipProps={{ content: 'Open document', compact: true }}
+                >
+                  <FaLink className={Classes.ICON} />
+                </Button>
+              )}
+            </div>
+          );
+        },
+      },
+    ],
+    [addHandler, deleteHandler],
+  );
 
   function resetHandler() {
     const workSpaceDisplayPreferences = getPreferencesByWorkspace(
@@ -93,102 +217,24 @@ function DatabasesTabContent({
             <DataBaseHeader
               text={text}
               onReset={resetHandler}
-              onAdd={addHandler}
+              onAdd={() => addHandler(databases)}
             />
           );
         }}
       >
-        <table style={style.table}>
-          <thead>
-            <tr>
-              <th style={style.th}>#</th>
-              <th style={style.th}>Label </th>
-              <th style={style.th}>URL</th>
-              <th style={style.th}>Enabled</th>
-              <th style={style.th}>Auto Load</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {databases?.data.map((item, index) => {
-              const num = index + 1;
-              return (
-                // eslint-disable-next-line react/no-array-index-key
-                <tr key={`${index}`} style={{ height: '33px' }}>
-                  <td style={style.serialCol}>
-                    <span>{num}</span>{' '}
-                  </td>
-                  <td style={style.labelCol}>
-                    <FormikInput
-                      style={{ input: style.input }}
-                      name={`databases.data.${index}.label`}
-                      checkErrorAfterInputTouched={false}
-                    />
-                  </td>
-                  <td>
-                    <FormikInput
-                      style={{ input: style.input }}
-                      name={`databases.data.${index}.url`}
-                      checkErrorAfterInputTouched={false}
-                    />
-                  </td>
-                  <td>
-                    <FormikCheckBox
-                      style={{
-                        container: style.checkbox,
-                      }}
-                      className="checkbox-element"
-                      name={`databases.data.${index}.enabled`}
-                    />
-                  </td>
-                  <td>
-                    <Field
-                      style={style.checkbox}
-                      type="radio"
-                      name="databases.defaultDatabase"
-                      value={item.key}
-                    />
-                  </td>
-                  <td>
-                    <div
-                      style={{
-                        display: 'flex',
-                        height: '100%',
-                      }}
-                    >
-                      <Button.Danger
-                        style={{ fontSize: '14px' }}
-                        fill="clear"
-                        onClick={() => deleteHandler(index)}
-                      >
-                        <FaTimes />
-                      </Button.Danger>
-
-                      {databases.length === index + 1 && (
-                        <Button.Done
-                          fill="clear"
-                          style={{ fontSize: '14px' }}
-                          onClick={addHandler}
-                        >
-                          <FaPlus />
-                        </Button.Done>
-                      )}
-                      {isGoogleDocument(item.url) && (
-                        <Button.Action
-                          style={{ fontSize: '13px' }}
-                          fill="clear"
-                          onClick={() => window.open(item.url, '_blank')}
-                        >
-                          <FaLink />
-                        </Button.Action>
-                      )}
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <ReactTable
+          style={{
+            'thead tr th': { zIndex: 1 },
+            td: { padding: 0 },
+          }}
+          rowStyle={{
+            hover: { backgroundColor: '#f7f7f7' },
+            active: { backgroundColor: '#f5f5f5' },
+          }}
+          data={databases}
+          columns={COLUMNS}
+          emptyDataRowText="No Fields"
+        />
       </GroupPane>
     </fieldset>
   );
@@ -199,17 +245,25 @@ function DataBaseHeader({ onReset, onAdd, text }) {
     <div className="section-header" style={{ display: 'flex' }}>
       <p style={{ flex: 1 }}>{text}</p>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Button.Danger
-          fill="clear"
-          size="xSmall"
+        <Button
+          small
+          minimal
+          intent="danger"
+          tooltipProps={{ content: '', disabled: true }}
           onClick={onReset}
-          style={{ margin: '0 5px' }}
         >
           Reset Databases
-        </Button.Danger>
-        <Button.Done fill="outline" size="xSmall" onClick={onAdd}>
+        </Button>
+
+        <Button
+          small
+          outlined
+          intent="success"
+          tooltipProps={{ content: '', disabled: true }}
+          onClick={onAdd}
+        >
           Add Database
-        </Button.Done>
+        </Button>
       </div>
     </div>
   );
