54314be5b98582e14d9032b2dc1c38e9842a760e
hamed musallam <hamed.musallam@gmail.com>
2024-06-25 16:30:22 +0200

    refactor: auto ranges detection options

--- src/component/header/RangesPickingOptionPanel.tsx
+++ src/component/header/RangesPickingOptionPanel.tsx
@@ -1,12 +1,13 @@
-import { Formik } from 'formik';
+import { Checkbox, Button } from '@blueprintjs/core';
+import { yupResolver } from '@hookform/resolvers/yup';
+import has from 'lodash/has';
+import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 
 import { useDispatch } from '../context/DispatchContext';
 import { useToaster } from '../context/ToasterContext';
-import Button from '../elements/Button';
 import Label from '../elements/Label';
-import FormikCheckBox from '../elements/formik/FormikCheckBox';
-import FormikInput from '../elements/formik/FormikInput';
+import { NumberInput2Controller } from '../elements/NumberInput2Controller';
 import {
   MIN_AREA_POINTS,
   useCheckPointsNumberInWindowArea,
@@ -15,17 +16,33 @@ import {
 import { headerLabelStyle } from './Header';
 import { HeaderContainer } from './HeaderContainer';
 
+interface AutoRangesOptions {
+  minMaxRatio: number;
+  lookNegative: boolean;
+}
+
 const validationSchema = Yup.object().shape({
   minMaxRatio: Yup.number().min(0).required(),
+  lookNegative: Yup.boolean().required(),
 });
 
-const initialValues = {
+const initialValues: AutoRangesOptions = {
   minMaxRatio: 0.05,
   lookNegative: false,
 };
 
 function RangesPickingOptionPanel() {
   const dispatch = useDispatch();
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors, isValid },
+  } = useForm<AutoRangesOptions>({
+    defaultValues: initialValues,
+    resolver: yupResolver(validationSchema),
+    mode: 'onChange',
+  });
   const pointsNumber = useCheckPointsNumberInWindowArea();
   const toaster = useToaster();
 
@@ -45,42 +62,37 @@ function RangesPickingOptionPanel() {
 
   return (
     <HeaderContainer>
-      <Formik
-        onSubmit={handleRangesPicking}
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-      >
-        {({ handleSubmit, isValid }) => (
-          <>
-            <Label title="Detect negative" style={headerLabelStyle}>
-              <FormikCheckBox name="lookNegative" />
-            </Label>
-            <Label title="Min/max ratio:" style={headerLabelStyle}>
-              <FormikInput
-                type="number"
-                name="minMaxRatio"
-                style={{
-                  input: {
-                    width: '70px',
-                    textAlign: 'center',
-                  },
-                  inputWrapper: { height: '100%' },
-                }}
-                step={0.01}
-                min={0}
-              />
-            </Label>
+      <Label title="Detect negative" style={headerLabelStyle}>
+        <Checkbox
+          style={{ margin: 0 }}
+          {...register(`lookNegative`)}
+          defaultChecked={false}
+        />
+      </Label>
+      <Label title="Min/max ratio:" style={headerLabelStyle}>
+        <NumberInput2Controller
+          control={control}
+          name="minMaxRatio"
+          inputProps={{
+            allowNumericCharactersOnly: true,
+            min: 0,
+            stepSize: 0.01,
+            majorStepSize: 0.01,
+            minorStepSize: 0.01,
+            intent: has(errors, 'minMaxRatio') ? 'danger' : 'none',
+            style: { width: '70px' },
+          }}
+        />
+      </Label>
 
-            <Button.Done
-              onClick={() => handleSubmit()}
-              style={{ margin: '0 10px' }}
-              disabled={!isValid}
-            >
-              Auto ranges picking
-            </Button.Done>
-          </>
-        )}
-      </Formik>
+      <Button
+        intent="success"
+        onClick={() => handleSubmit(handleRangesPicking)()}
+        style={{ margin: '0 10px' }}
+        disabled={!isValid}
+      >
+        Auto ranges picking
+      </Button>
     </HeaderContainer>
   );
 }
