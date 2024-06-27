9ad6b8c1c32703ab254b02344c92ce49d8464330
hamed musallam <hamed.musallam@gmail.com>
2024-06-27 17:47:01 +0200

    refactor: create color picker dropdown controller

---
+++ src/component/elements/ColorPickerDropdownController.tsx
@@ -0,0 +1,20 @@
+import { Controller, ControllerProps, FieldValues } from 'react-hook-form';
+import { ColorPickerDropdown } from 'react-science/ui';
+
+export function ColorPickerDropdownController<
+  TFieldValues extends FieldValues = FieldValues,
+>(props: Omit<ControllerProps<TFieldValues>, 'render'>) {
+  return (
+    <Controller
+      {...props}
+      render={({ field }) => {
+        return (
+          <ColorPickerDropdown
+            onChangeComplete={({ hex }) => field.onChange(hex)}
+            color={{ hex: field.value }}
+          />
+        );
+      }}
+    />
+  );
+}
