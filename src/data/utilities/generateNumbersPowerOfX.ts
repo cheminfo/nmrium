d7ddafc70a1f380db551e9b8a41896444c4f413f
hamed musallam <hamed.musallam@gmail.com>
2024-06-28 12:59:10 +0200

    refactor: zero filling options

--- src/data/utilities/generateNumbersPowerOfX.ts
+++ src/data/utilities/generateNumbersPowerOfX.ts
@@ -1,10 +1,10 @@
 export default function generateNumbersPowerOfX(
   start: number,
   end: number,
-  options?: { power?: number; format?: (input: number) => string | number },
+  options?: { power?: number; format?: (input: number) => string },
 ) {
   const { power = 2, format = formatNumber } = options || {};
-  const values: Array<{ label: string | number; value: number }> = [];
+  const values: Array<{ label: string; value: number }> = [];
   for (let i = start; i <= end; i++) {
     const result = power ** i;
     values.push({
@@ -15,7 +15,7 @@ export default function generateNumbersPowerOfX(
   return values;
 }
 
-function formatNumber(number: number): string | number {
+function formatNumber(number: number) {
   if (number >= 1024 * 1024) {
     return `${number / (1024 * 1024)}M`;
   } else if (number >= 1024) {
