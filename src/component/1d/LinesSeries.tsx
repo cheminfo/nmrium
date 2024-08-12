cba4e8a19e2dce7df84aa80740c5d7e54383b9b6
hamed musallam <hamed.musallam@gmail.com>
2024-08-12 09:28:43 +0200

    fix: prevent spectrum selection during zoom in stack mode
    close #3190

--- src/component/1d/LinesSeries.tsx
+++ src/component/1d/LinesSeries.tsx
@@ -12,6 +12,7 @@ import { useVerticalAlign } from '../hooks/useVerticalAlign';
 
 import Line from './Line';
 import { SPECTRA_BOTTOM_MARGIN } from './utilities/scale';
+import { useBrushTracker } from '../EventsTrackers/BrushTracker';
 
 const BOX_SIZE = 10;
 
@@ -24,49 +25,68 @@ const style = css`
 `;
 
 function LinesSeries() {
-  const {
-    data,
-    displayerKey,
-    xDomains,
-    width,
-    margin,
-    height,
-    toolOptions: { selectedTool },
-  } = useChartData();
+  const { data, displayerKey, xDomains } = useChartData();
   const activeSpectra = useActiveSpectra();
-  const { shiftY } = useScaleChecked();
-  const verticalAlign = useVerticalAlign();
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.bottom - SPECTRA_BOTTOM_MARGIN;
   const spectra = (data?.filter(
     (d) => isSpectrum1D(d) && d.display.isVisible && xDomains[d.id],
   ) || []) as Spectrum1D[];
 
-  const { setActiveSpectrum } = useSetActiveSpectrumAction();
   return (
     <g clipPath={`url(#${displayerKey}clip-chart-1d)`} className="spectra">
       {spectra.map((d, i) => (
         <g key={d.id}>
           <Line display={d.display} id={d.id} data={get1DDataXY(d)} index={i} />
-          {verticalAlign === 'stack' && selectedTool === 'zoom' && (
-            <rect
-              css={style}
-              y={innerHeight - shiftY * i - BOX_SIZE / 2}
-              x={margin.left}
-              width={`${innerWidth}px`}
-              height={`${BOX_SIZE}px`}
-              onClick={(e) => {
-                setActiveSpectrum(e as unknown as MouseEvent, d.id);
-              }}
-              data-no-export="true"
-            />
-          )}
+          <HeadlightRectStackMode spectrumID={d.id} index={i} />
         </g>
       ))}
       {activeSpectra?.map((activeSpectrum) => (
         <use key={activeSpectrum.id} href={`#${activeSpectrum.id}`} />
       ))}
     </g>
+  );
+}
+
+interface HeadlightRectStackModeProps {
+  spectrumID: string;
+  index: number;
+}
+
+function HeadlightRectStackMode(props: HeadlightRectStackModeProps) {
+  const { spectrumID, index } = props;
+  const { step } = useBrushTracker();
+  const {
+    width,
+    margin,
+    height,
+    toolOptions: { selectedTool },
+  } = useChartData();
+  const { shiftY } = useScaleChecked();
+  const verticalAlign = useVerticalAlign();
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.bottom - SPECTRA_BOTTOM_MARGIN;
+
+  const { setActiveSpectrum } = useSetActiveSpectrumAction();
+
+  if (
+    verticalAlign !== 'stack' ||
+    selectedTool !== 'zoom' ||
+    step === 'brushing'
+  ) {
+    return null;
+  }
+
+  return (
+    <rect
+      css={style}
+      y={innerHeight - shiftY * index - BOX_SIZE / 2}
+      x={margin.left}
+      width={`${innerWidth}px`}
+      height={`${BOX_SIZE}px`}
+      onClick={(e) => {
+        setActiveSpectrum(e as unknown as MouseEvent, spectrumID);
+      }}
+      data-no-export="true"
+    />
   );
 }
 
