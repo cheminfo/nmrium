ab891a3ff8d112d7640abe42b54e60b454e0e7d6
hamed musallam <hamed.musallam@gmail.com>
2024-08-15 09:09:23 +0200

    feat: improve draggable dialog

--- src/component/elements/DraggableDialog.tsx
+++ src/component/elements/DraggableDialog.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+
 import {
   Button,
   Classes,
@@ -11,80 +13,154 @@ import {
   OverlayProps,
 } from '@blueprintjs/core';
 import { SmallCross } from '@blueprintjs/icons';
-import { useRef } from 'react';
+import { css, Global } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ReactNode, RefObject, useLayoutEffect, useRef } from 'react';
+
+const Container = styled.div`
+  min-height: 100%;
+  pointer-events: none;
+  user-select: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
 
 interface Position {
   x: number;
   y: number;
 }
-interface RectPosition extends Position {
-  clientX: number;
-  clientY: number;
+
+type DialogPosition =
+  | 'top'
+  | 'bottom'
+  | 'center'
+  | 'top-left'
+  | 'top-right'
+  | 'bottom-left'
+  | 'bottom-right'
+  | 'center-left'
+  | 'center-right';
+
+interface HeaderElement {
+  headerLeftElement?: ReactNode;
+  headerRightElement?: ReactNode;
 }
 
-type DraggableDialogProps = Omit<DialogProps, 'hasBackdrop'> &
-  Pick<OverlayProps, 'hasBackdrop'>;
+interface DraggableDialogProps
+  extends Omit<DialogProps, 'hasBackdrop'>,
+    Pick<OverlayProps, 'hasBackdrop'>,
+    HeaderElement {
+  position?: DialogPosition;
+}
 
+interface InnerDraggableDialogProps extends DraggableDialogProps {
+  innerDialogRef: RefObject<HTMLDivElement>;
+}
 export function DraggableDialog(props: DraggableDialogProps) {
+  const {
+    onClose,
+    headerLeftElement,
+    headerRightElement,
+    position,
+    ...otherProps
+  } = props;
+
+  const dialogRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <>
+      <Global
+        styles={css`
+          div[class*='-portal']:has(.draggable-portal) {
+            height: 100%;
+          }
+        `}
+      />
+      <Overlay2
+        {...otherProps}
+        childRef={dialogRef}
+        onClose={onClose}
+        className="draggable-portal"
+      >
+        <InnerDraggableDialog innerDialogRef={dialogRef} {...props} />
+      </Overlay2>
+    </>
+  );
+}
+function InnerDraggableDialog(props: InnerDraggableDialogProps) {
   const {
     children,
     containerRef,
-    role,
+    role = 'dialog',
     style,
     className = '',
     isCloseButtonShown,
     onClose,
     icon,
     title,
-    ...otherProps
+    headerLeftElement,
+    headerRightElement,
+    position = 'center',
+    innerDialogRef,
   } = props;
-  const offsetRef = useRef<RectPosition>({
+  const offsetRef = useRef<Position>({
     x: 0,
     y: 0,
-    clientX: 0,
-    clientY: 0,
   });
-  const originRef = useRef<Position | null>(null);
-  const dialogRef = useRef<HTMLDivElement>(null);
+  const containerRefInternal = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const updateTransform = () => {
+      if (!innerDialogRef.current || !containerRefInternal.current) {
+        return;
+      }
+      const dialogRect = innerDialogRef.current.getBoundingClientRect();
+      const parentContainerRect =
+        containerRefInternal.current.getBoundingClientRect();
+      const { transformX, transformY } = calculateTransform(position, {
+        dialogRect,
+        parentContainerRect,
+      });
+
+      innerDialogRef.current.style.transform = `translate(${transformX}px, ${transformY}px)`;
+    };
+
+    const frameId = requestAnimationFrame(updateTransform);
+
+    return () => {
+      cancelAnimationFrame(frameId);
+    };
+  }, [innerDialogRef, position]);
 
   function handleMouseDown(event: React.MouseEvent<HTMLDivElement>) {
     const target = event.currentTarget as HTMLElement;
 
-    if (!dialogRef.current) {
+    if (!containerRefInternal.current) {
       return;
     }
 
     const dialogRect = target.getBoundingClientRect();
+    const containerRect = containerRefInternal.current.getBoundingClientRect();
 
-    if (!originRef.current) {
-      originRef.current = {
-        x: dialogRect.x,
-        y: dialogRect.y,
-      };
-    }
+    const offsetX = event.clientX - (dialogRect.left - containerRect.left);
+    const offsetY = event.clientY - (dialogRect.top - containerRect.top);
 
     offsetRef.current = {
-      x: dialogRect.x,
-      y: dialogRect.y,
-      clientX: event.clientX,
-      clientY: event.clientY,
+      x: offsetX,
+      y: offsetY,
     };
 
     function handleMouseMove(event: MouseEvent) {
       target.style.cursor = 'move';
-
-      if (!dialogRef.current || !offsetRef.current || !originRef.current) {
+      if (!innerDialogRef.current || !offsetRef.current) {
         return;
       }
+      const offsetX = event.clientX - offsetRef.current.x;
+      const offsetY = event.clientY - offsetRef.current.y;
 
-      const newX =
-        offsetRef.current.x +
-        (event.clientX - (offsetRef.current.clientX + originRef.current.x));
-      const newY =
-        offsetRef.current.y +
-        (event.clientY - (offsetRef.current.clientY + originRef.current.y));
-
-      dialogRef.current.style.transform = `translate(${newX}px, ${newY}px)`;
+      innerDialogRef.current.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
     }
     function handleMouseUp() {
       target.style.cursor = 'default';
@@ -97,28 +173,35 @@ export function DraggableDialog(props: DraggableDialogProps) {
   }
 
   return (
-    <Overlay2 {...otherProps} childRef={dialogRef} onClose={onClose}>
+    <Container ref={containerRefInternal}>
       <div
-        className={Classes.DIALOG_CONTAINER}
         ref={
           containerRef === undefined
-            ? dialogRef
-            : mergeRefs(containerRef, dialogRef)
+            ? innerDialogRef
+            : mergeRefs(containerRef, innerDialogRef)
         }
+        className={`${Classes.DIALOG} ${className}`}
+        role={role}
+        style={{
+          ...style,
+          position: 'absolute',
+          margin: 0,
+        }}
       >
-        <div
-          className={`${Classes.DIALOG} ${className}`}
-          role={role}
-          style={style}
-        >
-          <Header
-            {...{ isCloseButtonShown, onClose, icon, title }}
-            onMouseDown={handleMouseDown}
-          />
-          {children}
-        </div>
+        <Header
+          {...{
+            isCloseButtonShown,
+            onClose,
+            icon,
+            title,
+            headerLeftElement,
+            headerRightElement,
+          }}
+          onMouseDown={handleMouseDown}
+        />
+        {children}
       </div>
-    </Overlay2>
+    </Container>
   );
 }
 
@@ -142,14 +225,22 @@ function CloseButton(
   );
 }
 
-function Header(
-  props: Pick<
-    DialogProps,
-    'icon' | 'title' | 'isCloseButtonShown' | 'onClose'
-  > &
-    Pick<HTMLDivProps, 'onMouseDown'>,
-) {
-  const { icon, title, onMouseDown, ...otherProps } = props;
+type HeaderProps = Pick<
+  DialogProps,
+  'icon' | 'title' | 'isCloseButtonShown' | 'onClose'
+> &
+  Pick<HTMLDivProps, 'onMouseDown'> &
+  HeaderElement;
+
+function Header(props: HeaderProps) {
+  const {
+    icon,
+    title,
+    onMouseDown,
+    headerRightElement,
+    headerLeftElement,
+    ...otherProps
+  } = props;
   if (title == null) {
     return null;
   }
@@ -157,8 +248,64 @@ function Header(
   return (
     <div className={Classes.DIALOG_HEADER} onMouseDown={onMouseDown}>
       <Icon icon={icon} size={IconSize.STANDARD} tabIndex={-1} />
+      {headerLeftElement}
       <H6>{title}</H6>
+      {headerRightElement}
       <CloseButton {...otherProps} />
     </div>
   );
+}
+
+function calculateTransform(
+  position: DialogPosition,
+  option: {
+    dialogRect: DOMRect;
+    parentContainerRect: DOMRect;
+  },
+): { transformX: number; transformY: number } {
+  let transformX = 0;
+  let transformY = 0;
+  const { parentContainerRect, dialogRect } = option;
+  switch (position) {
+    case 'top':
+      transformX = (parentContainerRect.width - dialogRect.width) / 2;
+      transformY = 0;
+      break;
+    case 'bottom':
+      transformX = (parentContainerRect.width - dialogRect.width) / 2;
+      transformY = parentContainerRect.height - dialogRect.height;
+      break;
+    case 'center':
+      transformX = (parentContainerRect.width - dialogRect.width) / 2;
+      transformY = (parentContainerRect.height - dialogRect.height) / 2;
+      break;
+    case 'top-left':
+      transformX = 0;
+      transformY = 0;
+      break;
+    case 'top-right':
+      transformX = parentContainerRect.width - dialogRect.width;
+      transformY = 0;
+      break;
+    case 'bottom-left':
+      transformX = 0;
+      transformY = parentContainerRect.height - dialogRect.height;
+      break;
+    case 'bottom-right':
+      transformX = parentContainerRect.width - dialogRect.width;
+      transformY = parentContainerRect.height - dialogRect.height;
+      break;
+    case 'center-left':
+      transformX = 0;
+      transformY = (parentContainerRect.height - dialogRect.height) / 2;
+      break;
+    case 'center-right':
+      transformX = parentContainerRect.width - dialogRect.width;
+      transformY = (parentContainerRect.height - dialogRect.height) / 2;
+      break;
+    default:
+      break;
+  }
+
+  return { transformX, transformY };
 }
