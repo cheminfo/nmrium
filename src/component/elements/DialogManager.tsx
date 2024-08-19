61a719269706a79a5155a6707cb8a915c6cd28a6
hamed musallam <hamed.musallam@gmail.com>
2024-08-19 08:20:54 +0200

    feat: implement dialog manager
    refactor: dialog manager
    
    refactor: generate a unique key based on dialog identifier and data to force re-render the dialog
    
    refactor: improve withDialog options
    
    refactor: rename 'getDialogStatus' to 'isDialogOpen'

---
+++ src/component/elements/DialogManager.tsx
@@ -0,0 +1,166 @@
+import { createContext, useContext, useMemo, useState } from 'react';
+
+interface DialogItem {
+  isOpen: boolean;
+  data: any;
+}
+
+type DialogStates = Record<string, DialogItem>;
+
+type ComponentIdentifier = string | React.ComponentType<any>;
+
+export interface DialogProps<T> {
+  dialogData: T;
+  onCloseDialog: () => void;
+  onOpenDialog: () => void;
+}
+
+interface DialogContextType {
+  openDialog: (identifier: ComponentIdentifier, data?: any) => void;
+  closeDialog: (identifier: ComponentIdentifier) => void;
+}
+
+interface DialogDataContextType {
+  dialogStates: DialogStates;
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+  getDialogData: <T>(identifier: ComponentIdentifier) => T | null;
+  isDialogOpen: (identifier: ComponentIdentifier) => boolean;
+  getDialog: (identifier: ComponentIdentifier) => DialogItem;
+}
+
+const DialogContext = createContext<DialogContextType | null>(null);
+const DialogDataContext = createContext<DialogDataContextType | null>(null);
+
+function getDialogIdentifier(componentIdentifier: ComponentIdentifier): string {
+  if (typeof componentIdentifier === 'string') {
+    return componentIdentifier;
+  } else {
+    const name = componentIdentifier.displayName || componentIdentifier.name;
+
+    if (!name) {
+      throw new Error('Dialog identifier should be defined');
+    }
+    return name;
+  }
+}
+
+export function DialogProvider({ children }) {
+  const [dialogStates, setDialog] = useState<DialogStates>({});
+
+  const state = useMemo(() => {
+    function closeDialog(componentIdentifier: ComponentIdentifier) {
+      const identifier = getDialogIdentifier(componentIdentifier);
+      setDialog((prevDialogState) => {
+        prevDialogState[identifier].isOpen = false;
+        return prevDialogState;
+      });
+    }
+
+    function openDialog(componentIdentifier: ComponentIdentifier, data?: any) {
+      const identifier = getDialogIdentifier(componentIdentifier);
+      setDialog((prevDialogState) => ({
+        ...prevDialogState,
+        [identifier]: { isOpen: true, data },
+      }));
+    }
+
+    return { openDialog, closeDialog };
+  }, []);
+
+  const dataState = useMemo(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+    function getDialogData<T>(
+      componentIdentifier: ComponentIdentifier,
+    ): T | null {
+      const identifier = getDialogIdentifier(componentIdentifier);
+      return dialogStates?.[identifier]?.data;
+    }
+    function isDialogOpen(componentIdentifier: ComponentIdentifier) {
+      const identifier = getDialogIdentifier(componentIdentifier);
+      return dialogStates?.[identifier]?.isOpen || false;
+    }
+    function getDialog(componentIdentifier: ComponentIdentifier) {
+      const identifier = getDialogIdentifier(componentIdentifier);
+      return dialogStates?.[identifier];
+    }
+
+    return { dialogStates, getDialog, getDialogData, isDialogOpen };
+  }, [dialogStates]);
+
+  return (
+    <DialogContext.Provider value={state}>
+      <DialogDataContext.Provider value={dataState}>
+        {children}
+      </DialogDataContext.Provider>
+    </DialogContext.Provider>
+  );
+}
+
+export function useDialog() {
+  const context = useContext(DialogContext);
+  if (!context) {
+    throw new Error('useDialog must be used within a DialogProvider');
+  }
+  return context;
+}
+
+export function useDialogData() {
+  const context = useContext(DialogDataContext);
+  if (!context) {
+    throw new Error('useDialogData must be used within a DialogProvider');
+  }
+  return context;
+}
+
+type WrapperParameters<P> = Omit<P, keyof DialogProps<any>>;
+
+interface WithDialogOptions {
+  dialogName?: string;
+  /** force re-render the dialog */
+  force?: boolean;
+}
+
+export function withDialog<P extends object>(
+  WrappedComponent: React.ComponentType<P>,
+  options: WithDialogOptions = {},
+) {
+  const { dialogName, force = false } = options;
+  return (props: WrapperParameters<P>) => {
+    const { closeDialog, openDialog } = useDialog();
+    const { getDialog } = useDialogData();
+    const dialogIdentifier =
+      dialogName || getDialogIdentifier(WrappedComponent);
+
+    const { isOpen, data } = getDialog(dialogIdentifier) || {
+      isOpen: false,
+      data: null,
+    };
+
+    function handleCloseDialog() {
+      closeDialog(dialogIdentifier);
+    }
+
+    function handleOpenDialog() {
+      openDialog(dialogIdentifier);
+    }
+
+    if (!isOpen) {
+      return null;
+    }
+
+    // Generate a unique key based on dialogIdentifier and data to force re-render the dialog
+    const key = force
+      ? `${dialogIdentifier}-${JSON.stringify(data)}`
+      : undefined;
+
+    return (
+      <WrappedComponent
+        key={key}
+        onCloseDialog={handleCloseDialog}
+        onOpenDialog={handleOpenDialog}
+        dialogData={data}
+        {...(props as P)}
+      />
+    );
+  };
+}
