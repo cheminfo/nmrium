fd4ee2e4f95f9a5328d7c1822ab453d047ae8626
hamed musallam <hamed.musallam@gmail.com>
2024-08-21 13:52:10 +0200

    refactor: create a custom tab title component with delete button

---
+++ src/component/elements/TabTitle.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import { Button, ButtonProps, Popover } from '@blueprintjs/core';
+import styled from '@emotion/styled';
+import { ReactNode } from 'react';
+
+interface TabTitleProps {
+  onDelete?: ButtonProps['onClick'];
+  children?: ReactNode;
+}
+
+export function TabTitle(props: TabTitleProps) {
+  const { onDelete, children } = props;
+
+  function handleOnDelete(event) {
+    event.stopPropagation();
+    onDelete?.(event);
+  }
+
+  return (
+    <Popover
+      minimal
+      popoverClassName="popover-tab"
+      position="top-left"
+      interactionKind="hover"
+      enforceFocus={false}
+      disabled={typeof onDelete !== 'function'}
+      content={
+        <TabCloseButton
+          icon="cross"
+          intent="danger"
+          className="tab-close-btn"
+          onClick={handleOnDelete}
+          small
+        />
+      }
+    >
+      {children}
+    </Popover>
+  );
+}
+
+const TabCloseButton = styled(Button)`
+  border-radius: 50%;
+  box-shadow:
+    rgb(9 30 66 / 25%) 0 4px 8px -2px,
+    rgb(9 30 66 / 8%) 0 0 0 1px !important;
+`;
