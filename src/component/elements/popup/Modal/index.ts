846150a1b1344d7a42d7e9f45f3e8fd5c54febc5
hamed musallam <hamed.musallam@gmail.com>
2024-08-22 11:21:52 +0200

    refactor: remove deprecated modal component and associated context

--- src/component/elements/popup/Modal/index.ts
+++
@@ -1,4 +0,0 @@
-export { positions, transitions } from '../options';
-export { useModal } from './Context';
-export { default as Modal } from './Modal';
-export { default as ModalProvider } from './Provider';
