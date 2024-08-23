265742b359fc09f91bb0093c3145ed9cc69be7ee
hamed musallam <hamed.musallam@gmail.com>
2024-08-23 10:55:22 +0200

    refactor: create format field controller

---
+++ src/component/elements/FormatField.tsx
@@ -0,0 +1,74 @@
+import { CSSProperties } from 'react';
+import { ControllerProps, FieldValues } from 'react-hook-form';
+
+import { CheckController } from './CheckController';
+import { Input2Controller } from './Input2Controller';
+import Label, { LabelStyle } from './Label';
+
+export const formatFieldInputStyle: CSSProperties = {
+  textAlign: 'center',
+};
+
+export const fieldLabelStyle: LabelStyle = {
+  label: {
+    flex: 4,
+    fontSize: '11px',
+    fontWeight: 'bold',
+    color: '#232323',
+  },
+  wrapper: {
+    flex: 8,
+  },
+};
+
+export interface FormatFieldProps<
+  TFieldValues extends FieldValues = FieldValues,
+> extends Pick<ControllerProps<TFieldValues>, 'control'> {
+  label: string;
+  checkFieldName?: ControllerProps<TFieldValues>['name'];
+  formatFieldName?: ControllerProps<TFieldValues>['name'];
+  disableFormat?: boolean;
+  hideFormatField?: boolean;
+  hideCheckField?: boolean;
+}
+
+export function FormatField<TFieldValues extends FieldValues = FieldValues>(
+  props: FormatFieldProps<TFieldValues>,
+) {
+  const {
+    label,
+    checkFieldName,
+    formatFieldName,
+    disableFormat = false,
+    hideFormatField = false,
+    hideCheckField = false,
+    control,
+  } = props;
+
+  return (
+    <Label title={label} style={fieldLabelStyle}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          ...(hideFormatField && { height: '25px' }),
+        }}
+      >
+        {!hideCheckField && checkFieldName ? (
+          <CheckController name={checkFieldName} control={control} />
+        ) : (
+          <div style={{ width: '23px' }} />
+        )}
+        {!hideFormatField && formatFieldName && (
+          <Input2Controller
+            style={formatFieldInputStyle}
+            name={formatFieldName}
+            control={control}
+            disabled={disableFormat}
+            controllerProps={{ rules: { required: true } }}
+          />
+        )}
+      </div>
+    </Label>
+  );
+}
