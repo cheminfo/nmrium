9f37e521f76db7e9abf54e7bda9bc9bd13130736
hamed musallam <hamed.musallam@gmail.com>
2024-08-23 10:56:25 +0200

    refactor: create nucleus preferences controller
    refactor: remove the old 'NucleusPreferences' component

--- src/component/panels/extra/preferences/NucleusPreferences.tsx
+++ src/component/panels/extra/preferences/NucleusPreferences.tsx
@@ -1,46 +1,56 @@
-import FormikColumnFormatField, {
-  ColumnFormatField,
-} from '../../../elements/formik/FormikColumnFormatField';
+import { ControllerProps, FieldValues } from 'react-hook-form';
+
+import { FormatField, FormatFieldProps } from '../../../elements/FormatField';
 
 import { NucleusGroup, NucleusGroupProps } from './NucleusGroup';
 
-export type NucleusPreferenceField = ColumnFormatField & {
+export type NucleusPreferenceField = Omit<FormatFieldProps, 'control'> & {
   id: string | number;
 };
 
-interface NucleusPreferencesProps extends NucleusGroupProps {
+interface NucleusPreferencesProps<
+  TFieldValues extends FieldValues = FieldValues,
+> extends NucleusGroupProps,
+    Pick<ControllerProps<TFieldValues>, 'control'> {
   fields: NucleusPreferenceField[];
 }
 
-export function NucleusPreferences({
-  nucleus,
-  fields,
-  renderTop,
-  renderBottom,
-}: NucleusPreferencesProps) {
+export function NucleusPreferences(props: NucleusPreferencesProps) {
+  const { nucleus, fields, renderTop, renderBottom, control } = props;
   return (
     <NucleusGroup
       nucleus={nucleus}
       renderTop={renderTop}
       renderBottom={renderBottom}
     >
-      {fields.map((field) => (
-        <FormikColumnFormatField
-          key={field.id}
-          label={field.label}
-          checkControllerName={
-            field.checkControllerName &&
-            `nuclei.${nucleus}.${field.checkControllerName}`
-          }
-          formatControllerName={
-            field.formatControllerName &&
-            `nuclei.${nucleus}.${field.formatControllerName}`
-          }
-          hideCheckField={field.hideCheckField}
-          hideFormatField={field.hideFormatField}
-          disableFormat={field.disableFormat}
-        />
-      ))}
+      {fields.map((field) => {
+        const {
+          id,
+          label,
+          checkFieldName,
+          formatFieldName,
+          hideCheckField,
+          hideFormatField,
+          disableFormat,
+        } = field;
+
+        return (
+          <FormatField
+            control={control}
+            key={id}
+            label={label}
+            checkFieldName={
+              checkFieldName && `nuclei.${nucleus}.${checkFieldName}`
+            }
+            formatFieldName={
+              formatFieldName && `nuclei.${nucleus}.${formatFieldName}`
+            }
+            hideCheckField={hideCheckField}
+            hideFormatField={hideFormatField}
+            disableFormat={disableFormat}
+          />
+        );
+      })}
     </NucleusGroup>
   );
 }
