5ef845ee3c7650136fbe5c1b39caa6c1bac4fc17
hamed musallam <hamed.musallam@gmail.com>
2024-08-26 13:56:41 +0200

    refactor: database preferences
    replace Formik with react-form-hook

--- src/component/panels/databasePanel/DatabasePreferences.tsx
+++ src/component/panels/databasePanel/DatabasePreferences.tsx
@@ -1,33 +1,19 @@
-import { Formik } from 'formik';
-import {
-  useEffect,
-  useCallback,
-  useImperativeHandle,
-  useRef,
-  forwardRef,
-} from 'react';
+import { useEffect, useCallback, forwardRef } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { usePreferences } from '../../context/PreferencesContext';
+import { ColorPickerDropdownController } from '../../elements/ColorPickerDropdownController';
+import { FormatField, fieldLabelStyle } from '../../elements/FormatField';
 import Label from '../../elements/Label';
-import FormikColorPickerDropdown from '../../elements/formik/FormikColorPickerDropdown';
-import FormikColumnFormatField, {
-  formatFieldInputStyle,
-  formatFieldLabelStyle,
-} from '../../elements/formik/FormikColumnFormatField';
-import FormikInput from '../../elements/formik/FormikInput';
+import { NumberInput2Controller } from '../../elements/NumberInput2Controller';
 import { usePanelPreferences } from '../../hooks/usePanelPreferences';
 import { PreferencesContainer } from '../extra/preferences/PreferencesContainer';
 import { PreferencesGroup } from '../extra/preferences/PreferencesGroup';
+import { useSettingImperativeHandle } from '../extra/utilities/settingImperativeHandle';
 
 function DatabasePreferences(props, ref) {
   const preferences = usePreferences();
-
-  const formRef = useRef<any>();
   const databasePreferences = usePanelPreferences('database');
-
-  useEffect(() => {
-    formRef.current.setValues(databasePreferences);
-  }, [databasePreferences]);
 
   const saveHandler = useCallback(
     (values) => {
@@ -39,82 +25,93 @@ function DatabasePreferences(props, ref) {
     [preferences],
   );
 
-  useImperativeHandle(ref, () => ({
-    saveSetting: () => {
-      formRef.current.submitForm();
-    },
-  }));
+  const { handleSubmit, reset, control } = useForm<any>({
+    defaultValues: databasePreferences,
+  });
+
+  useEffect(() => {
+    reset(databasePreferences);
+  }, [databasePreferences, reset]);
+
+  useSettingImperativeHandle(ref, handleSubmit, saveHandler);
 
   return (
     <PreferencesContainer>
-      <Formik initialValues={{}} onSubmit={saveHandler} innerRef={formRef}>
-        <>
-          <PreferencesGroup>
-            <FormikColumnFormatField
-              label="Preview jcamp"
-              checkControllerName="previewJcamp"
-              hideFormatField
-            />
-            <Label title="Color" style={formatFieldLabelStyle}>
-              <FormikColorPickerDropdown name="color" />
-            </Label>
-            <Label title="Margin bottom (px):" style={formatFieldLabelStyle}>
-              <FormikInput
-                name="marginBottom"
-                type="number"
-                style={formatFieldInputStyle}
-              />
-            </Label>
-          </PreferencesGroup>
-          <PreferencesGroup header="Table Preferences">
-            <FormikColumnFormatField
-              label="Structure"
-              checkControllerName="showSmiles"
-              hideFormatField
-            />
-            <FormikColumnFormatField
-              label="Solvent"
-              checkControllerName="showSolvent"
-              hideFormatField
-            />
-            <FormikColumnFormatField
-              label="Names"
-              checkControllerName="showNames"
-              hideFormatField
-            />
-            <FormikColumnFormatField
-              label="Range"
-              checkControllerName="range.show"
-              formatControllerName="range.format"
-            />
-            <FormikColumnFormatField
-              label="δ (ppm)"
-              checkControllerName="delta.show"
-              formatControllerName="delta.format"
-            />
-            <FormikColumnFormatField
-              label="Assignment"
-              checkControllerName="showAssignment"
-              hideFormatField
-            />
-            <FormikColumnFormatField
-              label="J (Hz)"
-              checkControllerName="coupling.show"
-              formatControllerName="coupling.format"
-            />
-            <FormikColumnFormatField
-              label="Multiplicity"
-              checkControllerName="showMultiplicity"
-              hideFormatField
-            />
-            <FormikColumnFormatField
-              label="Save as nmrium"
-              checkControllerName="allowSaveAsNMRium"
-              hideFormatField
-            />
-          </PreferencesGroup>
-        </>
-      </Formik>
+      <PreferencesGroup>
+        <FormatField
+          control={control}
+          label="Preview jcamp"
+          checkFieldName="previewJcamp"
+          hideFormatField
+        />
+        <Label title="Color" style={fieldLabelStyle}>
+          <ColorPickerDropdownController control={control} name="color" />
+        </Label>
+        <Label title="Margin bottom (px):" style={fieldLabelStyle}>
+          <NumberInput2Controller
+            control={control}
+            name="marginBottom"
+            min={0}
+            controllerProps={{ rules: { min: 0, required: true } }}
+          />
+        </Label>
+      </PreferencesGroup>
+      <PreferencesGroup header="Table Preferences">
+        <FormatField
+          control={control}
+          label="Structure"
+          checkFieldName="showSmiles"
+          hideFormatField
+        />
+        <FormatField
+          control={control}
+          label="Solvent"
+          checkFieldName="showSolvent"
+          hideFormatField
+        />
+        <FormatField
+          control={control}
+          label="Names"
+          checkFieldName="showNames"
+          hideFormatField
+        />
+        <FormatField
+          control={control}
+          label="Range"
+          checkFieldName="range.show"
+          formatFieldName="range.format"
+        />
+        <FormatField
+          control={control}
+          label="δ (ppm)"
+          checkFieldName="delta.show"
+          formatFieldName="delta.format"
+        />
+        <FormatField
+          control={control}
+          label="Assignment"
+          checkFieldName="showAssignment"
+          hideFormatField
+        />
+        <FormatField
+          control={control}
+          label="J (Hz)"
+          checkFieldName="coupling.show"
+          formatFieldName="coupling.format"
+        />
+        <FormatField
+          control={control}
+          label="Multiplicity"
+          checkFieldName="showMultiplicity"
+          hideFormatField
+        />
+        <FormatField
+          control={control}
+          label="Save as nmrium"
+          checkFieldName="allowSaveAsNMRium"
+          hideFormatField
+        />
+      </PreferencesGroup>
     </PreferencesContainer>
   );
 }
