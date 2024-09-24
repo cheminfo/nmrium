c850388e753892f69e70157dfe9d0fd3671a0438
hamed musallam <hamed.musallam@gmail.com>
2024-09-24 13:46:24 +0200

    feat: improve editing of Shift filter options
    refactor: enable action buttons when shift options are changed

--- src/component/panels/filtersPanel/Filters/index.tsx
+++ src/component/panels/filtersPanel/Filters/index.tsx
@@ -6,6 +6,7 @@ import ApodizationOptionsPanel from './ApodizationOptionsPanel';
 import BaseLineCorrectionOptionsPanel from './BaseLineCorrectionOptionsPanel';
 import PhaseCorrectionOptionsPanel from './PhaseCorrectionOptionsPanel';
 import PhaseCorrectionTwoDimensionsOptionsPanel from './PhaseCorrectionTwoDimensionsOptionsPanel';
+import ShiftOptionsPanel from './ShiftOptionsPanel';
 import ZeroFillingOptionsPanel from './ZeroFillingOptionsPanel';
 
 const {
@@ -14,6 +15,9 @@ const {
   phaseCorrectionTwoDimensions,
   zeroFilling,
   baselineCorrection,
+  shiftX,
+  shift2DX,
+  shift2DY,
 } = Filters;
 export const filterOptionPanels = {
   [apodization.id]: ApodizationOptionsPanel,
@@ -21,6 +25,9 @@ export const filterOptionPanels = {
   [zeroFilling.id]: ZeroFillingOptionsPanel,
   [phaseCorrectionTwoDimensions.id]: PhaseCorrectionTwoDimensionsOptionsPanel,
   [baselineCorrection.id]: BaseLineCorrectionOptionsPanel,
+  [shiftX.id]: ShiftOptionsPanel,
+  [shift2DX.id]: ShiftOptionsPanel,
+  [shift2DY.id]: ShiftOptionsPanel,
 };
 
 export const formLabelStyle: LabelStyle = {
