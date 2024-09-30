164113162fdc6647942718ad6a0c19aa19c1da16
hamed musallam <hamed.musallam@gmail.com>
2024-09-30 12:17:21 +0200

    refactor: improve convertToPixels and remove old code

--- src/component/elements/export/units.ts
+++ src/component/elements/export/units.ts
@@ -43,24 +43,4 @@ export function convertToPixels(value: number, unit: Unit, dpi = 1): number {
   const factor = conversionFactors[unit] || 1;
 
   return round(value * factor * dpi);
-  // switch (unit) {
-  //   case 'in':
-  //     return value * dpi;
-  //   case 'ft':
-  //     return value * 12 * dpi; // 1 feet = 12 inch
-  //   case 'yd':
-  //     return value * 36 * dpi; // 1 yard = 3 feet = 36 inches
-  //   case 'cm':
-  //     return (value * dpi) / 2.54; // 1 inch = 2.54 cm
-  //   case 'mm':
-  //     return (value * dpi) / 25.4; // 1 inch = 25.4 mm
-  //   case 'm':
-  //     return (value * dpi) / 0.0254;   // 1 meter = 12 inch
-  //   case 'pt':
-  //     return (value * dpi) / 72; // 1 point = 1/72 inch
-  //   case 'pc':
-  //     return (value * dpi) / 6; // 1 Picas = 1/6 inch
-  //   default:
-  //     return value; // Default to pixels if no unit is specified
-  // }
 }
