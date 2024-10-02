6b6f50d000fd24b0d900a528e8a82137e4be3d15
hamed musallam <hamed.musallam@gmail.com>
2024-10-02 11:05:28 +0200

    refactor: export tab in general settings

--- src/component/modal/setting/settings-tabs/ExportTabContent.tsx
+++ src/component/modal/setting/settings-tabs/ExportTabContent.tsx
@@ -1,6 +1,5 @@
 import { Button, Tag } from '@blueprintjs/core';
 import { ExportPreferences, UniversalExportSettings } from 'nmr-load-save';
-import { useDeferredValue, useRef, useState } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 
 import { CheckController } from '../../../elements/CheckController';
@@ -8,13 +7,9 @@ import { GroupPane } from '../../../elements/GroupPane';
 import Label from '../../../elements/Label';
 import { NumberInput2Controller } from '../../../elements/NumberInput2Controller';
 import { Select2Controller } from '../../../elements/Select2Controller';
-import {
-  convert,
-  convertToPixels,
-  units,
-} from '../../../elements/export/units';
+import { units } from '../../../elements/export/units';
+import { useExportConfigurer } from '../../../elements/export/useExportConfigurer';
 import { WorkspaceWithSource } from '../../../reducer/preferences/preferencesReducer';
-import { roundNumber } from '../../../utility/roundNumber';
 import { labelStyle } from '../../SaveAsModal';
 
 const INITIAL_VALUE: UniversalExportSettings = {
@@ -49,84 +44,27 @@ export function ExportOptions(props: ExportOptionsProps) {
   const { exportAs } = props;
   const path: `export.${keyof ExportPreferences}` = `export.${exportAs}`;
   const { setValue, control } = useFormContext<WorkspaceWithSource>();
-
-  const [isAspectRatioEnabled, enableAspectRatio] = useState(true);
-
-  const {
-    unit: currentUnit,
-    width: currentWidth,
-    height: currentHeight,
-    dpi: currentDPI,
-  } = useWatch({
+  const currentOptions = useWatch({
     name: path,
     defaultValue: INITIAL_VALUE,
   });
 
-  const refSize = useRef({
-    width: currentWidth,
-    height: currentHeight,
-    dpi: currentDPI,
-  });
+  const { unit } = currentOptions;
 
-  const previousUnit = useDeferredValue(currentUnit);
-
-  function transformSize(
-    value: number,
-    target: 'width' | 'height',
-    source: 'width' | 'height',
-  ) {
-    if (isAspectRatioEnabled) {
-      const { width, height } = refSize.current;
-      const aspectRation = width / height;
-      const newSize = value * aspectRation;
-      refSize.current[target] = newSize;
-      refSize.current[source] = value;
-      setValue(`${path}.${target}`, newSize);
-    } else {
-      refSize.current[source] = value;
-    }
-    return value;
-  }
-
-  function transformResolution(value) {
-    const { width, height, dpi } = refSize.current;
-    if (currentUnit === 'px') {
-      setValue(
-        `${path}.width`,
-        roundNumber((convertToPixels(width, currentUnit, 1) * value) / dpi, 2),
-      );
-      setValue(
-        `${path}.height`,
-        roundNumber((convertToPixels(height, currentUnit, 1) * value) / dpi, 2),
-      );
-    }
-
-    return value;
-  }
-
-  function handleUnitChange({ unit }) {
-    const w = convert(currentWidth, previousUnit, unit, currentDPI, {
-      precision: 2,
-    });
-    const h = convert(currentHeight, previousUnit, unit, currentDPI, {
-      precision: 2,
-    });
-    setValue(`${path}.width`, w);
-    setValue(`${path}.height`, h);
-    refSize.current = { width: w, height: h, dpi: currentDPI };
-  }
-
-  const exportWidth = convertToPixels(currentWidth, currentUnit, currentDPI, {
-    precision: 2,
-  });
-  const exportHeight = convertToPixels(currentHeight, currentUnit, currentDPI, {
-    precision: 2,
-  });
+  const {
+    widthInPixel,
+    heightInPixel,
+    isAspectRatioEnabled,
+    changeDPI,
+    enableAspectRatio,
+    changeSize,
+    changeUnit,
+  } = useExportConfigurer(currentOptions);
 
   return (
     <>
       <Label style={labelStyle} title="Description:">
-        <Tag>{`${exportWidth} px x ${exportHeight} px @ ${currentDPI}DPI`}</Tag>
+        <Tag>{`${widthInPixel} px x ${heightInPixel} px @ ${unit}DPI`}</Tag>
       </Label>
       <Label style={labelStyle} title="Size">
         <div style={{ display: 'flex', flexDirection: 'row' }}>
@@ -134,9 +72,13 @@ export function ExportOptions(props: ExportOptionsProps) {
             name={`${path}.width`}
             control={control}
             style={{ width: 100 }}
-            rightElement={<Tag>{currentUnit}</Tag>}
+            rightElement={<Tag>{unit}</Tag>}
             controllerProps={{ rules: { required: true } }}
-            transformValue={(value) => transformSize(value, 'height', 'width')}
+            transformValue={(value) => {
+              const newHeight = changeSize(value, 'height', 'width');
+              setValue(`${path}.height`, newHeight);
+              return value;
+            }}
             debounceTime={250}
             placeholder="width"
           />
@@ -154,9 +96,13 @@ export function ExportOptions(props: ExportOptionsProps) {
             name={`${path}.height`}
             control={control}
             style={{ width: 100 }}
-            rightElement={<Tag>{currentUnit}</Tag>}
+            rightElement={<Tag>{unit}</Tag>}
             controllerProps={{ rules: { required: true } }}
-            transformValue={(value) => transformSize(value, 'width', 'height')}
+            transformValue={(value) => {
+              const newWidth = changeSize(value, 'width', 'height');
+              setValue(`${path}.width`, newWidth);
+              return value;
+            }}
             debounceTime={250}
             placeholder="height"
           />
@@ -169,7 +115,11 @@ export function ExportOptions(props: ExportOptionsProps) {
           itemTextKey="name"
           itemValueKey="unit"
           items={units}
-          onItemSelect={handleUnitChange}
+          onItemSelect={({ unit }) => {
+            const newSize = changeUnit({ unit });
+            setValue(`${path}.width`, newSize.width);
+            setValue(`${path}.height`, newSize.height);
+          }}
         />
       </Label>
       <Label style={labelStyle} title="DPI">
@@ -178,7 +128,15 @@ export function ExportOptions(props: ExportOptionsProps) {
           control={control}
           style={{ width: 100 }}
           controllerProps={{ rules: { required: true } }}
-          transformValue={transformResolution}
+          transformValue={(value) => {
+            if (unit === 'px') {
+              const convertedValue = changeDPI(value);
+              setValue(`${path}.width`, convertedValue.width);
+              setValue(`${path}.height`, convertedValue.height);
+            }
+
+            return value;
+          }}
           debounceTime={250}
           placeholder="DPI"
         />
