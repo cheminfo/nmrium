bd286028f7cfad5d6163eb7f56170eb422f05f4f
jobo322 <jose.bolanos@correounivalle.edu.co>
2024-10-07 10:51:09 -0500

    fix: add shape to manual peak added

--- src/component/reducer/actions/PeaksActions.ts
+++ src/component/reducer/actions/PeaksActions.ts
@@ -7,7 +7,7 @@ import {
   ViewState,
   RangesViewState,
 } from 'nmr-load-save';
-import { Peak1D, OptionsXYAutoPeaksPicking } from 'nmr-processing';
+import { Peak1D, OptionsXYAutoPeaksPicking, mapPeaks } from 'nmr-processing';
 
 import {
   getShiftX,
@@ -86,9 +86,16 @@ function handleAddPeak(draft: Draft<State>, action: AddPeakAction) {
         originalX: candidatePeak.x - shiftX,
         x: candidatePeak.x,
         y: candidatePeak.y,
-        width: 0,
+        width: 0.001,
+        shape: {
+          kind: 'generalizedLorentzian',
+          fwhm: 1,
+          gamma: 0.5,
+        },
       };
-      (draft.data[index] as Spectrum1D).peaks.values.push(peak);
+      (draft.data[index] as Spectrum1D).peaks.values.push(
+        ...mapPeaks([peak], draft.data[index] as Spectrum1D),
+      );
     }
   }
 }
@@ -110,7 +117,6 @@ function handleAddPeaks(draft: Draft<State>, action: AddPeaksAction) {
       const peak = getClosePeak(datumOriginal, { from, to });
 
       const shiftX = getShiftX(draft.data[index] as Spectrum1D);
-
       if (peak && !datumOriginal.peaks.values.some((p) => p.x === peak.x)) {
         const newPeak: Peak1D = {
           id: v4(),
@@ -118,6 +124,11 @@ function handleAddPeaks(draft: Draft<State>, action: AddPeaksAction) {
           x: peak.x,
           y: peak.y,
           width: 0,
+          shape: {
+            kind: 'generalizedLorentzian',
+            fwhm: 0.001,
+            gamma: 0.5,
+          },
         };
         (draft.data[index] as Spectrum1D).peaks.values.push(newPeak);
       }
@@ -193,7 +204,6 @@ function handleAutoPeakPicking(
       windowFromIndex,
       windowToIndex,
     });
-
     datum.peaks.values = datum.peaks.values.concat(peaks);
   }
 }
