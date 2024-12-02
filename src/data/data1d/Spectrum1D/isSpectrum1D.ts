c4580265706df5a7cae318f45ee3e2994f3cd650
hamed musallam <hamed.musallam@gmail.com>
2024-12-02 21:14:00 +0100

    refactor: improve domain calculation

--- src/data/data1d/Spectrum1D/isSpectrum1D.ts
+++ src/data/data1d/Spectrum1D/isSpectrum1D.ts
@@ -3,3 +3,11 @@ import type { Spectrum, Spectrum1D } from 'nmr-load-save';
 export function isSpectrum1D(spectrum: Spectrum): spectrum is Spectrum1D {
   return spectrum.info.dimension === 1;
 }
+
+export function isFid1DSpectrum(spectrum: Spectrum): spectrum is Spectrum1D {
+  return isSpectrum1D(spectrum) && spectrum.info.isFid;
+}
+
+export function isFt1DSpectrum(spectrum: Spectrum): spectrum is Spectrum1D {
+  return isSpectrum1D(spectrum) && spectrum.info.isFt;
+}
