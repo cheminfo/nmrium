9c36807237bfafa92f4aa8fa2d7adcc9cc93b164
hamed musallam <hamed.musallam@gmail.com>
2025-01-24 12:00:59 +0100

    refactor: base react table component style

--- src/component/elements/ReactTable/Style.ts
+++ src/component/elements/ReactTable/Style.ts
@@ -1,42 +1,6 @@
-import { css } from '@emotion/react';
-
 import type { InputStyle } from '../Input.js';
 
-const ReactTableStyle = css`
-  border-spacing: 0;
-  border: 1px solid #dedede;
-  width: 100%;
-  max-height: 100%;
-  font-size: 12px;
-
-  .react-contextmenu-wrapper {
-    display: contents;
-  }
-
-  tr {
-    :last-child {
-      td {
-        border-bottom: 0;
-      }
-    }
-  }
-
-  th {
-    position: sticky;
-    background-color: white;
-    z-index: 0;
-    top: 0;
-  }
-  th,
-  td {
-    margin: 0;
-    padding: 0.15rem 0.4rem;
-    border-bottom: 1px solid #dedede;
-    border-right: 1px solid #dedede;
-  }
-`;
-
-const tableInputStyle: InputStyle = {
+export const tableInputStyle: InputStyle = {
   input: {
     width: '100%',
     backgroundColor: 'transparent',
@@ -47,4 +11,3 @@ const tableInputStyle: InputStyle = {
   },
 };
 
-export { ReactTableStyle, tableInputStyle };
