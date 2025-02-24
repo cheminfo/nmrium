cheminfo/nmrium

commit 683eb63d2ca33cf2b5886eb5793f1f2ac8d5af38

feat: improve save as PNG, SVG and copy to clipboard (#3252)
refactor: transform value in number input controller

* refactor: export as svg options

 refactor: export functions

refactor: useExport

* chore: fix prettier

* feat: improve save as png, SVG and copy to clipboard

* refactor: improve description

* chore: fix types

* refactor: improve convertToPixels and remove old code

* refactor: replace Canvas with OffscreenCanvas

* refactor: set input placeholder

* fear: utility function to convert between units

* refactor: improve export content

* chore: update nmr-load-save to version 1.0.0

* feat: add PNG, SVG, and clipboard export options to general settings

* feat: save PNG, SVG, and Clipboard export options in the workspace

* refactor: improve export manager

* refactor: export tab in general settings

* refactor: reorganize PNG, SVG, and Clipboard options  in general settings

* refactor: rename export titles

* refactor: change default dpi to 300

* fix: adjust text, line, path stroke width, and font size according to DPI

* refactor: change confirm button text to 'Copy' if the destination is clipboard

* refactor: call getBlob correctly in useNMRiumRefAPI

* feat: validate export options

* fix: rescale from 96 DPI to the target DPI in PNG format

* fix: copy to clipboard

* feat: improve export

* refactor: move page sizes and utility functions to separate file

* feat: improve export

* refactor: export option modal and general settings

* feat: press 'ENTER' validates the dialog and 'ESC' cancel it

* refactor: rename 'Advance' to 'Advanced'

src/component/panels/MoleculesPanel/MoleculePanelHeader.tsx
@@ -111,7 +111,10 @@ export default function MoleculePanelHeader({
   const moleculeKey = molecules?.[currentIndex]?.id;
   const saveAsSVGHandler = useCallback(() => {
     if (!rootRef) return;
-    exportAsSVG(rootRef, `molSVG${currentIndex} `, 'molFile');
+    exportAsSVG('molFile', {
+      rootElement: rootRef,
+      fileName: `molSVG${currentIndex} `,
+    });
   }, [rootRef, currentIndex]);
 
   const saveAsPNGHandler = useCallback(async () => {

src/component/2d/Viewer2D.tsx
@@ -15,7 +15,7 @@ import { MouseTracker } from '../EventsTrackers/MouseTracker';
 import { useChartData } from '../context/ChartContext';
 import { useDispatch } from '../context/DispatchContext';
 import { useMapKeyModifiers } from '../context/KeyModifierContext';
-import { usePrintPage } from '../elements/print';
+import { useViewportSize } from '../hooks/useViewportSize';
 import Spinner from '../loader/Spinner';
 import { options } from '../toolbar/ToolTypes';
 
@@ -285,13 +285,13 @@ interface ViewerResponsiveWrapperProps {
 export function ViewerResponsiveWrapper(props: ViewerResponsiveWrapperProps) {
   const dispatch = useDispatch();
   const { width, height, children } = props;
-  const printOptions = usePrintPage();
+  const size = useViewportSize();
 
   useEffect(() => {
-    if (!printOptions) {
+    if (!size) {
       dispatch({ type: 'SET_DIMENSIONS', payload: { width, height } });
     }
-  }, [width, height, dispatch, printOptions]);
+  }, [width, height, dispatch, size]);
 
   return children;
 }

src/component/EventsTrackers/KeysListenerTracker.tsx
@@ -9,9 +9,10 @@ import { useLoader } from '../context/LoaderContext';
 import { usePreferences } from '../context/PreferencesContext';
 import { useToaster } from '../context/ToasterContext';
 import { AlertButton, useAlert } from '../elements/Alert';
+import { useExportManagerAPI } from '../elements/export/ExportManager';
 import { HighlightEventSource, useHighlightData } from '../highlight/index';
 import { useCheckToolsVisibility } from '../hooks/useCheckToolsVisibility';
-import useExport from '../hooks/useExport';
+import { useExport } from '../hooks/useExport';
 import useToolsFunctions from '../hooks/useToolsFunctions';
 import SaveAsModal from '../modal/SaveAsModal';
 import { options } from '../toolbar/ToolTypes';
@@ -49,10 +50,11 @@ function KeysListenerTracker(props: KeysListenerTrackerProps) {
     changeDisplayViewModeHandler,
   } = useToolsFunctions();
 
-  const { saveToClipboardHandler, saveAsJSONHandler } = useExport();
+  const { saveAsJSONHandler } = useExport();
   const isToolVisible = useCheckToolsVisibility();
 
   const { highlight, remove } = useHighlightData();
+  const exportViewportAPI = useExportManagerAPI();
 
   const mouseIsOverDisplayer = useRef(false);
   useEffect(() => {
@@ -418,7 +420,10 @@ function KeysListenerTracker(props: KeysListenerTrackerProps) {
         if (!e.shiftKey && (e.metaKey || e.ctrlKey)) {
           switch (e.key) {
             case 'c':
-              void saveToClipboardHandler();
+              exportViewportAPI.current?.export({
+                format: 'png',
+                destination: 'clipboard',
+              });
               e.preventDefault();
               break;
             case 's':
@@ -476,6 +481,7 @@ function KeysListenerTracker(props: KeysListenerTrackerProps) {
       allow1DTool,
       changeDisplayViewModeHandler,
       dispatch,
+      exportViewportAPI,
       handleChangeOption,
       handleFullZoomOut,
       isToolVisible,
@@ -483,7 +489,6 @@ function KeysListenerTracker(props: KeysListenerTrackerProps) {
       openLoader,
       openSaveAsDialog,
       saveAsJSONHandler,
-      saveToClipboardHandler,
       toaster,
     ],
   );

src/component/context/ChartContext.tsx
@@ -1,7 +1,7 @@
 import { produce } from 'immer';
 import { createContext, useContext } from 'react';
 
-import { usePrintPage } from '../elements/print';
+import { useViewportSize } from '../hooks/useViewportSize';
 import { initialState, State } from '../reducer/Reducer';
 
 export const ChartContext = createContext<State>(initialState);
@@ -10,11 +10,11 @@ export const ChartDataProvider = ChartContext.Provider;
 export function useChartData() {
   const data = useContext(ChartContext);
 
-  const printOptions = usePrintPage();
+  const viewportSize = useViewportSize();
 
-  if (!printOptions) return data;
+  if (!viewportSize) return data;
 
-  const { width, height } = printOptions;
+  const { width, height } = viewportSize;
 
   const updatedData = produce(data, (draft) => {
     draft.width = width;

src/component/elements/NumberInput2Controller.tsx
@@ -11,6 +11,7 @@ interface NumberInput2ControllerProps<
     'control' | 'name' | 'render'
   >;
   noShadowBox?: boolean;
+  transformValue?: (value: number) => number;
 }
 
 const numberPattern = /^-?\d+(\.\d+)?$/;
@@ -26,6 +27,7 @@ export function NumberInput2Controller<
     intent = 'none',
     noShadowBox = false,
     style,
+    transformValue,
     ...otherInputProps
   } = props;
 
@@ -44,7 +46,11 @@ export function NumberInput2Controller<
             {...otherFieldProps}
             onValueChange={(valueAsNumber, valueAsString, event) => {
               if (numberPattern.test(valueAsString)) {
-                onChange(valueAsNumber);
+                onChange(
+                  typeof transformValue === 'function'
+                    ? transformValue(valueAsNumber)
+                    : valueAsNumber,
+                );
               } else {
                 onChange(valueAsString);
               }

src/component/elements/export/ExportContent.tsx
@@ -1,0 +1,183 @@
+/** @jsxImportSource @emotion/react */
+import { ExportSettings } from 'nmr-load-save';
+import { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+import { ExportOptionsModal } from './ExportOptionsModal';
+import { ExportSettingsProvider } from './ExportSettingsProvider';
+import { RenderDetector } from './RenderDetector';
+import { INITIAL_BASIC_EXPORT_OPTIONS } from './utilities/getExportOptions';
+import { getSizeInPixel } from './utilities/getSizeInPixel';
+import { transferDocumentStyles } from './utilities/transferDocumentStyles';
+
+const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
+
+export interface BaseExportProps {
+  onExportOptionsChange: (options: ExportSettings) => void;
+  defaultExportOptions?: ExportSettings;
+}
+
+interface RenderSizeOption {
+  width: number;
+  minWidth?: number;
+  rescale?: boolean;
+}
+interface BaseExportFrameProps {
+  children: ReactNode;
+  onExportReady: (
+    documentElement: HTMLElement,
+    options: ExportSettings,
+  ) => void;
+  renderOptions: RenderSizeOption;
+}
+interface InnerExportFrameProps extends BaseExportFrameProps {
+  exportOptions: ExportSettings;
+}
+interface ExportFrameProps
+  extends BaseExportFrameProps,
+    Partial<BaseExportProps> {
+  exportOptions?: ExportSettings;
+  onExportDialogClose?: () => void;
+  confirmButtonText?: string;
+}
+
+export function ExportContent(props: ExportFrameProps) {
+  const [innerExportOptions, setInnerExportOptions] =
+    useState<ExportSettings | null>();
+
+  const {
+    children,
+    exportOptions,
+    defaultExportOptions,
+    onExportOptionsChange,
+    onExportReady,
+    onExportDialogClose,
+    confirmButtonText = 'Save',
+    renderOptions,
+  } = props;
+
+  if (!innerExportOptions && exportOptions?.useDefaultSettings === false) {
+    return (
+      <ExportOptionsModal
+        isOpen
+        onCloseDialog={() => {
+          onExportDialogClose?.();
+        }}
+        onExportOptionsChange={(options) => {
+          onExportOptionsChange?.(options);
+          setInnerExportOptions(options);
+        }}
+        defaultExportOptions={defaultExportOptions}
+        confirmButtonText={confirmButtonText}
+      />
+    );
+  }
+
+  const options =
+    innerExportOptions ||
+    exportOptions ||
+    defaultExportOptions ||
+    INITIAL_BASIC_EXPORT_OPTIONS;
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: 'white',
+          opacity: 0.5,
+          left: 0,
+          top: 0,
+          position: 'fixed',
+          width: '100%',
+          height: '100%',
+          zIndex: 10,
+        }}
+      />
+      <InnerPrintFrame
+        exportOptions={options}
+        onExportReady={onExportReady}
+        renderOptions={renderOptions}
+      >
+        {children}
+      </InnerPrintFrame>
+    </>
+  );
+}
+
+export function InnerPrintFrame(props: InnerExportFrameProps) {
+  const { children, exportOptions, onExportReady, renderOptions } = props;
+  const { width, height } = getSizeInPixel(exportOptions);
+
+  const frameRef = useRef<HTMLIFrameElement>(null);
+  const [content, setContent] = useState<HTMLElement>();
+
+  const load = useCallback(() => {
+    const contentWindow = frameRef.current?.contentWindow;
+    if (!contentWindow) return;
+    const document = contentWindow.document;
+
+    setContent(document.body);
+
+    transferDocumentStyles(document);
+    return contentWindow;
+  }, []);
+
+  useEffect(() => {
+    if (!isFirefox) {
+      load();
+    }
+  }, [load]);
+
+  let widthInPixel = Math.round(width);
+  let heightInPixel = Math.round(height);
+
+  const {
+    width: baseRenderWidth,
+    rescale = true,
+    minWidth = 0,
+  } = renderOptions;
+
+  if (rescale) {
+    const renderWidth = Math.max(baseRenderWidth, minWidth);
+    widthInPixel = Math.round(renderWidth);
+    heightInPixel = Math.round((height / width) * renderWidth);
+  }
+
+  return (
+    <ExportSettingsProvider width={widthInPixel} height={heightInPixel}>
+      <iframe
+        ref={frameRef}
+        style={{
+          width: 0,
+          height: 0,
+          border: 'none',
+        }}
+        onLoad={() => {
+          if (isFirefox) {
+            load();
+          }
+        }}
+      >
+        {content &&
+          createPortal(
+            <RenderDetector
+              onRender={() => {
+                const document = frameRef.current?.contentWindow?.document;
+                if (document) {
+                  onExportReady(document.documentElement, exportOptions);
+                }
+              }}
+              style={{
+                width: `${widthInPixel}px`,
+                height: `${heightInPixel}px`,
+                margin: 0,
+              }}
+            >
+              {children}
+            </RenderDetector>,
+            content,
+          )}
+      </iframe>
+    </ExportSettingsProvider>
+  );
+}

src/component/elements/export/ExportManager.tsx
@@ -1,0 +1,171 @@
+import { ExportPreferences, ExportSettings } from 'nmr-load-save';
+import {
+  createContext,
+  ReactNode,
+  RefObject,
+  useContext,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
+
+import { useChartData } from '../../context/ChartContext';
+import { usePreferences } from '../../context/PreferencesContext';
+import { useExportViewPort } from '../../hooks/useExport';
+import { useWorkspaceExportSettings } from '../../hooks/useWorkspaceExportSettings';
+
+import { ExportContent } from './ExportContent';
+import { getSizeInPixel } from './utilities/getSizeInPixel';
+
+export type ExportFormat = 'png' | 'svg';
+export type ExportDestination = 'file' | 'clipboard';
+
+interface ExportOptions {
+  format: ExportFormat;
+  destination?: ExportDestination;
+}
+
+interface ExportManagerState {
+  export: (options: ExportOptions) => void;
+  exportFinished: () => void;
+}
+
+const ExportManagerContext =
+  createContext<RefObject<ExportManagerState> | null>(null);
+
+export function useExportManagerAPI() {
+  const context = useContext(ExportManagerContext);
+
+  if (!context) {
+    throw new Error(
+      'useExportManagerAPI must be used within an ExportManagerProvider',
+    );
+  }
+
+  return context;
+}
+
+interface ExportManagerProviderProps {
+  children: ReactNode;
+}
+
+export function ExportManagerProvider(props: ExportManagerProviderProps) {
+  const { children } = props;
+  const ref = useRef<ExportManagerState>(null);
+
+  return (
+    <ExportManagerContext.Provider value={ref}>
+      {children}
+    </ExportManagerContext.Provider>
+  );
+}
+
+interface ExportManagerControllerProps {
+  children: ReactNode;
+}
+
+export function ExportManagerController(props: ExportManagerControllerProps) {
+  const { children } = props;
+
+  const [exportOptions, triggerExport] = useState<ExportOptions | null>(null);
+  const exportRef = useExportManagerAPI();
+  const workspaceExportSettings = useWorkspaceExportSettings();
+  const { dispatch } = usePreferences();
+  useImperativeHandle(
+    exportRef,
+    () => ({
+      export: (options: ExportOptions) => {
+        triggerExport(options);
+      },
+      exportFinished: () => {
+        triggerExport(null);
+      },
+    }),
+    [],
+  );
+
+  const { saveAsPNGHandler, saveAsSVGHandler, copyPNGToClipboardHandler } =
+    useExportViewPort();
+
+  function handleCloseExportOptionsDialog() {
+    triggerExport(null);
+  }
+
+  async function handleExport(
+    targetElement: HTMLElement,
+    options: ExportSettings,
+  ) {
+    if (!exportOptions) {
+      return null;
+    }
+
+    const { format, destination = 'file' } = exportOptions;
+    let exportKey: keyof ExportPreferences = format;
+
+    const sizeInPixel = getSizeInPixel(options);
+
+    if (destination === 'file') {
+      switch (format) {
+        case 'png':
+          await saveAsPNGHandler(targetElement, sizeInPixel);
+          break;
+        case 'svg':
+          await saveAsSVGHandler(targetElement, sizeInPixel);
+          break;
+
+        default:
+          // eslint-disable-next-line no-console
+          console.error(
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+            `Unsupported format '${format}' for destination 'file'.`,
+          );
+          break;
+      }
+    }
+    if (destination === 'clipboard') {
+      exportKey = 'clipboard';
+      switch (format) {
+        case 'png':
+          await copyPNGToClipboardHandler(targetElement, sizeInPixel);
+          break;
+        default:
+          // eslint-disable-next-line no-console
+          console.error(
+            `Unsupported format '${format}' for destination 'clipboard'.`,
+          );
+          break;
+      }
+    }
+    handleCloseExportOptionsDialog();
+    dispatch({
+      type: 'CHANGE_EXPORT_SETTINGS',
+      payload: { key: exportKey, options },
+    });
+  }
+  const { width } = useChartData();
+
+  if (!exportOptions) return null;
+
+  const { format, destination = 'file' } = exportOptions;
+
+  let exportAs: keyof ExportPreferences = format;
+
+  if (destination === 'clipboard') {
+    exportAs = 'clipboard';
+  }
+
+  const settings = workspaceExportSettings[exportAs];
+
+  return (
+    <ExportContent
+      onExportReady={handleExport}
+      onExportDialogClose={handleCloseExportOptionsDialog}
+      exportOptions={settings}
+      defaultExportOptions={settings}
+      confirmButtonText={destination === 'clipboard' ? 'Copy' : 'Save'}
+      renderOptions={{ minWidth: 800, width, rescale: format !== 'svg' }}
+    >
+      {children}
+    </ExportContent>
+  );
+}

src/component/elements/export/ExportOptionsModal.tsx
@@ -1,0 +1,326 @@
+/** @jsxImportSource @emotion/react */
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  Radio,
+  RadioGroup,
+  SegmentedControl,
+  Tag,
+} from '@blueprintjs/core';
+import { css } from '@emotion/react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import {
+  AdvanceExportSettings,
+  BasicExportSettings,
+  ExportSettings,
+} from 'nmr-load-save';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+import ActionButtons from '../ActionButtons';
+import { CheckController } from '../CheckController';
+import Label, { LabelStyle } from '../Label';
+import { NumberInput2Controller } from '../NumberInput2Controller';
+import { Select2Controller } from '../Select2Controller';
+import { getSizesList, SizeItem } from '../print/pageSize';
+
+import { BaseExportProps } from './ExportContent';
+import { units } from './units';
+import { useExportConfigurer } from './useExportConfigurer';
+import { exportOptionValidationSchema } from './utilities/exportOptionValidationSchema';
+import {
+  getExportDefaultOptions,
+  getExportDefaultOptionsByMode,
+} from './utilities/getExportOptions';
+import { Mode, MODES } from './utilities/getModes';
+
+interface InnerExportOptionsModalProps extends BaseExportProps {
+  onCloseDialog: () => void;
+  confirmButtonText?: string;
+}
+interface ExportOptionsModalProps extends InnerExportOptionsModalProps {
+  isOpen: boolean;
+}
+
+export function ExportOptionsModal(props: ExportOptionsModalProps) {
+  const { isOpen, ...otherProps } = props;
+
+  if (!isOpen) return;
+
+  return <InnerExportOptionsModal {...otherProps} />;
+}
+
+const labelStyle: LabelStyle = {
+  label: {
+    color: '#232323',
+    width: '80px',
+  },
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+  },
+  container: { margin: '5px 0' },
+};
+
+function InnerExportOptionsModal(props: InnerExportOptionsModalProps) {
+  const {
+    onCloseDialog,
+    onExportOptionsChange,
+    confirmButtonText,
+    defaultExportOptions,
+  } = props;
+  const defaultValues = getExportDefaultOptions(defaultExportOptions);
+
+  const [mode, setMode] = useState<Mode>(defaultValues.mode);
+
+  const methods = useForm<ExportSettings>({
+    defaultValues,
+    resolver: yupResolver(exportOptionValidationSchema) as any,
+  });
+  const {
+    handleSubmit,
+    control,
+    watch,
+    setValue,
+    formState: { isValid, errors },
+    reset,
+    setFocus,
+  } = methods;
+
+  const watchSettings = watch();
+  const {
+    unit,
+    dpi = 0,
+    layout,
+  } = watchSettings as AdvanceExportSettings & BasicExportSettings;
+  const {
+    widthInPixel,
+    heightInPixel,
+    isAspectRatioEnabled,
+    changeDPI,
+    enableAspectRatio,
+    changeSize,
+    changeUnit,
+  } = useExportConfigurer(watchSettings);
+
+  let sizesList: SizeItem[] = [];
+
+  if (layout) {
+    sizesList = getSizesList(layout);
+  }
+
+  function handleChangeMode(mode) {
+    const options = defaultValues;
+    setMode(mode);
+    if (options.mode === mode) {
+      reset(defaultValues);
+    } else {
+      reset(getExportDefaultOptionsByMode(mode));
+    }
+  }
+
+  useEffect(() => {
+    const handleRenderComplete = () => {
+      setTimeout(() => {
+        if (mode === 'advance') {
+          setFocus('width');
+        } else {
+          setFocus('dpi');
+        }
+      }, 0);
+    };
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        void handleSubmit(onExportOptionsChange)();
+      }
+    };
+
+    globalThis.addEventListener('keydown', handleKeyDown);
+
+    const animationFrameId = requestAnimationFrame(handleRenderComplete);
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+      globalThis.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleSubmit, mode, onExportOptionsChange, setFocus]);
+
+  return (
+    <Dialog
+      isOpen
+      title="Export options"
+      onClose={onCloseDialog}
+      style={{ width: 600 }}
+      canEscapeKeyClose
+      autoFocus
+    >
+      <DialogBody
+        css={css`
+          background-color: white;
+        `}
+      >
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginBottom: '15px',
+          }}
+        >
+          <SegmentedControl
+            defaultValue="list"
+            inline
+            options={MODES}
+            value={mode}
+            onValueChange={handleChangeMode}
+          />
+        </div>
+
+        <Label style={labelStyle} title="Description:">
+          <Tag
+            intent={
+              !isValid && Object.keys(errors).length > 0 ? 'danger' : 'none'
+            }
+          >{`${widthInPixel} px x ${heightInPixel} px @ ${dpi}DPI`}</Tag>
+        </Label>
+        {mode === 'basic' && (
+          <>
+            <Label style={labelStyle} title="Size">
+              <Select2Controller
+                control={control}
+                name="size"
+                items={sizesList}
+              />
+            </Label>
+            <Label style={labelStyle} title="Layout">
+              <Controller
+                name="layout"
+                control={control}
+                render={({ field }) => {
+                  const { value, onChange, ...otherFieldProps } = field;
+                  return (
+                    <RadioGroup
+                      inline
+                      onChange={onChange}
+                      selectedValue={value}
+                      {...otherFieldProps}
+                    >
+                      <Radio label="Portrait" value="portrait" />
+                      <Radio label="Landscape" value="landscape" />
+                    </RadioGroup>
+                  );
+                }}
+              />
+            </Label>
+          </>
+        )}
+        {mode === 'advance' && (
+          <>
+            <Label style={labelStyle} title="Size">
+              <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <NumberInput2Controller
+                  name="width"
+                  control={control}
+                  style={{ width: 100 }}
+                  rightElement={<Tag>{unit}</Tag>}
+                  controllerProps={{ rules: { required: true } }}
+                  transformValue={(value) => {
+                    const newHeight = changeSize(value, 'height', 'width');
+                    setValue('height', newHeight);
+                    return value;
+                  }}
+                  debounceTime={250}
+                  placeholder="width"
+                />
+                <div style={{ padding: '0px 5px' }}>
+                  <Button
+                    icon="link"
+                    minimal
+                    active={isAspectRatioEnabled}
+                    onClick={() => {
+                      enableAspectRatio((prevFlag) => !prevFlag);
+                    }}
+                  />
+                </div>
+                <NumberInput2Controller
+                  name="height"
+                  control={control}
+                  style={{ width: 100 }}
+                  rightElement={<Tag>{unit}</Tag>}
+                  controllerProps={{ rules: { required: true } }}
+                  transformValue={(value) => {
+                    const newWidth = changeSize(value, 'width', 'height');
+                    setValue('width', newWidth);
+                    return value;
+                  }}
+                  debounceTime={250}
+                  placeholder="height"
+                />
+              </div>
+            </Label>
+            <Label style={labelStyle} title="Units">
+              <Select2Controller
+                control={control}
+                name="unit"
+                itemTextKey="name"
+                itemValueKey="unit"
+                items={units}
+                onItemSelect={({ unit }) => {
+                  const newSize = changeUnit({ unit });
+                  setValue('width', newSize.width);
+                  setValue('height', newSize.height);
+                }}
+              />
+            </Label>
+          </>
+        )}
+        <Label style={labelStyle} title="DPI">
+          <NumberInput2Controller
+            name="dpi"
+            control={control}
+            style={{ width: 100 }}
+            controllerProps={{ rules: { required: true } }}
+            transformValue={(value) => {
+              if (unit === 'px') {
+                const convertedValue = changeDPI(value);
+                setValue('width', convertedValue.width);
+                setValue('height', convertedValue.height);
+              }
+
+              return value;
+            }}
+            debounceTime={250}
+            placeholder="DPI"
+          />
+        </Label>
+      </DialogBody>
+      <DialogFooter>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'baseline',
+          }}
+        >
+          <CheckController
+            control={control}
+            name="useDefaultSettings"
+            style={{ fontSize: '12px' }}
+            label="Don't show options dialog next time and use those settings"
+          />
+
+          <ActionButtons
+            style={{ flexDirection: 'row-reverse', margin: 0 }}
+            onDone={() => {
+              void handleSubmit(onExportOptionsChange)();
+            }}
+            doneLabel={confirmButtonText}
+            onCancel={() => onCloseDialog?.()}
+          />
+        </div>
+      </DialogFooter>
+    </Dialog>
+  );
+}

commit aa68cfc51b1dff32276bd88ae919d9d738fe2b76

fix: molecule export as SVG
Bug was introduced by https://github.com/cheminfo/nmrium/commit/683eb63d2ca33cf2b5886eb5793f1f2ac8d5af38

src/component/panels/MoleculesPanel/MoleculePanelHeader.tsx
@@ -101,9 +101,9 @@ export default function MoleculePanelHeader({
   const moleculeKey = molecules?.[currentIndex]?.id;
   const saveAsSVGHandler = useCallback(() => {
     if (!rootRef) return;
-    exportAsSVG('molFile', {
+    exportAsSVG(`molSVG${currentIndex}`, {
       rootElement: rootRef,
-      fileName: `molSVG${currentIndex} `,
+      fileName: 'molFile',
     });
   }, [rootRef, currentIndex]);
 