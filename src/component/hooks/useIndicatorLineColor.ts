a7d8310d90555e27e6bf7188100d3ab1c01dfda9
Luc Patiny <luc@patiny.com>
2025-03-17 15:33:30 +0100

    chore: remove console.log

--- src/component/hooks/useIndicatorLineColor.ts
+++ src/component/hooks/useIndicatorLineColor.ts
@@ -2,6 +2,5 @@ import { usePreferences } from '../context/PreferencesContext.js';
 
 export function useIndicatorLineColor() {
   const { current } = usePreferences();
-  console.log(current?.spectraColors);
   return current?.spectraColors?.indicatorLineColor || '#2FFF0085';
 }
