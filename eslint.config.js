8ecc55a3fab2ac3b8187e7259762b14cc7910c4b
Michaël Zasso <targos@protonmail.com>
2025-03-24 11:50:47 +0100

    chore: add knip and remove unused dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -43,10 +43,6 @@ export default [
           message:
             "Use a deep import instead, like for example 'lodash/merge.js'",
         },
-        {
-          name: '@simbathesailor/use-what-changed',
-          message: 'Remove use-what-changed before committing the code',
-        },
         { name: 'openchemlib', message: "Use 'openchemlib/full'" },
         { name: 'react-ocl', message: "Use 'react-ocl/full" },
       ],
