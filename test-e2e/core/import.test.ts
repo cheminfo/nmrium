631e93d23c059dc10ba0dafb9c1d0a42905b38ce
Luc Patiny <luc@patiny.com>
2025-03-25 09:03:25 +0100

    chore: fix spelling

--- test-e2e/core/import.test.ts
+++ test-e2e/core/import.test.ts
@@ -58,7 +58,7 @@ test('should load and migrate .nmrium data from version 2 to version 3', async (
   // If the file was loaded successfully, there should be a 13C tab.
   await expect(nmrium.page.locator('_react=Tab[tabid = "13C"]')).toBeVisible();
 
-  await nmrium.clickPanel('Processings');
+  await nmrium.clickPanel('Processing');
 
   await expect(
     nmrium.page.locator('_react=FiltersSectionsPanel >> text=Apodization'),
