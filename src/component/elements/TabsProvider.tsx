a3a2fba10912f43f91aa53270700b9c47f07b48e
hamed musallam <hamed.musallam@gmail.com>
2025-05-01 15:44:57 +0200

    refactor: tabs context to control tabs

---
+++ src/component/elements/TabsProvider.tsx
@@ -0,0 +1,42 @@
+import type { ReactNode } from 'react';
+import { createContext, useContext, useState, useMemo } from 'react';
+
+type TabId = string | number;
+
+interface TabsContextOptions<T extends TabId = TabId> {
+  selectedTabId: T | undefined;
+  selectTab: (id: T) => void;
+}
+
+const TabsContext = createContext<TabsContextOptions | undefined>(undefined);
+
+interface TabsProviderProps<T extends TabId> {
+  children: ReactNode;
+  defaultSelectedTabId?: T;
+}
+
+export function TabsProvider<T extends TabId>(props: TabsProviderProps<T>) {
+  const { defaultSelectedTabId, children } = props;
+  const [selectedTabId, setSelectedTabId] = useState<T | undefined>(
+    defaultSelectedTabId,
+  );
+
+  const tabState = useMemo(() => {
+    return {
+      selectTab: (tabId) => tabId !== undefined && setSelectedTabId(tabId),
+      selectedTabId,
+    };
+  }, [selectedTabId]);
+
+  return (
+    <TabsContext.Provider value={tabState}>{children}</TabsContext.Provider>
+  );
+}
+
+export function useTabsController<T extends TabId>() {
+  const context = useContext(TabsContext);
+  if (!context) {
+    throw new Error('useTabsContext must be used within a TabsProvider');
+  }
+  return context as unknown as TabsContextOptions<T>;
+}
