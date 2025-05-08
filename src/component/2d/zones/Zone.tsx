4dce4bdfb76f539596201e363b451561aee4e096
hamed musallam <hamed.musallam@gmail.com>
2025-05-08 18:09:06 +0200

    refactor: add assignment label button

--- src/component/2d/zones/Zone.tsx
+++ src/component/2d/zones/Zone.tsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled';
 import type { Zone as ZoneType } from 'nmr-processing';
 import { useEffect, useState } from 'react';
 import { PiTextTBold } from 'react-icons/pi';
@@ -6,33 +5,21 @@ import { PiTextTBold } from 'react-icons/pi';
 import { checkZoneKind } from '../../../data/utilities/ZoneUtilities.js';
 import { useAssignment } from '../../assignment/AssignmentsContext.js';
 import { useShareData } from '../../context/ShareDataContext.js';
-import { SVGButton } from '../../elements/SVGButton.js';
+import { ActionsButtonsPopover } from '../../elements/ActionsButtonsPopover.js';
+import type { ActionsButtonsPopoverProps } from '../../elements/ActionsButtonsPopover.js';
 import { HighlightEventSource, useHighlight } from '../../highlight/index.js';
 import { useActiveSpectrumZonesViewState } from '../../hooks/useActiveSpectrumZonesViewState.js';
 import { useScale2DX, useScale2DY } from '../utilities/scale.js';
 
 import Signal from './Signal.js';
 
-const GroupContainer = styled.g`
-  .target {
-    visibility: hidden;
-  }
-
-  &:hover {
-    .target {
-      visibility: visible;
-    }
-  }
-`;
-
 interface ZoneProps {
   zoneData: ZoneType;
 }
 
-const buttonSize = 16;
-
 function Zone({ zoneData }: ZoneProps) {
   const { x, y, id, signals, assignment } = zoneData;
+
   const { setData } = useShareData<{ id: string }>();
   const assignmentZone = useAssignment(id);
   const { showZones } = useActiveSpectrumZonesViewState();
@@ -56,58 +43,51 @@ function Zone({ zoneData }: ZoneProps) {
   const zoneWidth = scaleX(x1) - scaleX(x2);
   const zoneHeight = scaleY(y2) - scaleY(y1);
 
+  const actionButtons: ActionsButtonsPopoverProps['buttons'] = [
+    {
+      icon: <PiTextTBold />,
+      onClick: () => setData({ id }),
+      intent: 'success',
+      title: 'Add assignment label',
+      visible: !assignment,
+    },
+  ];
+
   return (
-    <g
-      key={id}
-      onMouseEnter={() => {
-        assignmentZone.highlight();
-        highlightZone.show();
-      }}
-      onMouseLeave={() => {
-        assignmentZone.clearHighlight();
-        highlightZone.hide();
-      }}
+    <ActionsButtonsPopover
+      buttons={actionButtons}
+      targetTagName="g"
+      {...(assignment && { isOpen: false })}
     >
-      {showZones && (
-        <GroupContainer
-          transform={`translate(${scaleX(x2)},${scaleY(y1)})`}
-          pointer-events="all"
-        >
-          <g
-            data-no-export="true"
-            transform={`translate(-${buttonSize} -${buttonSize})`}
-            className="target"
-          >
+      <g
+        key={id}
+        onMouseEnter={() => {
+          assignmentZone.highlight();
+          highlightZone.show();
+        }}
+        onMouseLeave={() => {
+          assignmentZone.clearHighlight();
+          highlightZone.hide();
+        }}
+      >
+        {showZones && (
+          <g transform={`translate(${scaleX(x2)},${scaleY(y1)})`}>
             <rect
-              width={zoneWidth + buttonSize * 2}
-              height={zoneHeight + buttonSize * 2}
-              fill="transparent"
+              x="0"
+              width={zoneWidth}
+              height={zoneHeight}
+              className="integral-area"
+              fill={isActive ? '#ff6f0057' : '#0000000f'}
+              stroke={reduceOpacity ? '#343a40' : 'darkgreen'}
+              strokeWidth={reduceOpacity ? '0' : '1'}
             />
-            {!assignment && (
-              <SVGButton
-                renderIcon={(props) => <PiTextTBold {...props} />}
-                backgroundColor="green"
-                title="Add assignment label"
-                style={{ cursor: 'hand' }}
-                onClick={() => setData({ id })}
-              />
-            )}
           </g>
-          <rect
-            x="0"
-            width={zoneWidth}
-            height={zoneHeight}
-            className="integral-area"
-            fill={isActive ? '#ff6f0057' : '#0000000f'}
-            stroke={reduceOpacity ? '#343a40' : 'darkgreen'}
-            strokeWidth={reduceOpacity ? '0' : '1'}
-          />
-        </GroupContainer>
-      )}
-      {signals.map((_signal, i) => (
-        <Signal key={String(id + i)} signal={_signal} />
-      ))}
-    </g>
+        )}
+        {signals.map((_signal, i) => (
+          <Signal key={String(id + i)} signal={_signal} />
+        ))}
+      </g>
+    </ActionsButtonsPopover>
   );
 }
 
