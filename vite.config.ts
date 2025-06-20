1b76218989b35f6e506cb704fc59aaeb05d5cbeb
Michaël Zasso <targos@protonmail.com>
2025-06-20 12:34:56 +0200

    chore: remove vite plugin-react-swc (#3599)
    The latest version causes a bug in some components and we don't use it in other projects

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import analyze from 'rollup-plugin-analyzer';
 import type { AliasOptions } from 'vite';
 import { defineConfig } from 'vitest/config';
