8d0fb8eba01807a4d18ee6cbf468ceddd6c43de0
hamed musallam <hamed.musallam@gmail.com>
2025-07-28 11:24:56 +0200

    refactor(demo): explicitly initialize nmrium core

--- src/demo/views/AuoProcessingView.tsx
+++ src/demo/views/AuoProcessingView.tsx
@@ -1,19 +1,19 @@
-import type { NMRiumCore } from '@zakodium/nmrium-core';
+import init from '@zakodium/nmrium-core-plugins';
 import { FileCollection } from 'file-collection';
 import { useEffect, useState } from 'react';
-
-import { useCore } from '../../component/context/CoreContext.js';
 
 import type { BaseViewProps } from './BaseView.js';
 import BaseView from './BaseView.js';
 import { Loading } from './Loading.js';
+
+const core = init();
 
 interface ExternalLoadViewProps extends Omit<BaseViewProps, 'data'> {
   file: string;
   baseURL: string;
 }
 
-async function loadSpectrumFromURL(core: NMRiumCore, url: string) {
+async function loadSpectrumFromURL(url: string) {
   const { pathname: relativePath, origin: baseURL } = new URL(url);
   const source = {
     entries: [
@@ -34,15 +34,14 @@ async function loadSpectrumFromURL(core: NMRiumCore, url: string) {
 
 export default function ExternalLoadView(props: ExternalLoadViewProps) {
   const [data, setData] = useState();
-  const core = useCore();
 
   const { file, baseURL, ...otherProps } = props;
 
   useEffect(() => {
-    void loadSpectrumFromURL(core, file).then((d: any) => {
+    void loadSpectrumFromURL(file).then((d: any) => {
       setData(d);
     });
-  }, [baseURL, core, file]);
+  }, [baseURL, file]);
 
   if (!data) {
     return <Loading />;
