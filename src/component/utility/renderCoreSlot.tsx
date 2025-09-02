80562a3f7b239dfe00c6fb0088bbdba90bbdca09
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-09-02 15:33:59 +0200

    feat(slot): support `topbar.about_us.modal` slot with fallback (#3676)
    * update nmrium repository link in about us dialog

--- src/component/utility/renderCoreSlot.tsx
+++ src/component/utility/renderCoreSlot.tsx
@@ -1,11 +1,21 @@
 import type { NMRiumCore, SupportedUISlot } from '@zakodium/nmrium-core';
 import type { ReactNode } from 'react';
 
+/**
+ * Render all components registered in the given slot.
+ *
+ * @param core
+ * @param slot
+ * @param fallback - return fallback if no component is registered in the slot
+ */
 export function renderCoreSlot(
   core: NMRiumCore,
   slot: SupportedUISlot,
-): ReactNode[] {
-  return Array.from(core.slot(slot), ([key, Component]) => (
+  fallback?: ReactNode,
+): ReactNode {
+  const jsx = Array.from(core.slot(slot), ([key, Component]) => (
     <Component key={key} slot={slot} />
   ));
+
+  return jsx.length > 0 ? jsx : fallback;
 }
