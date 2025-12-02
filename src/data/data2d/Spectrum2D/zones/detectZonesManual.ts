a774e68d4343429b40e21ed6aa5c83e07d0bedbe
Michaël Zasso <targos@protonmail.com>
2025-12-02 14:58:07 +0100

    fix: improve zone detection typings (#3871)

--- src/data/data2d/Spectrum2D/zones/detectZonesManual.ts
+++ src/data/data2d/Spectrum2D/zones/detectZonesManual.ts
@@ -1,27 +1,29 @@
 import type { Zone } from '@zakodium/nmr-types';
+import type { Spectrum2D } from '@zakodium/nmrium-core';
 import { mapZones } from 'nmr-processing';
 
 import type { DetectionZonesOptions } from './getDetectionZones.js';
 import { getDetectionZones } from './getDetectionZones.js';
 
+type DetectZonesManualOptions = Pick<
+  DetectionZonesOptions,
+  'selectedZone' | 'thresholdFactor' | 'maxPercentCutOff'
+>;
+
 /**
  *
- * @param datum
- * @param {object} options
- * @param {object} options.selectedZone
- * @param {number} options.selectedZone.fromX
- * @param {number} options.selectedZone.fromY
- * @param {number} options.selectedZone.toX
- * @param {number} options.selectedZone.toY
- * @param {number} options.thresholdFactor
- * @param {boolean} options.convolutionByFFT
+ * @param spectrum
+ * @param options
  */
-export function detectZonesManual(datum: any, options: DetectionZonesOptions) {
-  const { spectralWidth, originFrequency } = datum.info;
-  options.tolerances = spectralWidth.map(
-    (sw: any, i: any) => sw * originFrequency[i],
-  );
-  const zones = getDetectionZones(datum, options);
+export function detectZonesManual(
+  spectrum: Spectrum2D,
+  options: DetectZonesManualOptions,
+): Zone[] {
+  const { spectralWidth, originFrequency } = spectrum.info;
+  const zones = getDetectionZones(spectrum, {
+    ...options,
+    tolerances: spectralWidth.map((sw, i) => sw * originFrequency[i]),
+  });
   let { fromX, fromY, toX, toY } = options.selectedZone;
   if (fromX > toX) [fromX, toX] = [toX, fromX];
   if (fromX > toX) [fromY, toY] = [toY, fromY];
@@ -30,5 +32,6 @@ export function detectZonesManual(datum: any, options: DetectionZonesOptions) {
     x: { from: fromX, to: toX },
     y: { from: fromY, to: toY },
   };
-  return mapZones(zones as Zone[], datum);
+
+  return mapZones(zones, spectrum);
 }
