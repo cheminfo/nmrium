cheminfo/nmrium

commit 9f724fdbe138447b88d8157d7501c2cb5292acd1

feat: improving and simplifying the 1D assignment (#3835)
- improving and simplifying the 1D assignment to reduce ambiguity and ensure more consistent behavior.
- Assignments will now be handled exclusively at the signal level, rather than at the range level.
- Ranges will generally contain a single signal, except in cases where the user manually edits and another signal under the same range.

refactor: rename 'Assign range' to 'Assign multiplet'

chore: add 'ibuprofen' sample for testing

refactor: remove check assignment at level of range

src/component/reducer/actions/RangesActions.ts
@@ -1,4 +1,4 @@
-import type { Range, SignalKind } from '@zakodium/nmr-types';
+import type { Range, Signal1D, SignalKind } from '@zakodium/nmr-types';
 import type {
   BoundingBox,
   RangesViewState,
@@ -80,19 +80,22 @@ interface DeleteSignalProps {
 }
 type DeleteSignalAction = ActionType<'DELETE_1D_SIGNAL', DeleteSignalProps>;
 
-interface UnlinkRangeProps {
+interface UnAssign1DSignalOptions {
   rangeKey?: string;
   spectrumId?: string; // If not specified, the currently active spectrum will be used by default.
   signalIndex?: number;
 }
-type UnlinkRangeAction = ActionType<'UNLINK_RANGE', UnlinkRangeProps>;
+type UnAssign1DSignalAction = ActionType<
+  'UNASSIGN_1D_SIGNAL',
+  UnAssign1DSignalOptions
+>;
 
-type AssignRangeAction = ActionType<
-  'ASSIGN_RANGE',
+type Assign1DSignalAction = ActionType<
+  'ASSIGN_1D_SIGNAL',
   {
     keys: TargetAssignKeys;
     spectrumId?: string;
-  } & Required<Pick<Range, 'diaIDs' | 'nbAtoms' | 'assignment'>>
+  } & Required<Pick<Signal1D, 'diaIDs' | 'nbAtoms' | 'assignment'>>
 >;
 type ChangeRangesAssignmentsLabelsByDiaIdsAction = ActionType<
   'CHANGE_ASSIGNMENT_LABEL_BY_DIAIDS',
@@ -128,7 +131,7 @@ type ChangeRangeRelativeValueAction = ActionType<
 >;
 type ChangeRangeSignalValueAction = ActionType<
   'CHANGE_RANGE_SIGNAL_VALUE',
-  { rangeID: string; signalID: string; value: number }
+  { rangeId: string; signalId: string; value: number }
 >;
 type UpdateRangAction = ActionType<'UPDATE_RANGE', { range: Range }>;
 type CutRangAction = ActionType<
@@ -149,9 +152,9 @@ type DeleteRangePeakAction = ActionType<
   'DELETE_RANGE_PEAK',
   { id: string; spectrumKey: string }
 >;
-type ChangeRangeAssignmentLabelAction = ActionType<
-  'CHANGE_RANGE_ASSIGNMENT_LABEL',
-  { rangeId: string; value: string; spectrumId?: string }
+type Change1DSignalAssignmentLabelAction = ActionType<
+  'CHANGE_1D_SIGNAL_ASSIGNMENT_LABEL',
+  { rangeId: string; signalId?: string; value: string; spectrumId?: string }
 >;
 
 type ChangeRangesViewFloatingBoxBoundingAction = ActionType<
@@ -172,8 +175,8 @@ export type RangesActions =
   | ChangeRangeSignalKindAction
   | SaveEditedRangeAction
   | DeleteSignalAction
-  | UnlinkRangeAction
-  | AssignRangeAction
+  | UnAssign1DSignalAction
+  | Assign1DSignalAction
   | ResizeRangeAction
   | ChangeRangeSumAction
   | AddRangeAction
@@ -183,7 +186,7 @@ export type RangesActions =
   | CutRangAction
   | ToggleRangesViewAction
   | DeleteRangePeakAction
-  | ChangeRangeAssignmentLabelAction
+  | Change1DSignalAssignmentLabelAction
   | ChangeRangesViewFloatingBoxBoundingAction
   | ChangeRangesAssignmentsLabelsByDiaIdsAction
   | ActionType<
@@ -192,9 +195,10 @@ export type RangesActions =
       | 'TOGGLE_RANGES_PEAKS_DISPLAYING_MODE'
     >;
 
-function getRangeIndex(spectrum: Spectrum1D, rangeID: string) {
-  return spectrum.ranges.values.findIndex((range) => range.id === rangeID);
+function getRangeIndex(spectrum: Spectrum1D, rangeId: string) {
+  return spectrum.ranges.values.findIndex((range) => range.id === rangeId);
 }
+
 //action
 function handleAutoRangesDetection(
   draft: Draft<State>,
@@ -378,7 +382,10 @@ function handleDeleteSignal(draft: Draft<State>, action: DeleteSignalAction) {
   deleteSignal1D(draft, action.payload);
 }
 
-function unlinkRange(draft: Draft<State>, options: UnlinkRangeProps = {}) {
+function clearSignalAssignment(
+  draft: Draft<State>,
+  options: UnAssign1DSignalOptions = {},
+) {
   const { spectrumId, rangeKey, signalIndex = -1 } = options;
 
   const spectrum = getSpectrum(draft, spectrumId);
@@ -401,42 +408,41 @@ function unlinkRange(draft: Draft<State>, options: UnlinkRangeProps = {}) {
 }
 
 //action
-function handleUnlinkRange(draft: Draft<State>, action: UnlinkRangeAction) {
-  unlinkRange(draft, action.payload);
+function handleUnAssign1DSignal(
+  draft: Draft<State>,
+  action: UnAssign1DSignalAction,
+) {
+  clearSignalAssignment(draft, action.payload);
 }
 
 //action
-function handleAssignRange(draft: Draft<State>, action: AssignRangeAction) {
+function handleAssign1DSignal(
+  draft: Draft<State>,
+  action: Assign1DSignalAction,
+) {
   const { keys, diaIDs, nbAtoms, spectrumId, assignment } = action.payload;
 
   const spectrum = getSpectrum(draft, spectrumId);
-  if (!isSpectrum1D(spectrum)) return;
 
-  if (keys.length === 1) {
-    const [{ index }] = keys;
-    const range = spectrum.ranges.values[index];
+  //TODO: Refactor TargetAssignKeys after completing the 2D assignment and remove keys.length !== 2 condition
+  if (!isSpectrum1D(spectrum) || keys.length !== 2) return;
 
-    if (!range) return;
+  if (keys.length !== 2) return;
+  const [{ index: rangeIndex }, { index: signalIndex }] = keys;
+  const range = spectrum.ranges.values[rangeIndex];
 
-    range.diaIDs = diaIDs;
-    range.nbAtoms = nbAtoms + (range.nbAtoms || 0);
+  if (!range) return;
 
-    if (assignment && !range.assignment) {
-      range.assignment = assignment;
-    }
-  } else {
-    const [{ index: rangeIndex }, { index: signalIndex }] = keys;
-    const range = spectrum.ranges.values[rangeIndex];
+  const signal = range.signals[signalIndex];
 
-    if (!range) return;
+  if (!signal) return;
 
-    const signal = range.signals[signalIndex];
-
-    if (!signal) return;
-
-    signal.diaIDs = diaIDs;
-    signal.nbAtoms = nbAtoms + (signal.nbAtoms || 0);
+  if (assignment && !signal.assignment) {
+    signal.assignment = assignment;
   }
+
+  signal.diaIDs = diaIDs;
+  signal.nbAtoms = nbAtoms + (signal.nbAtoms || 0);
 }
 //action
 function handleChangeRangesAssignmentLabelsByDiaIds(
@@ -454,19 +460,15 @@ function handleChangeRangesAssignmentLabelsByDiaIds(
   } = spectrum;
 
   for (const range of values) {
-    const { diaIDs: rangeDiaIDs = [], signals = [] } = range;
+    const { signals = [] } = range;
 
-    const matchesRange = rangeDiaIDs.some((id) => uniqueDiaIds.has(id));
-    const matchesSignals = signals.some((s) =>
-      s.diaIDs?.some((id) => uniqueDiaIds.has(id)),
-    );
-
-    if (
-      ((!range.assignment || previousAssignment === range.assignment) &&
-        matchesRange) ||
-      matchesSignals
-    ) {
-      range.assignment = assignment;
+    for (const signal of signals) {
+      if (
+        previousAssignment === signal.assignment &&
+        uniqueDiaIds.has(signal.id)
+      ) {
+        signal.assignment = assignment;
+      }
     }
   }
 }
@@ -541,14 +543,14 @@ function handleChangeRangeSignalValue(
   draft: Draft<State>,
   action: ChangeRangeSignalValueAction,
 ) {
-  const { rangeID, signalID, value } = action.payload;
+  const { rangeId, signalId, value } = action.payload;
 
   const spectrum = getSpectrum(draft);
   if (!isSpectrum1D(spectrum)) return;
 
   const shift = changeRangeSignal(spectrum, {
-    rangeID,
-    signalID,
+    rangeId,
+    signalId,
     newSignalValue: value,
   });
   rollbackSpectrumByFilter(draft, {
@@ -653,11 +655,11 @@ function handleDeleteRangePeak(
   }
 }
 
-function handleChangeRangeAssignmentLabel(
+function handleChange1DSignalAssignmentLabel(
   draft: Draft<State>,
-  action: ChangeRangeAssignmentLabelAction,
+  action: Change1DSignalAssignmentLabelAction,
 ) {
-  const { rangeId, value, spectrumId } = action.payload;
+  const { rangeId, signalId, value, spectrumId } = action.payload;
 
   const spectrum = getSpectrum(draft, spectrumId);
   if (!isSpectrum1D(spectrum)) return;
@@ -670,9 +672,15 @@ function handleChangeRangeAssignmentLabel(
   }
 
   const range = spectrum.ranges.values.find((range) => range.id === rangeId);
-  if (range) {
-    range.assignment = value;
-  }
+  if (!range) return;
+
+  const signal = signalId
+    ? range.signals.find((s) => s.id === signalId)
+    : range.signals[0];
+
+  if (!signal) return;
+
+  signal.assignment = value;
 }
 
 function handleChangeRangesViewFloatingBoxBounding(
@@ -694,11 +702,11 @@ function handleChangeRangesViewFloatingBoxBounding(
 export {
   deleteSignal1D,
   handleAddRange,
-  handleAssignRange,
+  handleAssign1DSignal,
   handleAutoRangesDetection,
   handleAutoSpectraRangesDetection,
+  handleChange1DSignalAssignmentLabel,
   handleChangePeaksDisplayingMode,
-  handleChangeRangeAssignmentLabel,
   handleChangeRangeRelativeValue,
   handleChangeRangeSignalKind,
   handleChangeRangeSignalValue,
@@ -713,6 +721,6 @@ export {
   handleResizeRange,
   handleSaveEditedRange,
   handleToggleRangesViewProperty,
-  handleUnlinkRange,
+  handleUnAssign1DSignal,
   handleUpdateRange,
 };

src/component/1d/SVGContent1D.tsx
@@ -1,5 +1,6 @@
 import { ClipPathContainer } from '../1d-2d/components/ClipPathContainer.js';
 import SpectrumInfoBlock from '../1d-2d/components/SpectrumInfoBlock.js';
+import { ShareDataProvider } from '../context/ShareDataContext.tsx';
 
 import { ApodizationLine } from './ApodizationLine.js';
 import ExclusionZonesAnnotations from './ExclusionZonesAnnotations.js';
@@ -30,9 +31,12 @@ export function SVGContent1D() {
         <IntegralsSeries />
         <Peaks peaksSource="peaks" />
         <RangesIntegrals />
-        <Ranges />
+        <ShareDataProvider>
+          <Ranges />
+          <MultiplicityTrees />
+        </ShareDataProvider>
+
         <Peaks peaksSource="ranges" />
-        <MultiplicityTrees />
         <MultiAnalysisRanges />
         <BaseLineZones />
         <ExclusionZonesAnnotations />

src/component/1d/multiplicityTree/MultiplicityTree.tsx
@@ -3,7 +3,6 @@ import type { Range } from '@zakodium/nmr-types';
 import type { Spectrum1D } from '@zakodium/nmrium-core';
 import { xFindClosestIndex } from 'ml-spectra-processing';
 import { useRef } from 'react';
-import { LuLink, LuUnlink } from 'react-icons/lu';
 
 import { isSpectrum1D } from '../../../data/data1d/Spectrum1D/index.js';
 import type { AssignmentsData } from '../../assignment/AssignmentsContext.js';
@@ -11,12 +10,12 @@ import { useAssignment } from '../../assignment/AssignmentsContext.js';
 import { useChartData } from '../../context/ChartContext.js';
 import { useDispatch } from '../../context/DispatchContext.js';
 import { useScaleChecked } from '../../context/ScaleContext.js';
-import type { ActionsButtonsPopoverProps } from '../../elements/ActionsButtonsPopover.js';
 import { ActionsButtonsPopover } from '../../elements/ActionsButtonsPopover.js';
 import { useHighlight } from '../../highlight/index.js';
 import { useActiveSpectrum } from '../../hooks/useActiveSpectrum.ts';
 import useSpectrum from '../../hooks/useSpectrum.js';
 import { useIsInset } from '../inset/InsetProvider.js';
+import { useAssignmentsPopoverActionsButtons } from '../ranges/useAssignmentsPopoverActionsButtons.tsx';
 
 import type { TreeNodes } from './generateTreeNodes.js';
 import { generateTreeNodes } from './generateTreeNodes.js';
@@ -76,7 +75,15 @@ function Tree(props: TreeProps) {
     signalIndex,
     range,
     startY: originStartY,
-    treeNodes: { multiplicity = '', nodes, min, max, signalKey, diaIDs },
+    treeNodes: {
+      multiplicity = '',
+      nodes,
+      min,
+      max,
+      signalKey,
+      diaIDs,
+      assignment: assignmentLabel,
+    },
   } = props;
   const { from, to } = range;
   const { width } = useChartData();
@@ -118,6 +125,18 @@ function Tree(props: TreeProps) {
     startY,
   });
 
+  const hasDiaIDs = Array.isArray(diaIDs) && diaIDs.length > 0;
+  const isAssignmentActive = assignment.isActive;
+
+  const actionsButtons = useAssignmentsPopoverActionsButtons({
+    isUnAssignButtonVisible: isAssignmentActive || hasDiaIDs,
+    isAssignLabelButtonVisible: !assignmentLabel,
+    isUnAssignLabelButtonVisible: !!assignmentLabel,
+    onAssign: assignHandler,
+    onUnAssign: unAssignHandler,
+    rangeId: range.id,
+  });
+
   if (!multiplicity) return null;
 
   function assignHandler() {
@@ -127,38 +146,20 @@ function Tree(props: TreeProps) {
 
   function unAssignHandler() {
     dispatch({
-      type: 'UNLINK_RANGE',
+      type: 'UNASSIGN_1D_SIGNAL',
       payload: {
         rangeKey: range.id,
         signalIndex,
       },
     });
   }
-  const hasDiaIDs = Array.isArray(diaIDs) && diaIDs.length > 0;
-  const isAssignmentActive = assignment.isActive;
+
   const isHighlighted = highlight.isActive || isAssignmentActive;
   const padding = boxPadding * widthRatio;
   const x = scaleX()(max) - padding;
   const y = startY - headTextMargin - headerTextSize - padding;
   const boxWidth = treeWidth + padding * 2;
   const boxHeight = treeHeight + headTextMargin + headerTextSize + padding * 2;
-
-  const actionsButtons: ActionsButtonsPopoverProps['buttons'] = [
-    {
-      icon: <LuLink />,
-      onClick: assignHandler,
-      intent: 'success',
-      title: 'Assign range',
-    },
-    {
-      icon: <LuUnlink />,
-      onClick: () => unAssignHandler(),
-      intent: 'danger',
-      title: 'Unassign range',
-      visible: isAssignmentActive || hasDiaIDs,
-    },
-  ];
-
   const isOpen = isAssignBtnTrigged.current ? isAssignmentActive : undefined;
 
   return (

src/component/1d/multiplicityTree/generateTreeNodes.ts
@@ -19,6 +19,7 @@ export interface TreeNodes {
   multiplicity?: string;
   nodes: TreeNode[];
   diaIDs?: string[];
+  assignment?: string;
   min: number;
   max: number;
 }
@@ -78,7 +79,14 @@ export function generateTreeNodes(range: Range, spectrumData: Spectrum1D) {
   for (const signal of signals || []) {
     nodes = [];
 
-    const { multiplicity, js, delta, id: signalKey, diaIDs } = signal;
+    const {
+      multiplicity,
+      js,
+      delta,
+      id: signalKey,
+      diaIDs,
+      assignment,
+    } = signal;
     const minMax = { min: delta, max: delta };
 
     nodes.push({
@@ -104,6 +112,7 @@ export function generateTreeNodes(range: Range, spectrumData: Spectrum1D) {
       rangeKey,
       signalKey,
       diaIDs,
+      assignment,
       multiplicity,
       nodes,
       ...minMax,

src/component/1d/ranges/AssignmentLabel.tsx
@@ -1,4 +1,4 @@
-import type { Range } from '@zakodium/nmr-types';
+import type { Range, Signal1D } from '@zakodium/nmr-types';
 import { useRef } from 'react';
 
 import { FieldEdition } from '../../1d-2d/FieldEdition.js';
@@ -11,20 +11,22 @@ const marginTop = 15;
 
 interface AssignmentLabelProps {
   range: Range;
+  signal: Signal1D;
   width: number;
   stackIndex: number;
 }
 
 export function AssignmentLabel(props: AssignmentLabelProps) {
-  const { range, width, stackIndex } = props;
-  const { id, assignment } = range;
+  const { range, width, stackIndex, signal } = props;
+  const { id } = range;
+  const { assignment } = signal;
   const { showAssignmentsLabels } = useActiveSpectrumRangesViewState();
   const dispatch = useDispatch();
   const textRef = useRef<SVGTextElement>(null);
   const { margin } = useChartData();
   const { isNewAssignment, dismissNewLabel } = useTriggerNewAssignmentLabel(id);
 
-  if ((!range.assignment || !showAssignmentsLabels) && !isNewAssignment) {
+  if ((!assignment || !showAssignmentsLabels) && !isNewAssignment) {
     return null;
   }
 
@@ -32,10 +34,11 @@ export function AssignmentLabel(props: AssignmentLabelProps) {
     dismissNewLabel();
 
     dispatch({
-      type: 'CHANGE_RANGE_ASSIGNMENT_LABEL',
+      type: 'CHANGE_1D_SIGNAL_ASSIGNMENT_LABEL',
       payload: {
         value,
         rangeId: id,
+        signalId: signal.id,
       },
     });
   }

src/component/1d/ranges/Atoms.tsx
@@ -10,8 +10,8 @@ const fontSize = 12;
 const marginTop = 45;
 
 function getDiaIds(range: Range) {
-  const { diaIDs = [], signals } = range;
-  const ids = diaIDs.slice();
+  const { signals } = range;
+  const ids = [];
   for (const signal of signals) {
     if (signal?.diaIDs) {
       ids.push(...signal.diaIDs);

src/component/1d/ranges/Range.tsx
@@ -1,7 +1,5 @@
 import type { Range as RangeType } from '@zakodium/nmr-types';
 import { useRef } from 'react';
-import { LuLink, LuUnlink } from 'react-icons/lu';
-import { PiTextTBold, PiTextTSlashBold } from 'react-icons/pi';
 
 import {
   isSpectrum1D,
@@ -17,8 +15,6 @@ import { filterAssignedIDs } from '../../assignment/utilities/filterAssignedIDs.
 import { useChartData } from '../../context/ChartContext.js';
 import { useDispatch } from '../../context/DispatchContext.js';
 import { useLogger } from '../../context/LoggerContext.js';
-import { useShareData } from '../../context/ShareDataContext.js';
-import type { ActionsButtonsPopoverProps } from '../../elements/ActionsButtonsPopover.js';
 import { ActionsButtonsPopover } from '../../elements/ActionsButtonsPopover.js';
 import { useDialogData } from '../../elements/DialogManager.js';
 import { ResizerWithScale } from '../../elements/ResizerWithScale.js';
@@ -29,44 +25,38 @@ import { useHighlightColor } from '../../hooks/useHighlightColor.js';
 import { useResizerStatus } from '../../hooks/useResizerStatus.js';
 import useSpectrum from '../../hooks/useSpectrum.js';
 import { EditRangeModal } from '../../modal/editRange/EditRangeModal.js';
+import type { StackOverlappingLabelsMapReturnType } from '../../utility/stackOverlappingLabels.ts';
 import { useIsInset } from '../inset/InsetProvider.js';
 import { IntegralIndicator } from '../integral/IntegralIndicator.js';
 import { useScaleX } from '../utilities/scale.js';
 
-import { AssignmentLabel } from './AssignmentLabel.js';
+import { AssignmentLabel } from './AssignmentLabel.tsx';
 import { Atoms } from './Atoms.js';
+import { useAssignmentsPopoverActionsButtons } from './useAssignmentsPopoverActionsButtons.tsx';
 
 interface RangeProps {
   selectedTool: string;
   range: RangeType;
-  assignmentLabelStackIndex: number;
   relativeFormat: string;
+  signalsStackIndexes: StackOverlappingLabelsMapReturnType;
 }
 
 const minWidth = 10;
 
 function Range(options: RangeProps) {
-  const { range, selectedTool, relativeFormat, assignmentLabelStackIndex } =
-    options;
-  const {
-    id,
-    integration,
-    signals,
-    from,
-    to,
-    diaIDs: rangeDiaIDs,
-    assignment,
-  } = range;
+  const { range, selectedTool, relativeFormat, signalsStackIndexes } = options;
+  const { id, integration, signals, from, to } = range;
   const isInset = useIsInset();
   const spectrum = useSpectrum();
   const isAssignBtnTrigged = useRef(false);
   const { margin } = useChartData();
+  const signal = signals?.[0];
 
   const highlightColor = useHighlightColor();
   const assignmentData = useAssignmentContext();
-  const assignmentRange = useAssignment(id);
+  const signalAssignment = useAssignment(signal?.id || '');
   const highlightRange = useHighlight(
-    [id].concat(assignmentRange.assignedDiaIds?.x || []).concat(
+    [id].concat(signalAssignment.assignedDiaIds?.x || []).concat(
       filterAssignedIDs(
         assignmentData.data,
         signals.map((_signal) => _signal.id),
@@ -106,80 +96,53 @@ function Range(options: RangeProps) {
   }
 
   function mouseEnterHandler() {
-    assignmentRange.highlight('x');
+    signalAssignment.highlight('x');
     highlightRange.show();
   }
 
   function mouseLeaveHandler() {
-    assignmentRange.clearHighlight();
+    signalAssignment.clearHighlight();
     highlightRange.hide();
   }
 
   function assignHandler() {
     if (!isBlockedByEditing) {
       isAssignBtnTrigged.current = true;
-      assignmentRange.activate('x');
+      signalAssignment.activate('x');
     }
   }
 
-  function unAssignHandler(signalIndex = -1) {
+  function unAssignHandler() {
     dispatch({
-      type: 'UNLINK_RANGE',
+      type: 'UNASSIGN_1D_SIGNAL',
       payload: {
         rangeKey: range.id,
-        signalIndex,
+        signalIndex: 0,
       },
     });
   }
-  const isAssignmentActive = assignmentRange.isActive;
+  const isAssignmentActive = signalAssignment.isActive;
   const opacity = getOpacityBasedOnSignalKind(range);
 
   const isHighlighted =
     isBlockedByEditing || highlightRange.isActive || isAssignmentActive;
 
-  const isAssigned = isRangeAssigned(range);
+  const isSignalAssigned = isRangeAssigned(range);
   const isResizingActive = useResizerStatus('rangePicking');
-  const { setData: addNewAssignmentLabel } = useShareData();
+  const hasOnlyOneSignal = signals.length === 1;
+  const isAssigned =
+    hasOnlyOneSignal && signal.diaIDs ? signal.diaIDs.length > 0 : false;
 
-  function removeAssignmentLabel() {
-    dispatch({
-      type: 'CHANGE_RANGE_ASSIGNMENT_LABEL',
-      payload: {
-        value: '',
-        rangeId: id,
-      },
-    });
-  }
-
-  const actionsButtons: ActionsButtonsPopoverProps['buttons'] = [
-    {
-      icon: <LuLink />,
-      onClick: assignHandler,
-      intent: 'success',
-      title: 'Assign range',
-    },
-    {
-      icon: <LuUnlink />,
-      onClick: () => unAssignHandler(),
-      intent: 'danger',
-      title: 'Unassign range',
-      visible: !!(isAssignmentActive || rangeDiaIDs),
-    },
-    {
-      icon: <PiTextTBold />,
-      onClick: () => addNewAssignmentLabel(range.id),
-      intent: 'success',
-      title: 'Add assignment label',
-      visible: !assignment,
-    },
-    {
-      icon: <PiTextTSlashBold />,
-      onClick: removeAssignmentLabel,
-      intent: 'danger',
-      title: 'Remove assignment label',
-      visible: !!assignment,
-    },
-  ];
+  const actionsButtons = useAssignmentsPopoverActionsButtons({
+    isToggleMultiplicityTreeButtonVisible: !hasOnlyOneSignal,
+    isAssignButtonVisible: hasOnlyOneSignal,
+    isUnAssignButtonVisible: isAssignmentActive || isSignalAssigned,
+    isAssignLabelButtonVisible: hasOnlyOneSignal && !signal.assignment,
+    isUnAssignLabelButtonVisible: hasOnlyOneSignal && !!signal.assignment,
+    onAssign: assignHandler,
+    onUnAssign: unAssignHandler,
+    rangeId: range.id,
+  });
 
   const isOpen = isAssignBtnTrigged.current ? isAssignmentActive : undefined;
 
@@ -237,12 +200,16 @@ function Range(options: RangeProps) {
                   fill={isHighlighted || isActive ? '#ff6f0057' : 'transparent'}
                   data-no-export="true"
                 />
+                {range.signals.map((signal) => (
+                  <AssignmentLabel
+                    key={signal.id}
+                    stackIndex={signalsStackIndexes?.[signal.id] || 0}
+                    range={range}
+                    width={rangeWidth}
+                    signal={signal}
+                  />
+                ))}
 
-                <AssignmentLabel
-                  stackIndex={assignmentLabelStackIndex}
-                  range={range}
-                  width={rangeWidth}
-                />
                 <Atoms range={range} x={rangeWidth / 2} />
 
                 {showIntegralsValues && (

src/component/1d/ranges/Ranges.tsx
@@ -7,7 +7,6 @@ import { memo } from 'react';
 
 import { useChartData } from '../../context/ChartContext.js';
 import { useScaleChecked } from '../../context/ScaleContext.js';
-import { ShareDataProvider } from '../../context/ShareDataContext.js';
 import { usePanelPreferences } from '../../hooks/usePanelPreferences.js';
 import useSpectrum from '../../hooks/useSpectrum.js';
 import { useTextMetrics } from '../../hooks/useTextMetrics.ts';
@@ -26,6 +25,8 @@ const labelSize = 14;
 type ProcessedRange = RangeType & {
   labelWidth: number;
   startPosition: number;
+  signalId: string;
+  assignment: string;
 };
 
 function useStackRangesAssignmentsLabels(ranges: RangeType[]) {
@@ -35,23 +36,32 @@ function useStackRangesAssignmentsLabels(ranges: RangeType[]) {
 
   if (ranges.length === 0) return null;
 
-  const processedRanges: ProcessedRange[] = ranges.map((range) => {
-    const { from, assignment = '' } = range;
-    const labelWidth = getTextWidth(assignment);
+  const processedRanges: ProcessedRange[] = [];
 
-    return {
-      ...range,
-      labelWidth,
-      startPosition: scaleX()(from),
-    };
-  });
-  processedRanges.sort((a, b) => b.from - a.from);
+  for (const range of ranges) {
+    const { from, signals } = range;
 
+    const startPosition = scaleX()(from);
+    for (const signal of signals) {
+      const { id, assignment = '' } = signal;
+      const labelWidth = getTextWidth(assignment);
+
+      processedRanges.push({
+        ...range,
+        labelWidth,
+        startPosition,
+        signalId: id,
+        assignment,
+      });
+    }
+  }
+
+  processedRanges.sort((a, b) => b.startPosition - a.startPosition);
   return stackOverlappingLabelsMap(processedRanges, {
     startPositionKey: 'startPosition',
     labelWidthKey: 'labelWidth',
     padding: 0,
-    idKey: 'id',
+    idKey: 'signalId',
   });
 }
 
@@ -63,23 +73,18 @@ function RangesInner({
   const assignmentLabelsStackIndexes = useStackRangesAssignmentsLabels(
     ranges.values,
   );
-
   return (
-    <ShareDataProvider>
-      <g>
-        {ranges.values.map((range) => (
-          <Range
-            key={range.id}
-            range={range}
-            selectedTool={selectedTool}
-            relativeFormat={relativeFormat}
-            assignmentLabelStackIndex={
-              assignmentLabelsStackIndexes?.[range.id] || 0
-            }
-          />
-        ))}
-      </g>
-    </ShareDataProvider>
+    <g>
+      {ranges.values.map((range) => (
+        <Range
+          key={range.id}
+          range={range}
+          selectedTool={selectedTool}
+          relativeFormat={relativeFormat}
+          signalsStackIndexes={assignmentLabelsStackIndexes || {}}
+        />
+      ))}
+    </g>
   );
 }
 

commit f664ce06645c5e11e575bce87caa9bd09f5ea8da

fix: sync assignment labels
Bug introduced in PR #3835

src/component/reducer/actions/RangesActions.ts
@@ -465,7 +465,7 @@ function handleChangeRangesAssignmentLabelsByDiaIds(
     for (const signal of signals) {
       if (
         previousAssignment === signal.assignment &&
-        uniqueDiaIds.has(signal.id)
+        signal.diaIDs?.some((id) => uniqueDiaIds.has(id))
       ) {
         signal.assignment = assignment;
       }