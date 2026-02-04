e9e4bc40fd6241d07545d0aa5e7eb4240ed03e38
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2026-02-04 14:33:47 +0100

    chore: move `lib_interoperability.test-d.ts` to core

--- src/__tests__/lib_interoperability.test-d.ts
+++
@@ -1,9 +0,0 @@
-import type { TextStyle } from '@zakodium/nmrium-core';
-import type { SVGStyledTextUserConfig } from 'react-science/ui';
-import { describe, expectTypeOf, it } from 'vitest';
-
-describe('interoperability between nmrium-core and react-science', () => {
-  it('TextStyle should match `SVGStyledTextUserConfig`', () => {
-    expectTypeOf<TextStyle>().toEqualTypeOf<SVGStyledTextUserConfig>();
-  });
-});
