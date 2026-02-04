95d7d6b5f83b931c230d91113631852aa7d91195
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2026-02-04 09:50:59 +0100

    fix: vitest typecheck config

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,10 +17,11 @@ export default () => {
       },
     },
     test: {
-      include: [
-        './src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-        './src/**/*.test-d.ts',
-      ],
+      include: ['./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      typecheck: {
+        enabled: true,
+        include: ['./src/**/*.test-d.ts'],
+      },
     },
   });
 };
