880d4bbabf39eaeb7e1bd6f6ebed8e407070f616
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2026-02-12 15:45:36 +0100

    refactor: use radio group integration
    * move complex `onChange` functions outside of the jsx
    * flatten the `Subscribe` related to `mode`
    * split to `DescriptionPreview`

--- src/component/modal/setting/tanstack_general_settings/tabs/export_tab.fields.tsx
+++ src/component/modal/setting/tanstack_general_settings/tabs/export_tab.fields.tsx
@@ -1,20 +1,10 @@
-import {
-  Checkbox,
-  Radio,
-  RadioGroup,
-  SegmentedControl,
-  Tag,
-} from '@blueprintjs/core';
+import { Checkbox, Tag } from '@blueprintjs/core';
 import { useStore } from '@tanstack/react-form';
 import type { PageSizeName, Unit } from '@zakodium/nmrium-core';
 import { units } from '@zakodium/nmrium-core';
-import { useMemo } from 'react';
-import {
-  FormGroup,
-  assert,
-  assertUnreachable,
-  withFieldGroup,
-} from 'react-science/ui';
+import { memo, useMemo } from 'react';
+import { FormGroup, assertUnreachable, withFieldGroup } from 'react-science/ui';
+import type { z } from 'zod/v4';
 
 import { convertToPixels } from '../../../../elements/export/units.ts';
 import { useExportConfigurer } from '../../../../elements/export/useExportConfigurer.tsx';
@@ -54,98 +44,121 @@ const unitItems: Array<SelectItem<Unit>> = units.map((u) => ({
   label: u.name,
   value: u.unit,
 }));
+const layoutItems: Array<SelectItem<Layout>> = [
+  { value: 'portrait', label: 'Portrait' },
+  { value: 'landscape', label: 'Landscape' },
+];
 
 export const ExportFields = withFieldGroup({
   defaultValues: defaultGeneralSettingsFormValues.export.png,
-  /* eslint-disable react-hooks/rules-of-hooks */
   render: ({ group }) => {
+    /* eslint-disable react-hooks/rules-of-hooks */
     const inputValues = useStore(group.store, (s) => s.values);
     const outputValues = useMemo(() => {
       return exportSettingsValidation.decode(inputValues);
     }, [inputValues]);
     const advancedTransforms = useExportConfigurer(outputValues);
+    /* eslint-enable react-hooks/rules-of-hooks */
+
+    function onModeChange({ value }: { value: Mode }) {
+      const newOptions = getExportDefaultOptionsByMode(value);
+
+      for (const [key, value] of Object.entries(newOptions)) {
+        group.setFieldValue(key as keyof typeof newOptions, value, {
+          dontRunListeners: true,
+        });
+      }
+    }
+
+    function onChangeUnit({ value }: { value: Unit }) {
+      const { width, height } = advancedTransforms.changeUnit({ unit: value });
+      group.setFieldValue('width', String(width), {
+        dontRunListeners: true,
+      });
+      group.setFieldValue('height', String(height), {
+        dontRunListeners: true,
+      });
+    }
+    function onWidthChange({ value }: { value: string }) {
+      const height = advancedTransforms.changeSize(
+        Number(value),
+        'height',
+        'width',
+      );
+      if (!advancedTransforms.isAspectRatioEnabled) {
+        return;
+      }
+      group.setFieldValue('height', String(height), {
+        dontRunListeners: true,
+      });
+    }
+    function onHeightChange({ value }: { value: string }) {
+      const width = advancedTransforms.changeSize(
+        Number(value),
+        'width',
+        'height',
+      );
+      if (!advancedTransforms.isAspectRatioEnabled) {
+        return;
+      }
+      group.setFieldValue('width', String(width), {
+        dontRunListeners: true,
+      });
+    }
+
+    function onDPIChange({ value }: { value: string }) {
+      if (group.state.values.mode !== 'advance') return;
+      if (group.state.values.unit !== 'px') return;
+
+      const { width, height } = advancedTransforms.changeDPI(Number(value));
+      group.setFieldValue('width', String(width), {
+        dontRunListeners: true,
+      });
+      group.setFieldValue('height', String(height), {
+        dontRunListeners: true,
+      });
+    }
 
     return (
       <>
-        <group.Field name="mode">
-          {(field) => (
-            <FormGroup label="Mode">
-              <SegmentedControl
-                defaultValue={field.state.value}
-                onValueChange={(v) => {
-                  const newMode = v as Mode;
-                  const newOptions = getExportDefaultOptionsByMode(newMode);
-
-                  for (const [key, value] of Object.entries(newOptions)) {
-                    group.setFieldValue(key as keyof typeof newOptions, value, {
-                      dontRunListeners: true,
-                    });
-                  }
-                }}
-                options={modeItems}
-                inline
-              />
-            </FormGroup>
-          )}
-        </group.Field>
-        <group.Subscribe selector={(s) => s.values}>
-          {(values) => {
-            const { width, height, dpi, unit } = getExportOptions(
-              exportSettingsValidation.decode(values),
-            );
-            const widthInPixel = convertToPixels(width, unit, dpi, {
-              precision: 0,
-            });
-            const heightInPixel = convertToPixels(height, unit, dpi, {
-              precision: 0,
-            });
-
-            return (
-              <FormGroup label="Description">
-                <Tag>{`${widthInPixel} px x ${heightInPixel} px @ ${dpi}DPI`}</Tag>
-              </FormGroup>
-            );
+        <group.AppField name="mode" listeners={{ onChange: onModeChange }}>
+          {(field) => <field.RadioGroup label="Mode" options={modeItems} />}
+        </group.AppField>
+        <DescriptionPreview {...outputValues} />
+        <group.Subscribe
+          selector={(state) => {
+            const mode = state.values.mode;
+            switch (state.values.mode) {
+              case 'basic':
+                return { mode: state.values.mode, layout: state.values.layout };
+              case 'advance':
+                return { mode: state.values.mode, unit: state.values.unit };
+              default:
+                assertUnreachable(mode as never);
+            }
           }}
-        </group.Subscribe>
-        <group.Subscribe selector={(state) => state.values.mode}>
-          {(mode) => {
+        >
+          {({ mode, layout, unit }) => {
             switch (mode) {
               case 'basic':
                 return (
                   <>
-                    <group.Subscribe
-                      selector={(state) => {
-                        assert(state.values.mode === 'basic');
-                        return pageSizeItems[state.values.layout];
-                      }}
-                    >
-                      {(items) => (
-                        <group.AppField name="size">
-                          {(field) => (
-                            <field.Select label="Size" items={items} />
-                          )}
-                        </group.AppField>
-                      )}
-                    </group.Subscribe>
-                    <group.Field name="layout">
+                    <group.AppField name="size">
                       {(field) => (
-                        <FormGroup name={field.name} label="Layout">
-                          <RadioGroup
-                            inline
-                            name={field.name}
-                            selectedValue={field.state.value}
-                            onChange={(event) =>
-                              field.handleChange(
-                                event.currentTarget.value as Layout,
-                              )
-                            }
-                          >
-                            <Radio label="Portrait" value="portrait" />
-                            <Radio label="Landscape" value="landscape" />
-                          </RadioGroup>
-                        </FormGroup>
+                        <field.Select
+                          label="Size"
+                          items={pageSizeItems[layout]}
+                        />
                       )}
-                    </group.Field>
+                    </group.AppField>
+                    <group.AppField name="layout">
+                      {(field) => (
+                        <field.RadioGroup
+                          label="Layout"
+                          options={layoutItems}
+                        />
+                      )}
+                    </group.AppField>
                   </>
                 );
               case 'advance':
@@ -153,18 +166,7 @@ export const ExportFields = withFieldGroup({
                   <>
                     <group.AppField
                       name="unit"
-                      listeners={{
-                        onChange: ({ value }) => {
-                          const { width, height } =
-                            advancedTransforms.changeUnit({ unit: value });
-                          group.setFieldValue('width', String(width), {
-                            dontRunListeners: true,
-                          });
-                          group.setFieldValue('height', String(height), {
-                            dontRunListeners: true,
-                          });
-                        },
-                      }}
+                      listeners={{ onChange: onChangeUnit }}
                     >
                       {(field) => (
                         <field.Select label="Unit" items={unitItems} />
@@ -179,67 +181,28 @@ export const ExportFields = withFieldGroup({
                         );
                       }}
                     />
-                    <group.Subscribe
-                      selector={(state) => {
-                        assert(state.values.mode === 'advance');
-                        return state.values.unit;
-                      }}
+                    <group.AppField
+                      name="width"
+                      listeners={{ onChange: onWidthChange }}
                     >
-                      {(unit) => (
-                        <>
-                          <group.AppField
-                            name="width"
-                            listeners={{
-                              onChange: ({ value }) => {
-                                const height = advancedTransforms.changeSize(
-                                  Number(value),
-                                  'height',
-                                  'width',
-                                );
-                                if (!advancedTransforms.isAspectRatioEnabled) {
-                                  return;
-                                }
-                                group.setFieldValue('height', String(height), {
-                                  dontRunListeners: true,
-                                });
-                              },
-                            }}
-                          >
-                            {(field) => (
-                              <field.NumericInput
-                                label="Width"
-                                rightElement={<Tag>{unit}</Tag>}
-                              />
-                            )}
-                          </group.AppField>
-                          <group.AppField
-                            name="height"
-                            listeners={{
-                              onChange: ({ value }) => {
-                                const width = advancedTransforms.changeSize(
-                                  Number(value),
-                                  'width',
-                                  'height',
-                                );
-                                if (!advancedTransforms.isAspectRatioEnabled) {
-                                  return;
-                                }
-                                group.setFieldValue('width', String(width), {
-                                  dontRunListeners: true,
-                                });
-                              },
-                            }}
-                          >
-                            {(field) => (
-                              <field.NumericInput
-                                label="Height"
-                                rightElement={<Tag>{unit}</Tag>}
-                              />
-                            )}
-                          </group.AppField>
-                        </>
+                      {(field) => (
+                        <field.NumericInput
+                          label="Width"
+                          rightElement={<Tag>{unit}</Tag>}
+                        />
                       )}
-                    </group.Subscribe>
+                    </group.AppField>
+                    <group.AppField
+                      name="height"
+                      listeners={{ onChange: onHeightChange }}
+                    >
+                      {(field) => (
+                        <field.NumericInput
+                          label="Height"
+                          rightElement={<Tag>{unit}</Tag>}
+                        />
+                      )}
+                    </group.AppField>
                   </>
                 );
               default:
@@ -247,23 +210,7 @@ export const ExportFields = withFieldGroup({
             }
           }}
         </group.Subscribe>
-        <group.AppField
-          name="dpi"
-          listeners={{
-            onChange: ({ value }) => {
-              if (group.state.values.mode !== 'advance') return;
-              if (group.state.values.unit !== 'px') return;
-
-              const { width, height } = advancedTransforms.changeDPI(value);
-              group.setFieldValue('width', String(width), {
-                dontRunListeners: true,
-              });
-              group.setFieldValue('height', String(height), {
-                dontRunListeners: true,
-              });
-            },
-          }}
-        >
+        <group.AppField name="dpi" listeners={{ onChange: onDPIChange }}>
           {(field) => <field.NumericInput label="DPI" />}
         </group.AppField>
         <group.AppField name="useDefaultSettings">
@@ -274,5 +221,20 @@ export const ExportFields = withFieldGroup({
       </>
     );
   },
-  /* eslint-enable react-hooks/rules-of-hooks */
+});
+
+type DescriptionPreviewProps = z.output<typeof exportSettingsValidation>;
+const DescriptionPreview = memo(function DescriptionPreview(
+  props: DescriptionPreviewProps,
+) {
+  const { width, height, dpi, unit } = getExportOptions(props);
+  const convertOptions = { precision: 0 };
+  const widthInPixel = convertToPixels(width, unit, dpi, convertOptions);
+  const heightInPixel = convertToPixels(height, unit, dpi, convertOptions);
+
+  return (
+    <FormGroup label="Description">
+      <Tag>{`${widthInPixel} px x ${heightInPixel} px @ ${dpi}DPI`}</Tag>
+    </FormGroup>
+  );
 });
