395a351c43071035b3cf13711189bf35d595e26b
Michaël Zasso <targos@protonmail.com>
2026-02-16 08:34:29 +0100

    fix knip

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,6 @@
 import type { KnipConfig } from 'knip';
 
 export default {
-  entry: ['src/index.tsx'],
   ignoreBinaries: ['jq'],
   ignoreDependencies: ['@simbathesailor/use-what-changed'],
 } satisfies KnipConfig;
