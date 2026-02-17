7771111517adc5d16e25a4cbcfa4baca857b68de
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-02-17 16:58:44 +0100

    refactor: made changes according to reviewer

--- src/component/modal/setting/tanstack_general_settings/tabs/database_tab.tsx
+++ src/component/modal/setting/tanstack_general_settings/tabs/database_tab.tsx
@@ -1,33 +1,26 @@
-import { Checkbox, Classes } from '@blueprintjs/core';
+import { Classes } from '@blueprintjs/core';
 import styled from '@emotion/styled';
-import { useStore } from '@tanstack/react-form';
+import { useField } from '@tanstack/react-form';
 import { useCallback, useMemo } from 'react';
 import { FaLink, FaPlus, FaRegTrashAlt } from 'react-icons/fa';
 import { Button, withForm } from 'react-science/ui';
 import type { CellProps } from 'react-table';
+import type { z } from 'zod';
 
-import { usePreferences } from '../../../../context/PreferencesContext.tsx';
-import { GroupPane } from '../../../../elements/GroupPane.tsx';
-import { Input2 } from '../../../../elements/Input2.tsx';
 import type { Column } from '../../../../elements/ReactTable/ReactTable.tsx';
-import ReactTable from '../../../../elements/ReactTable/ReactTable.tsx';
-import { getPreferencesByWorkspace } from '../../../../reducer/preferences/utilities/getPreferencesByWorkspace.ts';
 import { isGoogleDocument } from '../../../../utility/isGoogleDocument.ts';
-import { Section } from '../../general_settings.tsx';
+import {
+  CellActions,
+  CellCheckbox,
+  CellInput,
+  TableSettings,
+} from '../ui/table.tsx';
+import { TableSection } from '../ui/table_section.tsx';
+import type { databasesValidation } from '../validation/database_tab_validation.ts';
 import { defaultGeneralSettingsFormValues } from '../validation.ts';
 
-interface DatabaseFormElement {
-  key: string;
-  label: string;
-  url: string;
-  enabled: boolean;
-}
-
-const Buttons = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 0 3px;
-`;
+type DatabaseForm = z.input<typeof databasesValidation>;
+type DatabaseFormElement = DatabaseForm['data'][number];
 
 const StyledButton = styled(Button, {
   shouldForwardProp(propName) {
@@ -47,57 +40,29 @@ const emptyDatabaseFormElement: DatabaseFormElement = {
 export const DatabaseTab = withForm({
   defaultValues: defaultGeneralSettingsFormValues,
   render: function Render({ form }) {
-    const {
-      originalWorkspaces,
-      workspace: { current: workspaceName },
-    } = usePreferences();
-
-    const databases = useStore(
-      form.store,
-      (state) => state.values.databases.data,
-    );
+    const field = useField({ form, name: 'databases.data', mode: 'array' });
 
     const handleDelete = useCallback(
-      (
-        data: readonly DatabaseFormElement[],
-        formElement: DatabaseFormElement,
-      ) => {
-        const databases = data.filter((element) => {
-          return element.key !== formElement.key;
-        });
+      (formElement: DatabaseFormElement) => {
+        const index = field.state.value.findIndex(
+          (e) => e.key === formElement.key,
+        );
 
-        form.setFieldValue('databases.data', databases);
+        field.removeValue(index);
       },
-      [form],
+      [field],
     );
 
     const handleAdd = useCallback(
-      (data: readonly DatabaseFormElement[], index: number) => {
-        let databases: DatabaseFormElement[] = [];
-
-        if (data) {
-          databases = [
-            ...data.slice(0, index),
-            emptyDatabaseFormElement,
-            ...data.slice(index),
-          ];
-        } else {
-          databases.push(emptyDatabaseFormElement);
-        }
-
-        form.setFieldValue('databases.data', databases);
+      (index: number) => {
+        field.insertValue(index, emptyDatabaseFormElement);
       },
-      [form],
+      [field],
     );
 
     const handleReset = useCallback(() => {
-      const workSpaceDisplayPreferences = getPreferencesByWorkspace(
-        workspaceName,
-        originalWorkspaces,
-      );
-
-      form.setFieldValue('databases', workSpaceDisplayPreferences.databases);
-    }, [form, originalWorkspaces, workspaceName]);
+      form.resetField('databases');
+    }, [form]);
 
     const COLUMNS = useMemo<Array<Column<DatabaseFormElement>>>(() => {
       return [
@@ -108,11 +73,8 @@ export const DatabaseTab = withForm({
             return (
               <form.Field key={index} name={`databases.data[${index}].label`}>
                 {(subField) => (
-                  <Input2
-                    style={{
-                      backgroundColor: 'transparent',
-                      boxShadow: 'none',
-                    }}
+                  <CellInput
+                    onBlur={field.handleBlur}
                     value={subField.state.value}
                     onChange={subField.handleChange}
                   />
@@ -128,11 +90,8 @@ export const DatabaseTab = withForm({
             return (
               <form.Field key={index} name={`databases.data[${index}].url`}>
                 {(subField) => (
-                  <Input2
-                    style={{
-                      backgroundColor: 'transparent',
-                      boxShadow: 'none',
-                    }}
+                  <CellInput
+                    onBlur={field.handleBlur}
                     value={subField.state.value}
                     onChange={subField.handleChange}
                   />
@@ -148,9 +107,9 @@ export const DatabaseTab = withForm({
             return (
               <form.Field key={index} name={`databases.data[${index}].enabled`}>
                 {(subField) => (
-                  <Checkbox
+                  <CellCheckbox
+                    onBlur={field.handleBlur}
                     checked={subField.state.value}
-                    value={String(subField.state.value)}
                     onChange={(event) => {
                       subField.handleChange(event.currentTarget.checked);
                     }}
@@ -172,10 +131,9 @@ export const DatabaseTab = withForm({
             return (
               <form.Field key={index} name={`databases.defaultDatabase`}>
                 {(subField) => (
-                  <Checkbox
-                    style={{ margin: 0 }}
+                  <CellCheckbox
+                    onBlur={field.handleBlur}
                     checked={subField.state.value === key}
-                    value={String(subField.state.value === key)}
                     onChange={() => {
                       subField.handleChange(
                         subField.state.value === key ? '' : key,
@@ -193,16 +151,15 @@ export const DatabaseTab = withForm({
           id: 'op-buttons',
           Cell: ({
             row: { original, index: rowIndex },
-            data,
           }: CellProps<DatabaseFormElement>) => {
             return (
-              <Buttons>
+              <CellActions>
                 <Button
                   size="small"
                   intent="success"
                   variant="outlined"
                   tooltipProps={{ content: '', disabled: true }}
-                  onClick={() => handleAdd(data, rowIndex + 1)}
+                  onClick={() => handleAdd(rowIndex + 1)}
                 >
                   <FaPlus className={Classes.ICON} />
                 </Button>
@@ -212,7 +169,7 @@ export const DatabaseTab = withForm({
                   variant="outlined"
                   intent="danger"
                   tooltipProps={{ content: '', disabled: true }}
-                  onClick={() => handleDelete(data, original)}
+                  onClick={() => handleDelete(original)}
                 >
                   <FaRegTrashAlt className={Classes.ICON} />
                 </StyledButton>
@@ -228,86 +185,51 @@ export const DatabaseTab = withForm({
                     <FaLink className={Classes.ICON} />
                   </Button>
                 )}
-              </Buttons>
+              </CellActions>
             );
           },
         },
       ];
-    }, [form, handleAdd, handleDelete]);
+    }, [field.handleBlur, form, handleAdd, handleDelete]);
 
     return (
-      <GroupPane
-        text="Databases"
-        renderHeader={(text) => {
-          return (
-            <DatabaseHeader
-              text={text}
-              handleReset={handleReset}
-              handleAdd={() => handleAdd(databases, 0)}
-            />
-          );
-        }}
+      <TableSection
+        title="Databases"
+        actions={
+          <DatabaseHeaderButtons>
+            <Button
+              size="small"
+              variant="minimal"
+              intent="danger"
+              tooltipProps={{ content: '', disabled: true }}
+              onClick={handleReset}
+            >
+              Reset Databases
+            </Button>
+
+            <Button
+              size="small"
+              variant="outlined"
+              intent="success"
+              tooltipProps={{ content: '', disabled: true }}
+              onClick={() => handleAdd(0)}
+            >
+              Add Database
+            </Button>
+          </DatabaseHeaderButtons>
+        }
       >
-        <ReactTable
-          style={{
-            'thead tr th': { zIndex: 1 },
-            td: { padding: 0 },
-          }}
-          rowStyle={{
-            hover: { backgroundColor: '#f7f7f7' },
-            active: { backgroundColor: '#f5f5f5' },
-          }}
-          data={databases}
+        <TableSettings
+          data={field.state.value}
           columns={COLUMNS}
           emptyDataRowText="No Fields"
         />
-      </GroupPane>
+      </TableSection>
     );
   },
 });
-
-interface DatabaseHeaderProps {
-  text: string;
-  handleReset: () => void;
-  handleAdd: () => void;
-}
-
-const DatabaseHeaderText = styled.p`
-  flex: 1;
-`;
 
 const DatabaseHeaderButtons = styled.div`
   display: flex;
   justify-content: space-between;
 `;
-
-function DatabaseHeader(props: DatabaseHeaderProps) {
-  const { text, handleReset, handleAdd } = props;
-
-  return (
-    <Section>
-      <DatabaseHeaderText>{text}</DatabaseHeaderText>
-      <DatabaseHeaderButtons>
-        <Button
-          size="small"
-          variant="minimal"
-          intent="danger"
-          tooltipProps={{ content: '', disabled: true }}
-          onClick={handleReset}
-        >
-          Reset Databases
-        </Button>
-
-        <Button
-          size="small"
-          variant="outlined"
-          intent="success"
-          tooltipProps={{ content: '', disabled: true }}
-          onClick={handleAdd}
-        >
-          Add Database
-        </Button>
-      </DatabaseHeaderButtons>
-    </Section>
-  );
-}
