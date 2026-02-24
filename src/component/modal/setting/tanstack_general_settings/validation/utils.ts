3af34d6ef5268fecc705132159541527383a66b0
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-02-24 13:04:50 +0100

    refactor: re-add min 1 on jpathcodex

--- src/component/modal/setting/tanstack_general_settings/validation/utils.ts
+++ src/component/modal/setting/tanstack_general_settings/validation/utils.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod/v4';
 
-export const jpathCodec = z.codec(z.string(), z.array(z.string()), {
+export const jpathCodec = z.codec(z.string(), z.array(z.string().min(1)), {
   encode: (path) => path.join('.'),
   decode: (path) => path.split('.'),
 });
