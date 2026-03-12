5ca9744c2aa8a53e29a22e50f57caf7afcd266d9
Michaël Zasso <targos@protonmail.com>
2026-03-12 15:30:18 +0100

    fixup! feat: add `loadFileCollection` imperative API

--- src/demo/views/RefAPI.tsx
+++ src/demo/views/RefAPI.tsx
@@ -1,3 +1,4 @@
+import { FileCollection } from 'file-collection';
 import { useCallback, useRef } from 'react';
 import { Button, DropZone } from 'react-science/ui';
 
@@ -7,7 +8,13 @@ import { saveAs } from '../../component/utility/save_as.ts';
 
 export default function RefAPI() {
   const nmriumRef = useRef<NMRiumRefAPI>(null);
-  const dropFileHandler = useCallback((dropFiles: File[]) => {
+  const dropLoadFileCollectionHandler = useCallback((dropFiles: File[]) => {
+    const fileCollection = new FileCollection();
+    void fileCollection.appendFileList(dropFiles).then(() => {
+      nmriumRef.current?.loadFileCollection(fileCollection);
+    });
+  }, []);
+  const dropLoadFilesHandler = useCallback((dropFiles: File[]) => {
     nmriumRef.current?.loadFiles(dropFiles);
   }, []);
 
@@ -52,14 +59,14 @@ export default function RefAPI() {
           <div style={{ flex: 3, display: 'flex', flexDirection: 'column' }}>
             <div style={{ flex: 1 }}>
               <DropZone
-                onDrop={dropFileHandler}
+                onDrop={dropLoadFileCollectionHandler}
                 emptyTitle="Drop data files"
                 emptyDescription="To load with ref.loadFileCollection API"
               />
             </div>
             <div style={{ flex: 1 }}>
               <DropZone
-                onDrop={dropFileHandler}
+                onDrop={dropLoadFilesHandler}
                 emptyTitle="Drop data files"
                 emptyDescription="To load with ref.loadFiles API"
               />
