50a23a663819d8f38ffbdb413ce9d2a3aae610be
hamed musallam <hamed.musallam@gmail.com>
2026-03-24 16:26:52 +0100

    refactor: props type of NumberInputFieldProps component

--- src/component/elements/NumberInputField.tsx
+++ src/component/elements/NumberInputField.tsx
@@ -1,13 +1,19 @@
+import type { FieldValues } from 'react-hook-form';
+
 import type { LabelProps } from './Label.tsx';
 import Label from './Label.tsx';
 import type { NumberInput2ControllerProps } from './NumberInput2Controller.tsx';
 import { NumberInput2Controller } from './NumberInput2Controller.tsx';
 
-interface NumberInputFieldProps extends NumberInput2ControllerProps {
+interface NumberInputFieldProps<
+  T extends FieldValues,
+> extends NumberInput2ControllerProps<T> {
   labelProps: Omit<LabelProps, 'children'>;
 }
 
-export function NumberInputField(props: NumberInputFieldProps) {
+export function NumberInputField<T extends FieldValues>(
+  props: NumberInputFieldProps<T>,
+) {
   const { labelProps, ...inputProps } = props;
   return (
     <Label {...labelProps}>
