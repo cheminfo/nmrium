603dddc13ff45045c113750a4440fb79145b0b3c
hamed musallam <hamed.musallam@gmail.com>
2026-04-10 10:57:52 +0200

    chore: update vite to version 8

--- knip.config.ts
+++ knip.config.ts
@@ -2,5 +2,5 @@ import type { KnipConfig } from 'knip';
 
 export default {
   ignoreBinaries: ['jq'],
-  ignoreDependencies: ['@simbathesailor/use-what-changed', '@vitest/coverage-v8'],
+  ignoreDependencies: ['@simbathesailor/use-what-changed'],
 } satisfies KnipConfig;
