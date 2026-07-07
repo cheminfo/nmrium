93be29a98d6e28398783269d14f7eb0f5cec76b1
Michaël Zasso <targos@protonmail.com>
2026-07-07 15:14:17 +0200

    refactor: migrate matrix exclusions table to tanstack

--- src/component/panels/MatrixGenerationPanel/ExclusionsZonesTable.tsx
+++ src/component/panels/MatrixGenerationPanel/ExclusionsZonesTable.tsx
@@ -1,77 +1,84 @@
 import { Button, Classes } from '@blueprintjs/core';
+import { useCallback, useMemo } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { FaRegTrashAlt } from 'react-icons/fa';
-import type { CellProps } from 'react-table';
 
 import { NumberInput2Controller } from '../../elements/NumberInput2Controller.js';
-import type { Column } from '../../elements/ReactTable/ReactTable.js';
-import ReactTable from '../../elements/ReactTable/ReactTable.js';
+import type { TanStackTableColumn } from '../../elements/TanStackTable/TanStackTable.js';
+import TanStackTable from '../../elements/TanStackTable/TanStackTable.js';
 
 export function ExclusionsZonesTable() {
-  const { setValue, control } = useFormContext<any>();
+  const { setValue, control, getValues } = useFormContext<any>();
   const exclusionsZones = useWatch({ name: 'exclusionsZones' });
+
+  const handleDelete = useCallback(
+    (index: any) => {
+      setValue(
+        'exclusionsZones',
+        getValues('exclusionsZones').filter((_: any, i: any) => i !== index),
+      );
+    },
+    [getValues, setValue],
+  );
+
+  const exclusionsZonesColumns = useMemo<Array<TanStackTableColumn<any>>>(
+    () => [
+      {
+        header: '#',
+        meta: { style: { width: '50px' } },
+        accessorFn: (_, index) => index + 1,
+      },
+      {
+        header: 'from',
+        cell: ({ row }) => (
+          <NumberInput2Controller
+            control={control}
+            name={`exclusionsZones.${row.index}.from`}
+            fill
+            noShadowBox
+            style={{ backgroundColor: 'transparent' }}
+          />
+        ),
+      },
+      {
+        header: 'To',
+        cell: ({ row }) => (
+          <NumberInput2Controller
+            name={`exclusionsZones.${row.index}.to`}
+            fill
+            noShadowBox
+            style={{ backgroundColor: 'transparent' }}
+          />
+        ),
+      },
+
+      {
+        header: '',
+        meta: { style: { width: '70px' } },
+        id: 'actions',
+        cell: ({ row }) => {
+          return (
+            <Button
+              size="small"
+              variant="outlined"
+              intent="danger"
+              onClick={() => handleDelete(row.index)}
+            >
+              <FaRegTrashAlt className={Classes.ICON} />
+            </Button>
+          );
+        },
+      },
+    ],
+    [control, handleDelete],
+  );
+
   if (!exclusionsZones || !Array.isArray(exclusionsZones)) {
     return null;
   }
 
-  function handleDelete(index: any) {
-    setValue(
-      'exclusionsZones',
-      exclusionsZones.filter((_: any, i: any) => i !== index),
-    );
-  }
-
-  const exclusionsZonesColumns: Array<Column<any>> = [
-    {
-      Header: '#',
-      style: { width: '50px' },
-      accessor: (_: any, index) => index + 1,
-    },
-    {
-      Header: 'from',
-      Cell: ({ row }: CellProps<any>) => (
-        <NumberInput2Controller
-          control={control}
-          name={`exclusionsZones.${row.index}.from`}
-          fill
-          noShadowBox
-          style={{ backgroundColor: 'transparent' }}
-        />
-      ),
-    },
-    {
-      Header: 'To',
-      Cell: ({ row }: CellProps<any>) => (
-        <NumberInput2Controller
-          name={`exclusionsZones.${row.index}.to`}
-          fill
-          noShadowBox
-          style={{ backgroundColor: 'transparent' }}
-        />
-      ),
-    },
-
-    {
-      Header: '',
-      style: { width: '70px' },
-      id: 'actions',
-      Cell: ({ row }: CellProps<any>) => {
-        return (
-          <Button
-            size="small"
-            variant="outlined"
-            intent="danger"
-            onClick={() => handleDelete(row.index)}
-          >
-            <FaRegTrashAlt className={Classes.ICON} />
-          </Button>
-        );
-      },
-    },
-  ];
-
   return (
-    <ReactTable
+    <TanStackTable
       columns={exclusionsZonesColumns}
       data={exclusionsZones}
       emptyDataRowText="No Zones"
