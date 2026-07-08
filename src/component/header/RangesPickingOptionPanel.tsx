b5807a945829d7e50563d1d7f88b7d953b5c97df
Sebastien Ahkrin <30870051+Sebastien-Ahkrin@users.noreply.github.com>
2026-07-08 16:12:24 +0200

    fix: make changes according to reviewer

--- src/component/header/RangesPickingOptionPanel.tsx
+++ src/component/header/RangesPickingOptionPanel.tsx
@@ -1,9 +1,7 @@
 import { Checkbox, FormGroup, NumericInput } from '@blueprintjs/core';
 import styled from '@emotion/styled';
 import { revalidateLogic } from '@tanstack/react-form';
-import type { FormEvent } from 'react';
-import { useCallback } from 'react';
-import { useForm } from 'react-science/ui';
+import { AppForm, useForm } from 'react-science/ui';
 import { z } from 'zod';
 
 import { useDispatch } from '../context/DispatchContext.js';
@@ -14,23 +12,24 @@ import {
 } from '../hooks/useCheckPointsNumberInWindowArea.js';
 
 const validationZodSchema = z.object({
-  minMaxRatio: z.number().min(0),
+  minMaxRatio: z.coerce.number<string>().min(0),
   lookNegative: z.boolean(),
 });
 
-type AutoRangesOptions = z.infer<typeof validationZodSchema>;
-
-const defaultValues: AutoRangesOptions = {
-  minMaxRatio: 0.05,
+const defaultValues: AutoRangesOptionsInput = {
+  minMaxRatio: '0.05',
   lookNegative: false,
 };
 
-const FormContainer = styled.div`
+const AppFormContainer = styled(AppForm)`
   display: flex;
   flex-direction: row;
   gap: 10px;
   align-items: center;
 `;
+
+type AutoRangesOptionsInput = z.input<typeof validationZodSchema>;
+type AutoRangesOptions = z.output<typeof validationZodSchema>;
 
 function RangesPickingOptionPanel() {
   const dispatch = useDispatch();
@@ -64,61 +63,43 @@ function RangesPickingOptionPanel() {
     },
   });
 
-  const onSubmit = useCallback(
-    (event: FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      void form.handleSubmit(event);
-    },
-    [form],
-  );
-
   return (
-    <form noValidate onSubmit={onSubmit}>
-      <form.AppForm>
-        <FormContainer>
-          <form.AppField name="lookNegative">
-            {(field) => (
-              <Checkbox
-                style={{ marginBottom: 0 }}
-                label="Detect negative"
-                checked={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={({ target: { checked } }) =>
-                  field.handleChange(checked)
-                }
-              />
-            )}
-          </form.AppField>
+    <AppFormContainer form={form}>
+      <form.AppField name="lookNegative">
+        {(field) => (
+          <Checkbox
+            style={{ marginBottom: 0 }}
+            label="Detect negative"
+            checked={field.state.value}
+            onBlur={field.handleBlur}
+            onChange={({ target: { checked } }) => field.handleChange(checked)}
+          />
+        )}
+      </form.AppField>
 
-          <form.AppField name="minMaxRatio">
-            {(field) => (
-              <FormGroup
-                label="Min/max ratio:"
-                inline
-                style={{ marginBottom: 0 }}
-              >
-                <NumericInput
-                  value={field.state.value ?? ''}
-                  onBlur={field.handleBlur}
-                  min={0}
-                  stepSize={0.01}
-                  minorStepSize={0.01}
-                  onValueChange={(valueAsNumber) => {
-                    field.handleChange(valueAsNumber);
-                  }}
-                />
-              </FormGroup>
-            )}
-          </form.AppField>
+      <form.AppField name="minMaxRatio">
+        {(field) => (
+          <FormGroup label="Min/max ratio:" inline style={{ marginBottom: 0 }}>
+            <NumericInput
+              value={field.state.value ?? ''}
+              onBlur={field.handleBlur}
+              min={0}
+              stepSize={0.01}
+              minorStepSize={0.01}
+              onValueChange={(_, valueAsString) => {
+                field.handleChange(valueAsString);
+              }}
+            />
+          </FormGroup>
+        )}
+      </form.AppField>
 
-          <div>
-            <form.SubmitButton intent="success">
-              Auto ranges picking
-            </form.SubmitButton>
-          </div>
-        </FormContainer>
-      </form.AppForm>
-    </form>
+      <div>
+        <form.SubmitButton intent="success">
+          Auto ranges picking
+        </form.SubmitButton>
+      </div>
+    </AppFormContainer>
   );
 }
 
