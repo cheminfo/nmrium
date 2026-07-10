4f99b83686c9de30a048ecf5e4fca444176b2399
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2026-07-10 12:02:03 +0200

    chore: lint

--- knip.config.ts
+++ knip.config.ts
@@ -2,5 +2,11 @@ import type { KnipConfig } from 'knip';
 
 export default {
   ignoreBinaries: ['jq'],
-  ignoreDependencies: ['@simbathesailor/use-what-changed'],
+  ignoreDependencies: [
+    // only for dev debugging
+    '@simbathesailor/use-what-changed',
+    // version fixed to 4.5.0
+    // https://github.com/react-grid-layout/react-draggable/issues/806
+    'react-draggable',
+  ],
 } satisfies KnipConfig;
