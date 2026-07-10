c849777ca96d52a6c337f7766f2ba1cb736d62d8
Michaël Zasso <targos@protonmail.com>
2026-07-10 09:27:54 +0200

    fix: do not deep clone immer drafts
    This leads to crashes when unexpected things are done with the proxies.
    Also simplify range utilities now that diaIDs only exist at the signal level.

--- src/component/reducer/preferences/actions/setPreferences.ts
+++ src/component/reducer/preferences/actions/setPreferences.ts
@@ -1,22 +1,30 @@
 import type { Draft } from 'immer';
+import { original } from 'immer';
 import cloneDeep from 'lodash/cloneDeep.js';
 
-import type { PreferencesState } from '../preferencesReducer.js';
+import type {
+  PreferencesState,
+  SetPreferencesAction,
+  WorkspaceWithSource,
+} from '../preferencesReducer.js';
 import { getActiveWorkspace } from '../utilities/getActiveWorkspace.js';
 
-export function setPreferences(draft: Draft<PreferencesState>, action: any) {
+export function setPreferences(
+  draft: Draft<PreferencesState>,
+  action: SetPreferencesAction,
+) {
   const currentWorkspacePreferences = getActiveWorkspace(draft);
 
-  if (action.payload) {
+  if ('payload' in action) {
     const preferences = action.payload;
 
     draft.workspaces[draft.workspace.current] = {
       ...currentWorkspacePreferences,
       ...preferences,
-    };
+    } as WorkspaceWithSource;
   }
 
   draft.originalWorkspaces[draft.workspace.current] = cloneDeep(
-    draft.workspaces[draft.workspace.current],
+    original(draft.workspaces[draft.workspace.current]),
   );
 }
